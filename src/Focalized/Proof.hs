29581027e6f3afa020fde7a488cb99922ae87be3
Rob Rix <rob.rix@me.com>
2021-05-17 21:58:30 -0400

    Rename Snoc to Context.

--- src/Focalized/Proof.hs
+++ src/Focalized/Proof.hs
@@ -6,12 +6,9 @@ module Focalized.Proof
 runDerivation :: Derivation a b -> [b]
 runDerivation (Derivation m) = m Nil
 
-type Context = Snoc
-
 newtype Derivation a b = Derivation (Context a -> [b])
 
-
-data Snoc a
+data Context a
   = Nil
-  | Snoc a :> a
+  | Context a :> a
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
