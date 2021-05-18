a474e63f74704fac2d20f5c8fb779cb2e0537295
Rob Rix <rob.rix@me.com>
2021-05-17 21:51:54 -0400

    Define a handler for Derivation.

--- src/Focalized/Proof.hs
+++ src/Focalized/Proof.hs
@@ -1,8 +1,12 @@
 module Focalized.Proof
-( Derivation(..)
+( runDerivation
+, Derivation(..)
 ) where
 
 import Control.Carrier.NonDet.Church
 import Data.Functor.Identity
+
+runDerivation :: Derivation a -> [a]
+runDerivation (Derivation m) = run (runNonDetA m)
 
 newtype Derivation a = Derivation (NonDetC Identity a)
