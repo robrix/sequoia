ea76486dc88ec2de1a481c364f4ec5a2a7a89a4c
Rob Rix <rob.rix@me.com>
2021-05-18 08:58:22 -0400

    Use Seq instead of Snoc/[] for contexts.

--- src/Focalized/Proof.hs
+++ src/Focalized/Proof.hs
@@ -10,44 +10,17 @@ module Focalized.Proof
 , init
 ) where
 
-import Control.Applicative (Alternative(..))
+import Data.Sequence
 import Prelude hiding (init)
 
-runProof :: Proof a b -> [b]
-runProof (Proof m) = m Nil
+runProof :: Proof a b -> Seq b
+runProof (Proof m) = m empty
 
 newtype Proof a b = Proof (Γ a |- Δ b)
 
-type Γ = Snoc
-type Δ = []
+type Γ = Seq
+type Δ = Seq
 type (|-) = (->)
-
-data Snoc a
-  = Nil
-  | Snoc a :> a
-  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
-
-infixl 5 :>
-
-instance Semigroup (Snoc a) where
-  as <> Nil       = as
-  as <> (bs :> b) = (as <> bs) :> b
-
-instance Monoid (Snoc a) where
-  mempty = Nil
-
-instance Applicative Snoc where
-  pure a = Nil :> a
-  Nil     <*> _  = Nil
-  fs :> f <*> as = (fs <*> as) <> (f <$> as)
-
-instance Alternative Snoc where
-  empty = mempty
-  (<|>) = (<>)
-
-instance Monad Snoc where
-  Nil     >>= _ = Nil
-  as :> a >>= f = (as >>= f) <> f a
 
 
 data a :|-: b = Γ a :|-: Δ b
