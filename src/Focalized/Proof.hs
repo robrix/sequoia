861d4110f014e49a7ca0881d70d9eee89a87b0e5
Rob Rix <rob.rix@me.com>
2021-05-17 21:59:24 -0400

    Use cute synonyms for the sequent.

--- src/Focalized/Proof.hs
+++ src/Focalized/Proof.hs
@@ -6,7 +6,11 @@ module Focalized.Proof
 runDerivation :: Derivation a b -> [b]
 runDerivation (Derivation m) = m Nil
 
-newtype Derivation a b = Derivation (Context a -> [b])
+newtype Derivation a b = Derivation (Γ a |- Δ b)
+
+type Γ = Context
+type Δ = []
+type (|-) = (->)
 
 data Context a
   = Nil
