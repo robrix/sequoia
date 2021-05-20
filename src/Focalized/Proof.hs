dff3b9f01a3fbfcaf3cef0160572b3579fd33e61
Rob Rix <rob.rix@me.com>
2021-05-20 11:09:38 -0400

    Define the right-rules for disjunction.

--- src/Focalized/Proof.hs
+++ src/Focalized/Proof.hs
@@ -25,6 +25,8 @@ module Focalized.Proof
 , truR
 , conjL
 , conjR
+, disjR1
+, disjR2
 ) where
 
 import           Control.Carrier.NonDet.Church
@@ -149,3 +151,15 @@ conjR p q =
   [ Just Γ :|-: Just (p <| Δ), Just Γ' :|-: Just (q <| Δ') ]
   :---:
   Just (Γ <> Γ') :|-: Just (P (p :/\: q) <| Δ <> Δ')
+
+disjR1, disjR2 :: Prop FOL String -> Prop FOL String -> Rule f (Prop FOL) String
+
+disjR1 p q =
+  [ Just Γ :|-: Just (p <| Δ) ]
+  :---:
+  Just Γ :|-: Just (P (p :/\: q) <| Δ)
+
+disjR2 p q =
+  [ Just Γ :|-: Just (q <| Δ) ]
+  :---:
+  Just Γ :|-: Just (P (p :/\: q) <| Δ)
