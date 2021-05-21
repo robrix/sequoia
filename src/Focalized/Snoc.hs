c1e34757b8ef6a227467f440130e1274ab8e5b47
Rob Rix <rob.rix@me.com>
2021-05-21 10:15:37 -0400

    Define a Show instance for Snoc.

--- src/Focalized/Snoc.hs
+++ src/Focalized/Snoc.hs
@@ -2,9 +2,14 @@ module Focalized.Snoc
 ( Snoc(..)
 ) where
 
+import Data.Foldable (toList)
+
 data Snoc a
   = Nil
   | Snoc a :> a
   deriving (Eq, Foldable, Functor, Ord, Traversable)
 
 infixl 5 :>
+
+instance Show a => Show (Snoc a) where
+  showsPrec p s = showParen (p > 10) $ showList (toList s)
