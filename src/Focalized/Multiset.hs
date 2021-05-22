ee16fd5c76cd8be7f0d1264f131a585fec88a321
Rob Rix <rob.rix@me.com>
2021-05-21 22:26:53 -0400

    Define singleton multisets.

--- src/Focalized/Multiset.hs
+++ src/Focalized/Multiset.hs
@@ -1,8 +1,12 @@
 module Focalized.Multiset
 ( Multiset
+, singleton
 ) where
 
 import qualified Data.Map as M
 
 newtype Multiset a = Multiset (M.Map a Word)
   deriving (Eq, Ord)
+
+singleton :: a -> Multiset a
+singleton a = Multiset (M.singleton a 1)
