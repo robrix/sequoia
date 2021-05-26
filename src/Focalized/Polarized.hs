aa51f59c6b6eb505f7a1b1ac9c5461d9dc83437b
Rob Rix <rob.rix@me.com>
2021-05-26 15:50:16 -0400

    Define negative and positive connectives.

--- src/Focalized/Polarized.hs
+++ src/Focalized/Polarized.hs
@@ -1,2 +1,33 @@
 module Focalized.Polarized
-() where
+( Neg(..)
+, Pos(..)
+) where
+
+data Neg a
+  = Bot
+  | Top
+  | a :⅋: a
+  | a :&: a
+  | a :->: a
+  | Not a
+  | Up a
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+infixr 6 :->:
+infixr 7 :⅋:
+infixr 8 :&:
+
+
+data Pos a
+  = Zero
+  | One
+  | a :+: a
+  | a :*: a
+  | a :-<: a
+  | Neg a
+  | Down a
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+infixr 6 :-<:
+infixr 7 :+:
+infixr 8 :*:
