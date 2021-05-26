1912042e9b2cefe6b14c6363323e23369b03a265
Rob Rix <rob.rix@me.com>
2021-05-26 17:27:03 -0400

    Define Applicative & Monad instances for Neg & Pos.

--- src/Focalized/Polarized.hs
+++ src/Focalized/Polarized.hs
@@ -3,6 +3,8 @@ module Focalized.Polarized
 , Pos(..)
 ) where
 
+import Control.Monad (ap)
+
 data Neg a
   = N a
   | Bot
@@ -17,6 +19,21 @@ data Neg a
 infixr 6 :->:
 infixr 7 :⅋:
 infixr 8 :&:
+
+instance Applicative Neg where
+  pure = N
+  (<*>) = ap
+
+instance Monad Neg where
+  m >>= f = case m of
+    N a      -> f a
+    Bot      -> Bot
+    Top      -> Top
+    a :⅋: b  -> (a >>= f) :⅋: (b >>= f)
+    a :&: b  -> (a >>= f) :&: (b >>= f)
+    a :->: b -> (a >>= Down . f) :->: (b >>= f)
+    Not a    -> Not (a >>= Down . f)
+    Up a     -> Up (a >>= Down . f)
 
 
 data Pos a
@@ -33,3 +50,18 @@ data Pos a
 infixr 6 :-<:
 infixr 7 :+:
 infixr 8 :*:
+
+instance Applicative Pos where
+  pure = P
+  (<*>) = ap
+
+instance Monad Pos where
+  m >>= f = case m of
+    P a      -> f a
+    Zero     -> Zero
+    One      -> One
+    a :+: b  -> (a >>= f) :+: (b >>= f)
+    a :*: b  -> (a >>= f) :*: (b >>= f)
+    a :-<: b -> (a >>= f) :-<: (b >>= Up . f)
+    Neg a    -> Neg (a >>= Up . f)
+    Down a   -> Down (a >>= Up . f)
