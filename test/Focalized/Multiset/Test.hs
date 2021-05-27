d0e71299d5435af212ea4fdb94aa8b9bff1bb752
Rob Rix <rob.rix@me.com>
2021-05-26 21:04:37 -0400

    Test the associativity of the Semigroup instance for Multiset.

--- test/Focalized/Multiset/Test.hs
+++ test/Focalized/Multiset/Test.hs
@@ -3,7 +3,17 @@ module Focalized.Multiset.Test
 ( tests
 ) where
 
-import Hedgehog
+import qualified Focalized.Multiset as S
+import           Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
 
 tests :: IO Bool
 tests = checkParallel $$(discover)
+
+prop_semigroup_associativity = property $ do
+  (a, b, c) <- (,,) <$> forAll set <*> forAll set <*> forAll set
+  a <> (b <> c) === (a <> b) <> c
+
+set = S.fromList <$> Gen.list (Range.linear 0 10) element
+element = Gen.choice (map pure ['a'..'z'])
