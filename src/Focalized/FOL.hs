f81dcccd41145c898adc1d4787a20555e3e5e909
Rob Rix <rob.rix@me.com>
2021-05-26 20:33:53 -0400

    Define the Judgement instance for FOL in terms of decomposeL/R.

--- src/Focalized/FOL.hs
+++ src/Focalized/FOL.hs
@@ -20,23 +20,23 @@ infixr 7 :\/:
 infixr 8 :/\:
 
 instance Judgement FOL FOL where
-  decompose (_Γ :|-: _Δ) = \case
-    Left  p -> case p of
-      F _      -> empty
-      Fls      -> pure ()
-      Tru      -> empty -- no L rule for truth
-      p :/\: q -> p <| q <| _Γ |- _Δ
-      p :\/: q -> p <| _Γ |- _Δ >> q <| _Γ |- _Δ
-      p :=>: q -> _Γ |- _Δ |> p >> q <| _Γ |- _Δ -- fixme: split _Γ & _Δ (multiplicative nondeterminism)
-      Not p    -> _Γ |- _Δ |> p
-    Right p -> case p of
-      F _      -> empty
-      Fls      -> empty -- no R rule for falsity
-      Tru      -> pure ()
-      p :/\: q -> _Γ |- _Δ |> p >> _Γ |- _Δ |> q -- fixme: split _Γ & _Δ (multiplicative nondeterminism)
-      p :\/: q -> (_Γ |- _Δ |> p) <|> (_Γ |- _Δ |> q)
-      p :=>: q -> p <| _Γ |- _Δ |> q
-      Not p    -> p <| _Γ |- _Δ
+  decomposeL p (_Γ :|-: _Δ) = case p of
+    F _      -> empty
+    Fls      -> pure ()
+    Tru      -> empty -- no L rule for truth
+    p :/\: q -> p <| q <| _Γ |- _Δ
+    p :\/: q -> p <| _Γ |- _Δ >> q <| _Γ |- _Δ
+    p :=>: q -> _Γ |- _Δ |> p >> q <| _Γ |- _Δ -- fixme: split _Γ & _Δ (multiplicative nondeterminism)
+    Not p    -> _Γ |- _Δ |> p
+
+  decomposeR (_Γ :|-: _Δ) = \case
+    F _      -> empty
+    Fls      -> empty -- no R rule for falsity
+    Tru      -> pure ()
+    p :/\: q -> _Γ |- _Δ |> p >> _Γ |- _Δ |> q -- fixme: split _Γ & _Δ (multiplicative nondeterminism)
+    p :\/: q -> (_Γ |- _Δ |> p) <|> (_Γ |- _Δ |> q)
+    p :=>: q -> p <| _Γ |- _Δ |> q
+    Not p    -> p <| _Γ |- _Δ
 
   unJudgementL = \case
     F a -> Left a
