4929606a4c477d763369d9b123c660def8219b93
Rob Rix <rob.rix@me.com>
2021-05-28 17:20:02 -0400

    Define focused proof search for polarized propositions.

--- src/Focalized/Polarized.hs
+++ src/Focalized/Polarized.hs
@@ -1,6 +1,10 @@
 module Focalized.Polarized
 ( Neg(..)
 , Pos(..)
+, inversion
+, neutral
+, focusL
+, focusR
 ) where
 
 import           Control.Applicative (Alternative(..))
@@ -114,3 +118,53 @@ instance Ord a => Sequent (Γ (Pos a)) (Δ (Neg a)) where
     where
     (aΓ, qΓ) = partitionEithers [ (, _Γ') <$> unPos p | (p, _Γ') <- S.quotients _Γ ]
     (aΔ, qΔ) = partitionEithers [ (, _Δ') <$> unNeg p | (p, _Δ') <- S.quotients _Δ ]
+
+
+inversion :: (Alternative m, Monad m, Ord a) => (Γ (Pos a), Γ (Either a (Neg a))) :|-: (Δ (Either (Pos a) a), Δ (Neg a)) -> m ()
+inversion ((iΓ, _Γ) :|-: (_Δ, iΔ)) = case (S.minView iΓ, S.minView iΔ) of
+  (Nothing,      Nothing)       -> neutral (_Γ :|-: _Δ)
+  (Just (p, iΓ), _)             -> case p of
+    P a      -> inversion ((iΓ, Left a <| _Γ) :|-: (_Δ, iΔ))
+    Zero     -> pure ()
+    One      -> inversion ((iΓ, _Γ) :|-: (_Δ, iΔ))
+    p :+: q  -> inversion ((p <| iΓ, _Γ) :|-: (_Δ, iΔ)) >> inversion ((q <| iΓ, _Γ) :|-: (_Δ, iΔ))
+    p :*: q  -> inversion ((p <| q <| iΓ, _Γ) :|-: (_Δ, iΔ))
+    p :-<: q -> inversion ((p <| iΓ, _Γ) :|-: (_Δ, iΔ |> q))
+    Inv p    -> inversion ((iΓ, _Γ) :|-: (_Δ |> Left p, iΔ))
+    Down p   -> inversion ((iΓ, Right p <| _Γ) :|-: (_Δ, iΔ))
+  (_,            Just (n, iΔ)) -> case n of
+    N a      -> inversion ((iΓ, _Γ) :|-: (_Δ |> Right a, iΔ))
+    Bot      -> inversion ((iΓ, _Γ) :|-: (_Δ, iΔ))
+    Top      -> pure ()
+    p :⅋: q  -> inversion ((iΓ, _Γ) :|-: (_Δ, iΔ |> p |> q))
+    p :&: q  -> inversion ((iΓ, _Γ) :|-: (_Δ, iΔ |> p)) >> inversion ((iΓ, _Γ) :|-: (_Δ, iΔ |> q))
+    p :->: q -> inversion ((p <| iΓ, _Γ) :|-: (_Δ, iΔ |> q))
+    Not p    -> inversion ((iΓ, Right p <| _Γ) :|-: (_Δ, iΔ))
+    Up p     -> inversion ((iΓ, _Γ) :|-: (_Δ |> Left p, iΔ))
+
+neutral :: (Alternative m, Monad m, Ord a) => Γ (Either a (Neg a)) :|-: Δ (Either (Pos a) a) -> m ()
+neutral (_Γ :|-: _Δ)
+  =   foldMapA (\ (p, _Γ') -> either (const empty) (\ n -> focusL ((n, _Γ') :|-: _Δ)) p) (S.quotients _Γ)
+  <|> foldMapA (\ (p, _Δ') -> either (\ p -> focusR (_Γ :|-: (_Δ', p))) (const empty) p) (S.quotients _Δ)
+
+focusL :: (Alternative m, Monad m, Ord a) => (Neg a, Γ (Either a (Neg a))) :|-: Δ (Either (Pos a) a) -> m ()
+focusL ((n, _Γ) :|-: _Δ) = case n of
+  N a      -> guard (Right a `elem` _Δ)
+  Bot      -> pure ()
+  Top      -> empty -- no left rule for ⊤
+  p :⅋: q  -> focusL ((p, _Γ) :|-: _Δ) <|> focusL ((q, _Γ) :|-: _Δ)
+  p :&: q  -> focusL ((p, _Γ) :|-: _Δ) >> focusL ((q, _Γ) :|-: _Δ)
+  p :->: q -> focusR (_Γ :|-: (_Δ, p)) >> focusL ((q, _Γ) :|-: _Δ)
+  Not p    -> inversion ((mempty, _Γ) :|-: (_Δ, S.singleton p))
+  Up p     -> inversion ((S.singleton p, _Γ) :|-: (_Δ, mempty))
+
+focusR :: (Alternative m, Monad m, Ord a) => Γ (Either a (Neg a)) :|-: (Δ (Either (Pos a) a), Pos a) -> m ()
+focusR (_Γ :|-: (_Δ, p)) = case p of
+  P a      -> guard (Left a `elem` _Γ)
+  Zero     -> empty -- no right rule for 0
+  One      -> pure ()
+  p :+: q  -> focusR (_Γ :|-: (_Δ, p)) <|> focusR (_Γ :|-: (_Δ, q))
+  p :*: q  -> focusR (_Γ :|-: (_Δ, p)) >> focusR (_Γ :|-: (_Δ, q))
+  p :-<: q -> focusR (_Γ :|-: (_Δ, p)) >> focusL ((q, _Γ) :|-: _Δ)
+  Inv p    -> inversion ((S.singleton p, _Γ) :|-: (_Δ, mempty))
+  Down p   -> inversion ((mempty, _Γ) :|-: (_Δ, S.singleton p))
