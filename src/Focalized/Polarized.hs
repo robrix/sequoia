992d33674d6b6555099583ba3f3bf1d343df2943
Rob Rix <rob.rix@me.com>
2021-05-29 10:23:24 -0400

    Go back to the simple definition of Neg & Pos.

--- src/Focalized/Polarized.hs
+++ src/Focalized/Polarized.hs
@@ -1,8 +1,6 @@
-{-# LANGUAGE GADTs #-}
 module Focalized.Polarized
-( N
-, P
-, Prop(..)
+( Neg(..)
+, Pos(..)
 , inversion
 , neutral
 , focusL
@@ -15,77 +13,69 @@ import           Control.Monad (ap)
 import qualified Focalized.Multiset as S
 import           Focalized.Proof
 
-data N
-data P
-
-data Prop polarity a where
-  N :: a -> Prop N a
-  Bot :: Prop N a
-  Top :: Prop N a
-  (:⅋:) :: Prop N a -> Prop N a -> Prop N a
-  (:&:) :: Prop N a -> Prop N a -> Prop N a
-  (:->:) :: Prop P a -> Prop N a -> Prop N a
-  Not :: Prop N a -> Prop N a
-  Up :: Prop P a -> Prop N a
-  P :: a -> Prop P a
-  Zero :: Prop P a
-  One :: Prop P a
-  (:+:) :: Prop P a -> Prop P a -> Prop P a
-  (:*:) :: Prop P a -> Prop P a -> Prop P a
-  (:-<:) :: Prop P a -> Prop N a -> Prop P a
-  Inv :: Prop P a -> Prop P a
-  Down :: Prop N a -> Prop P a
-
-deriving instance Eq a => Eq (Prop polaritty a)
-deriving instance Ord a => Ord (Prop polaritty a)
-deriving instance Show a => Show (Prop polaritty a)
-deriving instance Foldable (Prop polaritty)
-deriving instance Functor (Prop polaritty)
-deriving instance Traversable (Prop polaritty)
+data Neg a
+  = N a
+  | Bot
+  | Top
+  | Neg a :⅋: Neg a
+  | Neg a :&: Neg a
+  | Pos a :->: Neg a
+  | Not (Neg a)
+  | Up (Pos a)
+  deriving (Eq, Ord, Show, Foldable, Functor, Traversable)
 
 infixr 6 :->:
 infixr 7 :⅋:
 infixr 8 :&:
 
+instance Applicative Neg where
+  pure = N
+  (<*>) = ap
+
+instance Monad Neg where
+  m >>= f = case m of
+    N a      -> f a
+    Bot      -> Bot
+    Top      -> Top
+    a :⅋: b  -> (a >>= f) :⅋: (b >>= f)
+    a :&: b  -> (a >>= f) :&: (b >>= f)
+    a :->: b -> (a >>= Down . f) :->: (b >>= f)
+    Not a    -> Not (a >>= f)
+    Up a     -> Up (a >>= Down . f)
+
+
+data Pos a
+  = P a
+  | Zero
+  | One
+  | Pos a :+: Pos a
+  | Pos a :*: Pos a
+  | Pos a :-<: Neg a
+  | Inv (Pos a)
+  | Down (Neg a)
+  deriving (Eq, Ord, Show, Foldable, Functor, Traversable)
+
 infixr 6 :-<:
 infixr 7 :+:
 infixr 8 :*:
 
-instance Applicative (Prop N) where
-  pure = N
-  (<*>) = ap
-
-instance Monad (Prop N) where
-  (>>=) = bind
-
-instance Applicative (Prop P) where
+instance Applicative Pos where
   pure = P
   (<*>) = ap
 
-instance Monad (Prop P) where
-  (>>=) = bind
-
-bind :: Prop polarity a -> (a -> Prop polarity b) -> Prop polarity b
-bind m f = case m of
-  N a      -> f a
-  Bot      -> Bot
-  Top      -> Top
-  a :⅋: b  -> (a >>= f) :⅋: (b >>= f)
-  a :&: b  -> (a >>= f) :&: (b >>= f)
-  a :->: b -> (a >>= Down . f) :->: (b >>= f)
-  Not a    -> Not (a >>= f)
-  Up a     -> Up (a >>= Down . f)
-  P a      -> f a
-  Zero     -> Zero
-  One      -> One
-  a :+: b  -> (a >>= f) :+: (b >>= f)
-  a :*: b  -> (a >>= f) :*: (b >>= f)
-  a :-<: b -> (a >>= f) :-<: (b >>= Up . f)
-  Inv a    -> Inv (a >>= f)
-  Down a   -> Down (a >>= Up . f)
+instance Monad Pos where
+  m >>= f = case m of
+    P a      -> f a
+    Zero     -> Zero
+    One      -> One
+    a :+: b  -> (a >>= f) :+: (b >>= f)
+    a :*: b  -> (a >>= f) :*: (b >>= f)
+    a :-<: b -> (a >>= f) :-<: (b >>= Up . f)
+    Inv a    -> Inv (a >>= f)
+    Down a   -> Down (a >>= Up . f)
 
 
-inversion :: (Alternative m, Monad m, Ord a) => (Γ (Prop P a), Γ (Either a (Prop N a))) :|-: (Δ (Either (Prop P a) a), Δ (Prop N a)) -> m ()
+inversion :: (Alternative m, Monad m, Ord a) => (Γ (Pos a), Γ (Either a (Neg a))) :|-: (Δ (Either (Pos a) a), Δ (Neg a)) -> m ()
 inversion ((iΓ, _Γ) :|-: (_Δ, iΔ)) = case (S.minView iΓ, S.minView iΔ) of
   (Nothing,      Nothing)       -> neutral (_Γ :|-: _Δ)
   (Just (p, iΓ), _)             -> case p of
@@ -107,12 +97,12 @@ inversion ((iΓ, _Γ) :|-: (_Δ, iΔ)) = case (S.minView iΓ, S.minView iΔ) of
     Not p    -> inversion ((iΓ, Right p <| _Γ) :|-: (_Δ, iΔ))
     Up p     -> inversion ((iΓ, _Γ) :|-: (_Δ |> Left p, iΔ))
 
-neutral :: (Alternative m, Monad m, Ord a) => Γ (Either a (Prop N a)) :|-: Δ (Either (Prop P a) a) -> m ()
+neutral :: (Alternative m, Monad m, Ord a) => Γ (Either a (Neg a)) :|-: Δ (Either (Pos a) a) -> m ()
 neutral (_Γ :|-: _Δ)
   =   foldMapA (\ (p, _Γ') -> either (const empty) (\ n -> focusL ((n, _Γ') :|-: _Δ)) p) (S.quotients _Γ)
   <|> foldMapA (\ (p, _Δ') -> either (\ p -> focusR (_Γ :|-: (_Δ', p))) (const empty) p) (S.quotients _Δ)
 
-focusL :: (Alternative m, Monad m, Ord a) => (Prop N a, Γ (Either a (Prop N a))) :|-: Δ (Either (Prop P a) a) -> m ()
+focusL :: (Alternative m, Monad m, Ord a) => (Neg a, Γ (Either a (Neg a))) :|-: Δ (Either (Pos a) a) -> m ()
 focusL ((n, _Γ) :|-: _Δ) = case n of
   N a      -> guard (Right a `elem` _Δ)
   Bot      -> pure ()
@@ -123,7 +113,7 @@ focusL ((n, _Γ) :|-: _Δ) = case n of
   Not p    -> inversion ((mempty, _Γ) :|-: (_Δ, S.singleton p))
   Up p     -> inversion ((S.singleton p, _Γ) :|-: (_Δ, mempty))
 
-focusR :: (Alternative m, Monad m, Ord a) => Γ (Either a (Prop N a)) :|-: (Δ (Either (Prop P a) a), Prop P a) -> m ()
+focusR :: (Alternative m, Monad m, Ord a) => Γ (Either a (Neg a)) :|-: (Δ (Either (Pos a) a), Pos a) -> m ()
 focusR (_Γ :|-: (_Δ, p)) = case p of
   P a      -> guard (Left a `elem` _Γ)
   Zero     -> empty -- no right rule for 0
