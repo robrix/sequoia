ed4e75a29766641f85f09eca00a1c2186e20a168
Rob Rix <rob.rix@me.com>
2021-06-24 21:27:28 -0400

    foldMu takes a Fun.

--- src/Focalized/Calculus.hs
+++ src/Focalized/Calculus.hs
@@ -653,8 +653,8 @@ instance (Pos (f a), Neg a) => Polarized N (MuF r f a) where
 mu :: ForAll N (MuF r f) -> Mu r f
 mu r = Mu (getMuF (runForAll r))
 
-foldMu :: Neg a => Down (Fun r (f a) a) -> CPS r (Mu r f) a
-foldMu alg = liftCPS $ \ (Mu f) -> appFun f alg
+foldMu :: Neg a => Fun r (f a) a -> CPS r (Mu r f) a
+foldMu alg = liftCPS $ \ (Mu f) -> appFun f (Down alg)
 
 unfoldMu :: Traversable f => CPS r a (f a) -> CPS r a (Mu r f)
 unfoldMu coalg = cps $ \ a -> Mu $ liftFun' $ \ (Down (Fun alg)) -> appCPS (refoldCPS alg coalg) a
