47c3b2618571df0694211f0d1bcf03c9a7b47de1
Rob Rix <rob.rix@me.com>
2021-06-25 13:13:51 -0400

    Move K & CPS to their own module.

--- src/Focalized/Calculus.hs
+++ src/Focalized/Calculus.hs
@@ -98,33 +98,16 @@ module Focalized.Calculus
 , ShiftingN(..)
 , Down(..)
 , ShiftingP(..)
-  -- * Utilities
-, mapK
-, K(..)
-, cps
-, liftCPS
-, appCPS
-, pappCPS
-, execCPS
-, evalCPS
-, refoldCPS
-, resetCPS
-, shiftCPS
-, curryCPS
-, uncurryCPS
-, CPS(..)
-, CPST(..)
 ) where
 
 import           Control.Applicative (liftA2)
-import           Control.Arrow hiding ((>>>))
 import qualified Control.Category as Cat
 import           Control.Monad (ap, join)
 import           Control.Monad.Trans.Class
 import           Data.Functor.Identity
 import           Data.Kind (Constraint, Type)
 import           Data.Profunctor
-import           Data.Profunctor.Traversing
+import           Focalized.CPS
 import           Focalized.Connective
 import           Prelude hiding (init)
 
@@ -975,104 +958,6 @@ instance ShiftingP Seq where
 -- Utilities
 
 type ForAllC cx cf f = (forall x . cx x => cf (f x)) :: Constraint
-
-
-mapK :: (a' -> a) -> K r a -> K r a'
-mapK f (K g) = K (g . f)
-
-newtype K r a = K { runK :: a -> r }
-
-instance Cat.Category K where
-  id = K id
-  K f . K g = K (g . f)
-
-
-cps :: (a -> b) -> CPS r a b
-cps = CPS . flip (.)
-
-liftCPS :: (a -> (b -> r) -> r) -> CPS r a b
-liftCPS = CPS . flip
-
-appCPS :: CPS r a b -> a -> (b -> r) -> r
-appCPS c a k = runCPS c k a
-
-pappCPS :: CPS r a b -> a -> CPS r () b
-pappCPS c a = c Cat.<<< pure a
-
-execCPS :: CPS r () a -> (a -> r) -> r
-execCPS c = appCPS c ()
-
-evalCPS :: CPS r i r -> i -> r
-evalCPS c = runCPS c id
-
-refoldCPS :: Traversable f => CPS r (f b) b -> CPS r a (f a) -> CPS r a b
-refoldCPS f g = go where go = f Cat.<<< traverse' go Cat.<<< g
-
-resetCPS :: CPS o i o -> CPS r i o
-resetCPS c = CPS (. evalCPS c)
-
-shiftCPS :: ((o -> r) -> CPS r i r) -> CPS r i o
-shiftCPS f = CPS (evalCPS . f)
-
-curryCPS :: CPS r (a, b) c -> CPS r a (CPS r b c)
-curryCPS c = CPS (\ k -> k . (`lmap` c) . (,))
-
-uncurryCPS :: CPS r a (CPS r b c) -> CPS r (a, b) c
-uncurryCPS c = CPS (\ k -> uncurry (flip (runCPS c . flip (`runCPS` k))))
-
-newtype CPS r a b = CPS { runCPS :: (b -> r) -> (a -> r) }
-
-instance Cat.Category (CPS r) where
-  id = CPS id
-  CPS f . CPS g = CPS (g . f)
-
-instance Functor (CPS r a) where
-  fmap f (CPS r) = CPS (r . (. f))
-
-instance Applicative (CPS r a) where
-  pure a = CPS (const . ($ a))
-  (<*>) = ap
-
-instance Monad (CPS r a) where
-  r >>= f = CPS $ \ k a -> runCPS r (\ a' -> runCPS (f a') k a) a
-
-instance Arrow (CPS r) where
-  arr = cps
-  first  f = CPS (\ k (l, r) -> appCPS f l (k . (, r)))
-  second g = CPS (\ k (l, r) -> appCPS g r (k . (l,)))
-  f *** g = CPS (\ k (l, r) -> appCPS f l (appCPS g r . fmap k . (,)))
-  f &&& g = CPS (\ k a -> appCPS f a (appCPS g a . fmap k . (,)))
-
-instance ArrowChoice (CPS r) where
-  left  f = CPS (\ k -> exlr (runCPS f (k . inl)) (k . inr))
-  right g = CPS (\ k -> exlr (k . inl) (runCPS g (k . inr)))
-  f +++ g = CPS (\ k -> exlr (runCPS f (k . inl)) (runCPS g (k . inr)))
-  f ||| g = CPS (exlr <$> runCPS f <*> runCPS g)
-
-instance ArrowApply (CPS r) where
-  app = CPS (flip (uncurry appCPS))
-
-instance Profunctor (CPS r) where
-  dimap f g (CPS c) = CPS ((. f) . c . (. g))
-
-instance Strong (CPS r) where
-  first' = first
-  second' = second
-
-instance Choice (CPS r) where
-  left' = left
-  right' = right
-
-instance Traversing (CPS r) where
-  traverse' c = liftCPS (execCPS . traverse (pappCPS c))
-  wander traverse c = liftCPS (execCPS . traverse (pappCPS c))
-
-
-newtype CPST r i m o = CPST { getCPST :: CPS (m r) i o }
-  deriving (Applicative, Functor, Monad)
-
-instance MonadTrans (CPST r i) where
-  lift m = CPST (CPS (const . (m >>=)))
 
 
 newtype (f · g) a = C { getC :: f (g a) }
