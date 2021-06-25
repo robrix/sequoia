54d5b6eaabefd42ca3a7251622e2c6fcc079016e
Rob Rix <rob.rix@me.com>
2021-06-25 13:12:56 -0400

    Move Conj & Disj into their own module.

--- src/Focalized/Calculus.hs
+++ src/Focalized/Calculus.hs
@@ -114,10 +114,6 @@ module Focalized.Calculus
 , uncurryCPS
 , CPS(..)
 , CPST(..)
-, Conj(..)
-, curryConj
-, uncurryConj
-, Disj(..)
 ) where
 
 import           Control.Applicative (liftA2)
@@ -129,6 +125,7 @@ import           Data.Functor.Identity
 import           Data.Kind (Constraint, Type)
 import           Data.Profunctor
 import           Data.Profunctor.Traversing
+import           Focalized.Connective
 import           Prelude hiding (init)
 
 -- Sequents
@@ -1088,43 +1085,3 @@ instance Polarized p (f (g a)) => Polarized p ((f · g) a) where
 instance (Applicative f, Applicative g) => Applicative (f · g) where
   pure = C . pure . pure
   f <*> a = C ((<*>) <$> getC f <*> getC a)
-
-
-class Conj c where
-  inlr :: a -> b -> (a `c` b)
-  exl :: (a `c` b) -> a
-  exr :: (a `c` b) -> b
-
-instance Conj (,) where
-  inlr = (,)
-  exl = fst
-  exr = snd
-
-curryConj :: Conj p => ((a `p` b) -> r) -> (a -> b -> r)
-curryConj f = fmap f . inlr
-
-uncurryConj :: Conj p => (a -> b -> r) -> ((a `p` b) -> r)
-uncurryConj f = f <$> exl <*> exr
-
-foldMapConj :: Conj p => (b -> m) -> (a `p` b) -> m
-foldMapConj f = f . exr
-
-traverseConj :: (Conj p, Applicative m) => (b -> m b') -> (a `p` b) -> m (a `p` b')
-traverseConj f c = inlr (exl c) <$> f (exr c)
-
-
-class Disj d where
-  inl :: a -> (a `d` b)
-  inr :: b -> (a `d` b)
-  exlr :: (a -> r) -> (b -> r) -> ((a `d` b) -> r)
-
-instance Disj Either where
-  inl = Left
-  inr = Right
-  exlr = either
-
-foldMapDisj :: (Disj p, Monoid m) => (b -> m) -> (a `p` b) -> m
-foldMapDisj = exlr (const mempty)
-
-traverseDisj :: (Disj p, Applicative m) => (b -> m b') -> (a `p` b) -> m (a `p` b')
-traverseDisj f = exlr (pure . inl) (fmap inr . f)
--- src/Focalized/Connective.hs
+++ src/Focalized/Connective.hs
@@ -1,2 +1,51 @@
 module Focalized.Connective
-() where
+( -- * Disjunction
+  Disj(..)
+, foldMapDisj
+, traverseDisj
+  -- * Conjunction
+, Conj(..)
+, curryConj
+, uncurryConj
+, foldMapConj
+, traverseConj
+) where
+
+class Disj d where
+  inl :: a -> (a `d` b)
+  inr :: b -> (a `d` b)
+  exlr :: (a -> r) -> (b -> r) -> ((a `d` b) -> r)
+
+instance Disj Either where
+  inl = Left
+  inr = Right
+  exlr = either
+
+foldMapDisj :: (Disj p, Monoid m) => (b -> m) -> (a `p` b) -> m
+foldMapDisj = exlr (const mempty)
+
+traverseDisj :: (Disj p, Applicative m) => (b -> m b') -> (a `p` b) -> m (a `p` b')
+traverseDisj f = exlr (pure . inl) (fmap inr . f)
+
+
+class Conj c where
+  inlr :: a -> b -> (a `c` b)
+  exl :: (a `c` b) -> a
+  exr :: (a `c` b) -> b
+
+instance Conj (,) where
+  inlr = (,)
+  exl = fst
+  exr = snd
+
+curryConj :: Conj p => ((a `p` b) -> r) -> (a -> b -> r)
+curryConj f = fmap f . inlr
+
+uncurryConj :: Conj p => (a -> b -> r) -> ((a `p` b) -> r)
+uncurryConj f = f <$> exl <*> exr
+
+foldMapConj :: Conj p => (b -> m) -> (a `p` b) -> m
+foldMapConj f = f . exr
+
+traverseConj :: (Conj p, Applicative m) => (b -> m b') -> (a `p` b) -> m (a `p` b')
+traverseConj f c = inlr (exl c) <$> f (exr c)
