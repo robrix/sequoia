aacebbe0e7375d3c00a4db7b0d796ac4f08e9728
Rob Rix <rob.rix@me.com>
2021-06-27 16:46:08 -0400

    Pull the continuation conversions out.

--- src/Focalized/Calculus/Negation.hs
+++ src/Focalized/Calculus/Negation.hs
@@ -10,6 +10,10 @@ module Focalized.Calculus.Negation
 , type (¬-)
   -- * Negative negation
 , NegNegation(..)
+, notLK
+, notRK
+, notLK'
+, notRK'
   -- * Negate
 , type (-)(..)
 , negateNot
@@ -17,6 +21,10 @@ module Focalized.Calculus.Negation
 , type (-¬)
   -- * Positive negation
 , PosNegation(..)
+, negateLK
+, negateRK
+, negateLK'
+, negateRK'
 ) where
 
 import Focalized.CPS
@@ -41,48 +49,24 @@ class (Core s, Structural s, Contextual s, Control s) => NegNegation s where
     -- ----------------------
     -> r ¬a < i -|s r|- o
   notL = notLK . kL
-  notLK
-    :: Pos a
-    =>  r •a < i -|s r|- o
-    -- -------------------
-    ->  r ¬a < i -|s r|- o
-  notLK = mapL getNot
   notL'
     :: Pos a
     => r ¬a < i -|s r|- o
     -- ----------------------
     ->        i -|s r|- o > a
   notL' p = notR init >>> wkR p
-  notLK'
-    :: Pos a
-    =>  r ¬a < i -|s r|- o
-    -- --------------------
-    ->  r •a < i -|s r|- o
-  notLK' = mapL Not
   notR
     :: Pos a
     => a < i -|s r|- o
     -- ----------------------
     ->     i -|s r|- o > r ¬a
   notR = notRK . kR
-  notRK
-    :: Pos a
-    => i -|s r|- o > r •a
-    -- ------------------
-    -> i -|s r|- o > r ¬a
-  notRK = mapR Not
   notR'
     :: Pos a
     =>     i -|s r|- o > r ¬a
     -- ----------------------
     -> a < i -|s r|- o
   notR' p = wkL p >>> notL init
-  notRK'
-    :: Pos a
-    => i -|s r|- o > r ¬a
-    -- ------------------
-    -> i -|s r|- o > r •a
-  notRK' = mapR getNot
   shiftP
     :: Pos a
     => r ¬a < i -|s r|- o > r
@@ -134,6 +118,36 @@ class (Core s, Structural s, Contextual s, Control s) => NegNegation s where
   dneNRK = mapR notNegate
 
 
+notLK
+  :: Contextual s
+  =>  r •a < i -|s r|- o
+  -- -------------------
+  ->  r ¬a < i -|s r|- o
+notLK = mapL getNot
+
+notRK
+  :: Contextual s
+  => i -|s r|- o > r •a
+  -- ------------------
+  -> i -|s r|- o > r ¬a
+notRK = mapR Not
+
+
+notLK'
+  :: Contextual s
+  =>  r ¬a < i -|s r|- o
+  -- --------------------
+  ->  r •a < i -|s r|- o
+notLK' = mapL Not
+
+notRK'
+  :: Contextual s
+  => i -|s r|- o > r ¬a
+  -- ------------------
+  -> i -|s r|- o > r •a
+notRK' = mapR getNot
+
+
 -- Positive negation
 
 class (Core s, Structural s, Control s, Contextual s) => PosNegation s where
@@ -143,18 +157,6 @@ class (Core s, Structural s, Control s, Contextual s) => PosNegation s where
     -- ----------------------
     -> r -a < i -|s r|- o
   negateL = negateLK . kL
-  negateLK
-    :: Neg a
-    => r •a < i -|s r|- o
-    -- ------------------
-    -> r -a < i -|s r|- o
-  negateLK = mapL getNegate
-  negateLK'
-    :: Neg a
-    => r -a < i -|s r|- o
-    -- ------------------
-    -> r •a < i -|s r|- o
-  negateLK' = mapL Negate
   negateL'
     :: Neg a
     => r -a < i -|s r|- o
@@ -167,24 +169,12 @@ class (Core s, Structural s, Control s, Contextual s) => PosNegation s where
     -- ----------------------
     ->     i -|s r|- o > r -a
   negateR = negateRK . kR
-  negateRK
-    :: Neg a
-    => i -|s r|- o > r •a
-    -- ------------------
-    -> i -|s r|- o > r -a
-  negateRK = mapR Negate
   negateR'
     :: Neg a
     =>     i -|s r|- o > r -a
     -- ----------------------
     -> a < i -|s r|- o
   negateR' p = wkL p >>> negateL init
-  negateRK'
-    :: Neg a
-    => i -|s r|- o > r -a
-    -- ------------------
-    -> i -|s r|- o > r •a
-  negateRK' = mapR getNegate
   shiftN
     :: Neg a
     => r -a < i -|s r|- o > r
@@ -234,3 +224,33 @@ class (Core s, Structural s, Control s, Contextual s) => PosNegation s where
     -- -------------------
     -> i -|s r|- o > r -¬a
   dnePRK = mapR negateNot
+
+
+negateLK
+  :: Contextual s
+  => r •a < i -|s r|- o
+  -- ------------------
+  -> r -a < i -|s r|- o
+negateLK = mapL getNegate
+
+negateRK
+  :: Contextual s
+  => i -|s r|- o > r •a
+  -- ------------------
+  -> i -|s r|- o > r -a
+negateRK = mapR Negate
+
+
+negateLK'
+  :: Contextual s
+  => r -a < i -|s r|- o
+  -- ------------------
+  -> r •a < i -|s r|- o
+negateLK' = mapL Negate
+
+negateRK'
+  :: Contextual s
+  => i -|s r|- o > r -a
+  -- ------------------
+  -> i -|s r|- o > r •a
+negateRK' = mapR getNegate
