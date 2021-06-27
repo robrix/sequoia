83cba3fa6bd8c1315387cafb8bdbd5563b706d5c
Rob Rix <rob.rix@me.com>
2021-06-27 18:28:04 -0400

    Pull almost all of the rules out of Contextual.

--- src/Focalized/Calculus/Core.hs
+++ src/Focalized/Calculus/Core.hs
@@ -12,6 +12,20 @@ module Focalized.Calculus.Core
 , Exchange(..)
   -- * Contextual
 , Contextual(..)
+, poppedL
+, poppedR
+, popL2
+, popR2
+, pushL2
+, pushR2
+, mapL
+, mapR
+-- , mapL2
+, mapR2
+, liftL
+, liftR
+, lowerL
+, lowerR
 , Contextually(..)
 ) where
 
@@ -110,8 +124,16 @@ class Core s => Contextual s where
   -- @
   popL :: (a -> s r i o) -> s r (a < i) o
 
-  poppedL :: (s r i o -> s r i' o') -> (s r (a < i) o -> s r (a < i') o')
-  poppedL f p = popL (f . pushL p)
+  -- | Pop something off the output context which can later be pushed. Used with 'pushR', this provides a generalized context restructuring facility.
+  --
+  -- @
+  -- popR . pushR = id
+  -- @
+  -- @
+  -- pushR . popR = id
+  -- @
+  popR :: (r •a -> s r i o) -> s r i (o > a)
+
 
   -- | Push something onto the input context which was previously popped off it. Used with 'popL', this provides a generalized context restructuring facility. It i undefined what will happen if you push something which was not previously popped.
   --
@@ -123,39 +145,6 @@ class Core s => Contextual s where
   -- @
   pushL :: s r (a < i) o -> (a -> s r i o)
 
-  popL2 :: (a -> b -> s r i o) -> s r (a < b < i) o
-  popL2 f = popL (popL . f)
-
-  pushL2 :: s r (a < b < i) o -> a -> b -> s r i o
-  pushL2 p = pushL . pushL p
-
-  mapL :: (a' -> a) -> s r (a < i) o -> s r (a' < i) o
-  mapL f p = popL (pushL p . f)
-
-  -- FIXME: this is clearly possible, tho it’s less clear that it’s a good idea.
-  -- mapL2 :: (c -> (b, a)) -> s r (a < i) o -> s r (b < i) o -> s r (c < i) o
-  -- mapL2 f a b = popL (pushL b . exl . f) <> popL (pushL a . exr . f)
-
-  liftL :: r •a -> s r (a < i) o
-  liftL = pushR init
-
-  lowerL :: (r •a -> s r i o) -> (s r (a < i) o -> s r i o)
-  lowerL k p = popR k >>> p
-
-
-  -- | Pop something off the output context which can later be pushed. Used with 'pushR', this provides a generalized context restructuring facility.
-  --
-  -- @
-  -- popR . pushR = id
-  -- @
-  -- @
-  -- pushR . popR = id
-  -- @
-  popR :: (r •a -> s r i o) -> s r i (o > a)
-
-  poppedR :: (s r i o -> s r i' o') -> (s r i (o > a) -> s r i' (o' > a))
-  poppedR f p = popR (f . pushR p)
-
   -- | Push something onto the output context which was previously popped off it. Used with 'popR', this provides a generalized context restructuring facility. It i undefined what will happen if you push something which was not previously popped.
   --
   -- @
@@ -166,24 +155,56 @@ class Core s => Contextual s where
   -- @
   pushR :: s r i (o > a) -> (r •a -> s r i o)
 
-  popR2 :: (r •a -> r •b -> s r i o) -> s r i (o > b > a)
-  popR2 f = popR (popR . f)
 
-  pushR2 :: s r i (o > b > a) -> r •a -> r •b -> s r i o
-  pushR2 p = pushR . pushR p
+poppedL :: Contextual s => (s r i o -> s r i' o') -> (s r (a < i) o -> s r (a < i') o')
+poppedL f p = popL (f . pushL p)
 
-  mapR :: (a -> a') -> s r i (o > a) -> s r i (o > a')
-  mapR f p = popR (pushR p . contramap f)
+poppedR :: Contextual s => (s r i o -> s r i' o') -> (s r i (o > a) -> s r i' (o' > a))
+poppedR f p = popR (f . pushR p)
 
-  mapR2 :: (a -> b -> c) -> s r i (o > a) -> s r i (o > b) -> s r i (o > c)
-  default mapR2 :: (Weaken s, Exchange s) => (a -> b -> c) -> s r i (o > a) -> s r i (o > b) -> s r i (o > c)
-  mapR2 f a b = popR (pushR (wkR' a) . contramap f) >>> popL (\ f -> popR (pushR b . contramap f))
 
-  liftR :: a -> s r i (o > a)
-  liftR = pushL init
+popL2 :: Contextual s => (a -> b -> s r i o) -> s r (a < b < i) o
+popL2 f = popL (popL . f)
 
-  lowerR :: (a -> s r i o) -> (s r i (o > a) -> s r i o)
-  lowerR k p = p >>> popL k
+popR2 :: Contextual s => (r •a -> r •b -> s r i o) -> s r i (o > b > a)
+popR2 f = popR (popR . f)
+
+
+pushL2 :: Contextual s => s r (a < b < i) o -> a -> b -> s r i o
+pushL2 p = pushL . pushL p
+
+pushR2 :: Contextual s => s r i (o > b > a) -> r •a -> r •b -> s r i o
+pushR2 p = pushR . pushR p
+
+
+mapL :: Contextual s => (a' -> a) -> s r (a < i) o -> s r (a' < i) o
+mapL f p = popL (pushL p . f)
+
+mapR :: Contextual s => (a -> a') -> s r i (o > a) -> s r i (o > a')
+mapR f p = popR (pushR p . contramap f)
+
+
+-- FIXME: this is clearly possible, tho it’s less clear that it’s a good idea.
+-- mapL2 :: (c -> (b, a)) -> s r (a < i) o -> s r (b < i) o -> s r (c < i) o
+-- mapL2 f a b = popL (pushL b . exl . f) <> popL (pushL a . exr . f)
+
+mapR2 :: Contextual s => (a -> b -> c) -> s r i (o > a) -> s r i (o > b) -> s r i (o > c)
+mapR2 f a b = mapR f (wkR' a) >>> popL (`mapR` b)
+  where wkR' = popR2 . flip . const . pushR
+
+
+liftL :: Contextual s => r •a -> s r (a < i) o
+liftL = pushR init
+
+liftR :: Contextual s => a -> s r i (o > a)
+liftR = pushL init
+
+
+lowerL :: Contextual s => (r •a -> s r i o) -> (s r (a < i) o -> s r i o)
+lowerL k p = popR k >>> p
+
+lowerR :: Contextual s => (a -> s r i o) -> (s r i (o > a) -> s r i o)
+lowerR k p = p >>> popL k
 
 
 newtype Contextually s r i o = Contextually { getContextually :: s r i o }
