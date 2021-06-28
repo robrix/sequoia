cf757b843614f2afd9bad9ef5f41388c03e78938
Rob Rix <rob.rix@me.com>
2021-06-28 10:36:36 -0400

    :fire: multisets.

--- test/Test.hs
+++ test/Test.hs
@@ -2,10 +2,8 @@ module Main
 ( main
 ) where
 
-import qualified Focalized.Multiset.Test
-import           Hedgehog.Main
+import Hedgehog.Main
 
 main :: IO ()
 main = defaultMain
-  [ Focalized.Multiset.Test.tests
-  ]
+  []
