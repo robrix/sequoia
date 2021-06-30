036be73fac5d4f76007ce2ca1003fcd1c791b412
Rob Rix <rob.rix@me.com>
2021-06-29 23:20:25 -0400

    Give a definition of popΓΔ.

--- src/Focalized/Sequent.hs
+++ src/Focalized/Sequent.hs
@@ -91,6 +91,7 @@ deriving via Contextually Seq instance Exchange Seq
 instance Contextual Seq where
   popΓ f = sequent $ K . \ k _Γ -> runSeq (f _Γ) k • Γ
   popΔ f = sequent (K . evalSeq . f)
+  popΓΔ f = sequent (K . fmap (`evalSeq` Γ) . f)
 
   pushΓ s _Γ = sequent $ K . \ k -> (runSeq s k •) . const _Γ
   pushΔ s _Δ = sequent $ K . const (runSeq s (K (_Δ •)) •)
