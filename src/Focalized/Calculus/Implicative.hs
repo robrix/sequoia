38cf0cb0e7423e7197a9f074c95909110f77c4ae
Rob Rix <rob.rix@me.com>
2021-07-01 17:57:50 -0400

    Eliminate sub with fun.

--- src/Focalized/Calculus/Implicative.hs
+++ src/Focalized/Calculus/Implicative.hs
@@ -3,6 +3,7 @@ module Focalized.Calculus.Implicative
 ( -- * Implicative rules
   ImplicativeIntro
 , funLSub
+, subLFun
   -- * Re-exports
 , module Focalized.Calculus.Function
 , module Focalized.Calculus.Subtraction
@@ -22,3 +23,9 @@ funLSub
   =>             _Γ -|s r|- _Δ > a ~-r-< b
   -> a ~~r~> b < _Γ -|s r|- _Δ
 funLSub s = wkL s >>> subL (exL (funL init init))
+
+subLFun
+  :: (Weaken s, Exchange s, FunctionIntro s, SubtractionIntro s, Pos a, Neg b)
+  =>             _Γ -|s r|- _Δ > a ~~r~> b
+  -> a ~-r-< b < _Γ -|s r|- _Δ
+subLFun s = wkL s >>> funL (subL (wkR init)) (exL (subL (wkL init)))
