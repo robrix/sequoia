21d3d7c739c3c115068eb23a25f0b0409f4027a8
Rob Rix <rob.rix@me.com>
2021-07-01 17:48:14 -0400

    Eliminate par with tensor.

--- src/Focalized/Calculus/Multiplicative.hs
+++ src/Focalized/Calculus/Multiplicative.hs
@@ -2,6 +2,7 @@
 module Focalized.Calculus.Multiplicative
 ( -- * Multiplicative rules
   MultiplicativeIntro
+, parLTensor
   -- * Re-exports
 , module Focalized.Calculus.Bottom
 , module Focalized.Calculus.One
@@ -10,8 +11,20 @@ module Focalized.Calculus.Multiplicative
 ) where
 
 import Focalized.Calculus.Bottom
+import Focalized.Calculus.Context
+import Focalized.Calculus.Core
+import Focalized.Calculus.Negate
 import Focalized.Calculus.One
 import Focalized.Calculus.Par
 import Focalized.Calculus.Tensor
+import Focalized.Polarity
+import Prelude hiding (init)
 
 type MultiplicativeIntro s = (BottomIntro s, OneIntro s, ParIntro s, TensorIntro s)
+
+
+parLTensor
+  :: (Weaken s, ParIntro s, TensorIntro s, NegateIntro s, Neg a, Neg b)
+  =>         _Γ -|s r|- _Δ > r -a ⊗ r -b
+  -> a ⅋ b < _Γ -|s r|- _Δ
+parLTensor s = wkL s >>> tensorL (negateL (negateL (parL (wkR init) init)))
