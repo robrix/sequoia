59761dd6e7d27e8e07a8141e2dc968f8e773ce9a
Rob Rix <rob.rix@me.com>
2021-07-01 17:35:49 -0400

    Eliminate with using sum.

--- src/Focalized/Calculus/Additive.hs
+++ src/Focalized/Calculus/Additive.hs
@@ -2,6 +2,7 @@
 module Focalized.Calculus.Additive
 ( -- * Additive rules
   AdditiveIntro
+, withLSum
   -- * Re-exports
 , module Focalized.Calculus.Top
 , module Focalized.Calculus.Zero
@@ -9,9 +10,20 @@ module Focalized.Calculus.Additive
 , module Focalized.Calculus.Sum
 ) where
 
+import Focalized.Calculus.Context
+import Focalized.Calculus.Core
+import Focalized.Calculus.Negate
 import Focalized.Calculus.Sum
 import Focalized.Calculus.Top
 import Focalized.Calculus.With
 import Focalized.Calculus.Zero
+import Focalized.Polarity
+import Prelude hiding (init)
 
 type AdditiveIntro s = (TopIntro s, ZeroIntro s, WithIntro s, SumIntro s)
+
+withLSum
+  :: (Weaken s, SumIntro s, WithIntro s, NegateIntro s, Neg a, Neg b)
+  =>         _Γ -|s r|- _Δ > r -a ⊕ r -b
+  -> a & b < _Γ -|s r|- _Δ
+withLSum s = wkL s >>> sumL (negateL (withL1 init)) (negateL (withL2 init))
