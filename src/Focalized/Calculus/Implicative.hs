85efcc83b303c35783fa022e5d0e10a97caf2645
Rob Rix <rob.rix@me.com>
2021-07-01 17:55:18 -0400

    Eliminate fun with sub.

--- src/Focalized/Calculus/Implicative.hs
+++ src/Focalized/Calculus/Implicative.hs
@@ -2,12 +2,23 @@
 module Focalized.Calculus.Implicative
 ( -- * Implicative rules
   ImplicativeIntro
+, funLSub
   -- * Re-exports
 , module Focalized.Calculus.Function
 , module Focalized.Calculus.Subtraction
 ) where
 
+import Focalized.Calculus.Context
+import Focalized.Calculus.Core
 import Focalized.Calculus.Function
 import Focalized.Calculus.Subtraction
+import Focalized.Polarity
+import Prelude hiding (init)
 
 type ImplicativeIntro s = (FunctionIntro s, SubtractionIntro s)
+
+funLSub
+  :: (Weaken s, Exchange s, FunctionIntro s, SubtractionIntro s, Pos a, Neg b)
+  =>             _Γ -|s r|- _Δ > a ~-r-< b
+  -> a ~~r~> b < _Γ -|s r|- _Δ
+funLSub s = wkL s >>> subL (exL (funL init init))
