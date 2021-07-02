1411fae71d4cca2693f3c5b49d5dec6a15066112
Rob Rix <rob.rix@me.com>
2021-07-01 23:46:15 -0400

    Invert iffR.

--- src/Focalized/Calculus/Iff.hs
+++ src/Focalized/Calculus/Iff.hs
@@ -1,13 +1,17 @@
 module Focalized.Calculus.Iff
 ( -- * Logical biconditional
   IffIntro(..)
+, iffR1'
+, iffR2'
   -- * Connectives
 , module Focalized.Connective.Iff
 ) where
 
 import Focalized.Calculus.Context
+import Focalized.Calculus.Core
 import Focalized.Connective.Iff
 import Focalized.Polarity
+import Prelude hiding (init)
 
 -- * Logical biconditional
 
@@ -29,3 +33,18 @@ class IffIntro s where
     => a < _Γ -|s r|- _Δ > b   ->   b < _Γ -|s r|- _Δ > a
     -- --------------------------------------------------
     ->             _Γ -|s r|- _Δ > a <~r~> b
+
+
+iffR1'
+  :: (Weaken s, Exchange s, IffIntro s, Neg a, Neg b)
+  =>     _Γ -|s r|- _Δ > a <~r~> b
+  -- --------------------------------------------------
+  -> a < _Γ -|s r|- _Δ > b
+iffR1' s = wkL (wkR' s) >>> iffL1 init init
+
+iffR2'
+  :: (Weaken s, Exchange s, IffIntro s, Neg a, Neg b)
+  =>     _Γ -|s r|- _Δ > a <~r~> b
+  -- --------------------------------------------------
+  -> b < _Γ -|s r|- _Δ > a
+iffR2' s = wkL (wkR' s) >>> iffL2 init init
