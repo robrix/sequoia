0bf5f78d3c97bc1fe741fe7ee306e0cb7c93ff09
Rob Rix <rob.rix@me.com>
2021-07-01 21:25:05 -0400

    Polarize Iff.

--- src/Focalized/Connective/Iff.hs
+++ src/Focalized/Connective/Iff.hs
@@ -7,12 +7,15 @@ module Focalized.Connective.Iff
 
 import Focalized.CPS
 import Focalized.Function (type (~>))
+import Focalized.Polarity
 
 -- Logical biconditional
 
 data Iff r a b
   = IffT a b
   | IffF (r •a) (r •b)
+
+instance (Neg a, Neg b) => Polarized N (Iff r a b)
 
 type a <~r = Iff r a
 
