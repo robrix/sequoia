0a30b608c1a2fe8a787a1118a9ebab3c9fbc6902
Rob Rix <rob.rix@me.com>
2021-07-02 14:59:41 -0400

    Define a bunch of lifted projections out of conjunctions.

--- src/Focalized/Conjunction.hs
+++ src/Focalized/Conjunction.hs
@@ -9,11 +9,22 @@ module Focalized.Conjunction
 , bifoldMapConj
 , bimapConj
 , bitraverseConj
+  -- * Lifted projections
+, exlC
+, exrC
+, exlF
+, exrF
+, exlL
+, exrL
+, exlR
+, exrR
 ) where
 
 -- Conjunction
 
 import Control.Applicative (liftA2)
+import Data.Functor.Contravariant
+import Data.Profunctor
 
 class Conj c where
   (-><-) :: a -> b -> (a `c` b)
@@ -49,3 +60,26 @@ bimapConj = exlrC (-><-)
 
 bitraverseConj :: (Conj p, Applicative m) => (a -> m a') -> (b -> m b') -> (a `p` b -> m (a' `p` b'))
 bitraverseConj = exlrC (liftA2 (-><-))
+
+
+-- Lifted projections
+
+exlC :: (Contravariant f, Conj c) => f a -> f (a `c` b)
+exrC :: (Contravariant f, Conj c) => f b -> f (a `c` b)
+exlC = contramap exl
+exrC = contramap exr
+
+exlF :: (Functor f, Conj c) => f (a `c` b) -> f a
+exrF :: (Functor f, Conj c) => f (a `c` b) -> f b
+exlF = fmap exl
+exrF = fmap exr
+
+exlL :: (Profunctor p, Conj c) => p a r -> p (a `c` b) r
+exrL :: (Profunctor p, Conj c) => p b r -> p (a `c` b) r
+exlL = lmap exl
+exrL = lmap exr
+
+exlR :: (Profunctor p, Conj c) => p l (a `c` b) -> p l a
+exrR :: (Profunctor p, Conj c) => p l (a `c` b) -> p l b
+exlR = rmap exl
+exrR = rmap exr
