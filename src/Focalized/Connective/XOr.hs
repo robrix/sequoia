516650b2c7b510805b2eaca3802d75fff2bdf38b
Rob Rix <rob.rix@me.com>
2021-07-01 20:57:24 -0400

    Define an XOr datatype.

--- src/Focalized/Connective/XOr.hs
+++ src/Focalized/Connective/XOr.hs
@@ -1,2 +1,12 @@
 module Focalized.Connective.XOr
-() where
+( -- * Exclusive disjunction
+  XOr(..)
+) where
+
+import Focalized.CPS
+
+-- Exclusive disjunction
+
+data XOr r a b
+  = XOrL a (r •b)
+  | XOrR b (r •a)
