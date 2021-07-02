99fb35919850f31921a20ffdcae53a9f60a97543
Rob Rix <rob.rix@me.com>
2021-07-02 15:03:22 -0400

    Define a bunch of lifted injections into disjunctions.

--- src/Focalized/Disjunction.hs
+++ src/Focalized/Disjunction.hs
@@ -8,7 +8,19 @@ module Focalized.Disjunction
 , bifoldMapDisj
 , bimapDisj
 , bitraverseDisj
+  -- * Lifted injections
+, inlC
+, inrC
+, inlF
+, inrF
+, inlL
+, inrL
+, inlR
+, inrR
 ) where
+
+import Data.Functor.Contravariant
+import Data.Profunctor
 
 -- Disjunction
 
@@ -43,3 +55,26 @@ bimapDisj f g = inl . f <--> inr . g
 
 bitraverseDisj :: (Disj p, Functor m) => (a -> m a') -> (b -> m b') -> (a `p` b -> m (a' `p` b'))
 bitraverseDisj f g = fmap inl . f <--> fmap inr . g
+
+
+-- Lifted injections
+
+inlC :: (Contravariant f, Disj d) => f (a `d` b) -> f a
+inrC :: (Contravariant f, Disj d) => f (a `d` b) -> f b
+inlC = contramap inl
+inrC = contramap inr
+
+inlF :: (Functor f, Disj d) => f a -> f (a `d` b)
+inrF :: (Functor f, Disj d) => f b -> f (a `d` b)
+inlF = fmap inl
+inrF = fmap inr
+
+inlL :: (Profunctor p, Disj d) => p (a `d` b) r -> p a r
+inrL :: (Profunctor p, Disj d) => p (a `d` b) r -> p b r
+inlL = lmap inl
+inrL = lmap inr
+
+inlR :: (Profunctor p, Disj d) => p l a -> p l (a `d` b)
+inrR :: (Profunctor p, Disj d) => p l b -> p l (a `d` b)
+inlR = rmap inl
+inrR = rmap inr
