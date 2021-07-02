ca49114022bd4aaf11c8f1fdb5dd5b1f68fe14de
Rob Rix <rob.rix@me.com>
2021-07-02 15:42:34 -0400

    Add spacing stuff.

--- src/Focalized/Printer.hs
+++ src/Focalized/Printer.hs
@@ -11,6 +11,7 @@ module Focalized.Printer
 , Print(..)
 ) where
 
+import Control.Applicative (liftA2)
 import Data.Functor.Contravariant
 import Data.Monoid (Endo(..))
 
@@ -66,11 +67,22 @@ class Monoid p => Print p where
   lparen = char '('
   rparen = char ')'
 
+  space :: p
+  space = char ' '
+
+  (<+>) :: p -> p -> p
+  (<+>) = surround space
+
+  infixr 6 <+>
+
   surround :: p -> p -> p -> p
-  surround l r x = l <> x <> r
+  surround x l r = enclose l r x
+
+  enclose :: p -> p -> p -> p
+  enclose l r x = l <> x <> r
 
   parens :: p -> p
-  parens = surround lparen rparen
+  parens = enclose lparen rparen
 
 instance Print b => Print (a -> b) where
   char   = pure . char
@@ -78,7 +90,11 @@ instance Print b => Print (a -> b) where
 
   lparen = pure lparen
   rparen = pure rparen
+  space = pure space
 
-  surround l r x = surround <$> l <*> r <*> x
+  (<+>) = liftA2 (<+>)
+
+  surround x l r = enclose <$> x <*> l <*> r
+  enclose l r x = enclose <$> l <*> r <*> x
 
   parens f = parens <$> f
