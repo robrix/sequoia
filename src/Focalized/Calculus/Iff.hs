32723c894490f38c921cfca02dba79e7ea92adec
Rob Rix <rob.rix@me.com>
2021-07-01 23:03:52 -0400

    Polarize the iff rules.

--- src/Focalized/Calculus/Iff.hs
+++ src/Focalized/Calculus/Iff.hs
@@ -7,21 +7,25 @@ module Focalized.Calculus.Iff
 
 import Focalized.Calculus.Context
 import Focalized.Connective.Iff
+import Focalized.Polarity
 
 -- * Logical biconditional
 
 class IffIntro s where
   iffL1
-    :: _Γ -|s r|- _Δ > a   ->   b < _Γ -|s r|- _Δ
+    :: (Neg a, Neg b)
+    => _Γ -|s r|- _Δ > a   ->   b < _Γ -|s r|- _Δ
     -- ------------------------------------------
     ->         a <~r~> b < _Γ -|s r|- _Δ
 
   iffL2
-    :: _Γ -|s r|- _Δ > b   ->   a < _Γ -|s r|- _Δ
+    :: (Neg a, Neg b)
+    => _Γ -|s r|- _Δ > b   ->   a < _Γ -|s r|- _Δ
     -- ------------------------------------------
     ->         a <~r~> b < _Γ -|s r|- _Δ
 
   iffR
-    :: a < _Γ -|s r|- _Δ > b   ->   b < _Γ -|s r|- _Δ > a
+    :: (Neg a, Neg b)
+    => a < _Γ -|s r|- _Δ > b   ->   b < _Γ -|s r|- _Δ > a
     -- --------------------------------------------------
     ->             _Γ -|s r|- _Δ > a <~r~> b
