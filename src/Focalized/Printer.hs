1af931fa81c7100fe83b2f5c5740e2bd5d371a46
Rob Rix <rob.rix@me.com>
2021-07-02 14:41:46 -0400

    Define more printing infra.

--- src/Focalized/Printer.hs
+++ src/Focalized/Printer.hs
@@ -1,13 +1,47 @@
 -- | Pretty-printing.
 module Focalized.Printer
 ( Printer(..)
+, prec
+, atom
+, withPrec
 , Prec(..)
 , PrecPrinter(..)
+, Print(..)
 ) where
 
 newtype Printer a = Printer { runPrinter :: a -> ShowS }
 
 
+prec :: Print (p a) => Prec -> p a -> PrecPrinter p a
+prec i pr = PrecPrinter $ \ i' -> parensIf (i' > i) pr
+
+atom :: p a -> PrecPrinter p a
+atom = PrecPrinter . const
+
+withPrec :: Prec -> PrecPrinter p a -> p a
+withPrec = flip runPrecPrinter
+
 newtype Prec = Prec { getPrec :: Int }
+  deriving (Eq, Ord, Show)
 
 newtype PrecPrinter p a = PrecPrinter { runPrecPrinter :: Prec -> p a }
+
+
+parensIf :: Print p => Bool -> p -> p
+parensIf True  = parens
+parensIf False = id
+
+class Monoid p => Print p where
+  char :: Char -> p
+  string :: String -> p
+  string = foldMap char
+
+  lparen, rparen :: p
+  lparen = char '('
+  rparen = char ')'
+
+  surround :: p -> p -> p -> p
+  surround l r x = l <> x <> r
+
+  parens :: p -> p
+  parens = surround lparen rparen
