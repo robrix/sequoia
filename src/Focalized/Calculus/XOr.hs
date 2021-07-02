c51c9a53d0910cbed65fd621898374b0068fb785
Rob Rix <rob.rix@me.com>
2021-07-01 21:59:41 -0400

    Define the intro rules for XOr.

--- src/Focalized/Calculus/XOr.hs
+++ src/Focalized/Calculus/XOr.hs
@@ -1,6 +1,27 @@
 module Focalized.Calculus.XOr
-( -- * Connectives
-  module Focalized.Connective.XOr
+( -- * Exclusive disjunction
+  XOrIntro(..)
+  -- * Connectives
+, module Focalized.Connective.XOr
 ) where
 
+import Focalized.Calculus.Context
 import Focalized.Connective.XOr
+
+-- Exclusive disjunction
+
+class XOrIntro s where
+  xorL
+    :: a < _Γ -|s r|- _Δ > b   ->   b < _Γ -|s r|- _Δ > a
+    -- --------------------------------------------------
+    ->             a </r/> b < _Γ -|s r|- _Δ
+
+  xorR1
+    :: _Γ -|s r|- _Δ > a   ->   b < _Γ -|s r|- _Δ
+    -- ------------------------------------------
+    ->         _Γ -|s r|- _Δ > a </r/> b
+
+  xorR2
+    :: _Γ -|s r|- _Δ > b   ->   a < _Γ -|s r|- _Δ
+    -- ------------------------------------------
+    ->         _Γ -|s r|- _Δ > a </r/> b
