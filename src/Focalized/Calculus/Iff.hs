e9c7a05319d652d9f58de964807dfefb5bebfb90
Rob Rix <rob.rix@me.com>
2021-07-01 22:09:19 -0400

    Define the intro rules for Iff.

--- src/Focalized/Calculus/Iff.hs
+++ src/Focalized/Calculus/Iff.hs
@@ -1,6 +1,27 @@
 module Focalized.Calculus.Iff
-( -- * Connectives
-  module Focalized.Connective.Iff
+( -- * Logical biconditional
+  IffIntro(..)
+  -- * Connectives
+, module Focalized.Connective.Iff
 ) where
 
+import Focalized.Calculus.Context
 import Focalized.Connective.Iff
+
+-- * Logical biconditional
+
+class IffIntro s where
+  iffL1
+    :: _Γ -|s r|- _Δ > a   ->   b < _Γ -|s r|- _Δ
+    -- ------------------------------------------
+    ->         a <~r~> b < _Γ -|s r|- _Δ
+
+  iffL2
+    :: _Γ -|s r|- _Δ > a   ->   b < _Γ -|s r|- _Δ
+    -- ------------------------------------------
+    ->         a <~r~> b < _Γ -|s r|- _Δ
+
+  iffR
+    :: a < _Γ -|s r|- _Δ > b   ->   b < _Γ -|s r|- _Δ > a
+    -- --------------------------------------------------
+    ->             _Γ -|s r|- _Δ > a <~r~> b
