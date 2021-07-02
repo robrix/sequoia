fba74937e127ff2b7fc265a5255361bf8e0d8276
Rob Rix <rob.rix@me.com>
2021-07-01 23:49:39 -0400

    Invert xorL.

--- src/Focalized/Calculus/XOr.hs
+++ src/Focalized/Calculus/XOr.hs
@@ -1,13 +1,17 @@
 module Focalized.Calculus.XOr
 ( -- * Exclusive disjunction
   XOrIntro(..)
+, xorL1'
+, xorL2'
   -- * Connectives
 , module Focalized.Connective.XOr
 ) where
 
 import Focalized.Calculus.Context
+import Focalized.Calculus.Core
 import Focalized.Connective.XOr
 import Focalized.Polarity
+import Prelude hiding (init)
 
 -- Exclusive disjunction
 
@@ -29,3 +33,17 @@ class XOrIntro s where
     => _Γ -|s r|- _Δ > b   ->   a < _Γ -|s r|- _Δ
     -- ------------------------------------------
     ->         _Γ -|s r|- _Δ > a </r/> b
+
+xorL1'
+  :: (Weaken s, Exchange s, XOrIntro s, Pos a, Pos b)
+  => a </r/> b < _Γ -|s r|- _Δ
+  -- -----------------------------
+  ->         a < _Γ -|s r|- _Δ > b
+xorL1' s = xorR1 init init >>> wkR (wkL' s)
+
+xorL2'
+  :: (Weaken s, Exchange s, XOrIntro s, Pos a, Pos b)
+  => a </r/> b < _Γ -|s r|- _Δ
+  -- -----------------------------
+  ->         b < _Γ -|s r|- _Δ > a
+xorL2' s = xorR2 init init >>> wkR (wkL' s)
