40e0d42000b3297da37184291e7885c2b4e81224
Rob Rix <rob.rix@me.com>
2021-07-03 17:46:02 -0400

    Define Applicative & Monad instances for I.

--- src/Focalized/Functor/I.hs
+++ src/Focalized/Functor/I.hs
@@ -3,5 +3,16 @@ module Focalized.Functor.I
   I(..)
 ) where
 
+import Control.Applicative (liftA2)
+import Data.Coerce
+
 newtype I a = I { getI :: a }
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+instance Applicative I where
+  pure = coerce
+  liftA2 = coerce
+  (<*>) = coerce
+
+instance Monad I where
+  (>>=) = flip coerce
