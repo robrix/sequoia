451e916bfa0577e923263f42fa95a9e14a768813
Rob Rix <rob.rix@me.com>
2021-07-03 22:17:53 -0400

    Define a Representable instance for compositions.

--- src/Focalized/Functor/C.hs
+++ src/Focalized/Functor/C.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
 module Focalized.Functor.C
 ( type(·)(..)
 ) where
 
 import Data.Distributive
+import Data.Functor.Rep
 import Focalized.Polarity
 
 newtype (f · g) a = C { getC :: f (g a) }
@@ -22,3 +24,8 @@ instance (Applicative f, Applicative g) => Applicative (f · g) where
 instance (Distributive f, Distributive g) => Distributive (f · g) where
   distribute r = C (distribute <$> distribute (getC <$> r))
   collect f r = C (distribute <$> distribute (getC . f <$> r))
+
+instance (Representable f, Representable g) => Representable (f · g) where
+  type Rep (f · g) = (Rep f, Rep g)
+  tabulate = C . tabulate . fmap tabulate . curry
+  index    = uncurry . fmap index . index . getC
