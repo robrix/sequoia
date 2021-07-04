7e7920e70cd800eaff4b319af25dac63cc5a2e2f
Rob Rix <rob.rix@me.com>
2021-07-04 15:12:05 -0400

    Define a Functor instance for Sig.

--- src/Sequoia/Signal.hs
+++ src/Sequoia/Signal.hs
@@ -68,6 +68,9 @@ instance Cat.Category (Sig k) where
   id = Sig id
   (.) = dimap2 runSig runSig Sig (flip (.))
 
+instance Contravariant k => Functor (Sig k a) where
+  fmap f = Sig . lmap (contramap f) . runSig
+
 mapKSig :: (forall x . k x <-> k' x) -> (Sig k a b -> Sig k' a b)
 mapKSig b = Sig . (~> dimapping b b) . runSig
 
