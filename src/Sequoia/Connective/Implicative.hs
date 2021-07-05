0aa40f881a0cecf29b9807d09286ddf55f78be7e
Rob Rix <rob.rix@me.com>
2021-07-05 18:37:37 -0400

    Convert A → B ←→ ¬A ⅋ B under single negation.

--- src/Sequoia/Connective/Implicative.hs
+++ src/Sequoia/Connective/Implicative.hs
@@ -1,13 +1,23 @@
 module Sequoia.Connective.Implicative
 ( elimFun
+, funPar1
   -- * Connectives
 , module Sequoia.Connective.Function
 , module Sequoia.Connective.Subtraction
 ) where
 
+import Sequoia.Bijection
 import Sequoia.Connective.Function
+import Sequoia.Connective.Not
+import Sequoia.Connective.Par
 import Sequoia.Connective.Subtraction
 import Sequoia.Continuation
+import Sequoia.Disjunction
 
 elimFun :: Representable k => a ~~k~> b -> a ~-k-< b -> Rep k
 elimFun f = (•) <$> appFun f . subA <*> subK
+
+funPar1 :: Representable k => k (k ¬a ⅋ b) <-> k (a ~~k~> b)
+funPar1
+  =   inK1 (\ k f -> k (inl (inK (\ a -> appFun f a • inK (k . inr)))))
+  <-> inK1 (\ k p -> k (Fun (inK1 (\ b a -> ((• a) <--> b) p))))
