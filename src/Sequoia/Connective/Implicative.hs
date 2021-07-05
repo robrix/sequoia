c64fda8259da03b35046b2a249143812e2a2716b
Rob Rix <rob.rix@me.com>
2021-07-05 11:09:21 -0400

    Eliminate fun with sub.

--- src/Sequoia/Connective/Implicative.hs
+++ src/Sequoia/Connective/Implicative.hs
@@ -1,8 +1,14 @@
 module Sequoia.Connective.Implicative
-( -- * Connectives
-  module Sequoia.Connective.Function
+( elimFun
+  -- * Connectives
+, module Sequoia.Connective.Function
 , module Sequoia.Connective.Subtraction
 ) where
 
+import Sequoia.Conjunction
 import Sequoia.Connective.Function
 import Sequoia.Connective.Subtraction
+import Sequoia.Continuation
+
+elimFun :: Representable k => a ~~k~> b -> a ~-k-< b -> Rep k
+elimFun f (Sub s) = appFun f (exl s) • coerceK (exr s)
