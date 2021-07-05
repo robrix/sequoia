8b231c4630f6dbfe8cf841506a57d68f9db013cc
Rob Rix <rob.rix@me.com>
2021-07-05 11:30:46 -0400

    Tacit.

--- src/Sequoia/Connective/Implicative.hs
+++ src/Sequoia/Connective/Implicative.hs
@@ -10,4 +10,4 @@ import Sequoia.Connective.Subtraction
 import Sequoia.Continuation
 
 elimFun :: Representable k => a ~~k~> b -> a ~-k-< b -> Rep k
-elimFun f s = appFun f (subA s) • subK s
+elimFun f = (•) <$> appFun f . subA <*> subK
