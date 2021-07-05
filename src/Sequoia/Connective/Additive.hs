1437ced880cc0fd5c3da626ce77d41fa2a60f361
Rob Rix <rob.rix@me.com>
2021-07-04 23:38:29 -0400

    Re-export the connectives.

--- src/Sequoia/Connective/Additive.hs
+++ src/Sequoia/Connective/Additive.hs
@@ -1,2 +1,12 @@
 module Sequoia.Connective.Additive
-() where
+( -- * Connectives
+  module Sequoia.Connective.Sum
+, module Sequoia.Connective.Top
+, module Sequoia.Connective.With
+, module Sequoia.Connective.Zero
+) where
+
+import Sequoia.Connective.Sum
+import Sequoia.Connective.Top
+import Sequoia.Connective.With
+import Sequoia.Connective.Zero
