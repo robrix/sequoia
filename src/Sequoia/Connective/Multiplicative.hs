b725f484f007426b1aed4a5b6dbbe2bde9f4dde1
Rob Rix <rob.rix@me.com>
2021-07-05 09:48:52 -0400

    Eliminate par with tensor.

--- src/Sequoia/Connective/Multiplicative.hs
+++ src/Sequoia/Connective/Multiplicative.hs
@@ -1,12 +1,20 @@
 module Sequoia.Connective.Multiplicative
-( -- * Connectives
-  module Sequoia.Connective.Bottom
+( elimPar
+  -- * Connectives
+, module Sequoia.Connective.Bottom
 , module Sequoia.Connective.One
 , module Sequoia.Connective.Par
 , module Sequoia.Connective.Tensor
 ) where
 
+import Sequoia.Conjunction
 import Sequoia.Connective.Bottom
+import Sequoia.Connective.Negation
 import Sequoia.Connective.One
 import Sequoia.Connective.Par
 import Sequoia.Connective.Tensor
+import Sequoia.Continuation
+import Sequoia.Disjunction
+
+elimPar :: Representable k => a ⅋ b -> k -a ⊗ k -b -> Rep k
+elimPar = (. exl) . flip (•) <--> (. exr) . flip (•)
