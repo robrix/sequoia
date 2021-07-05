63204e79fcb664c70833cb41346695f4e47c1b04
Rob Rix <rob.rix@me.com>
2021-07-05 09:42:34 -0400

    Re-export the multiplicative connectives.

--- src/Sequoia/Connective/Multiplicative.hs
+++ src/Sequoia/Connective/Multiplicative.hs
@@ -1,2 +1,12 @@
 module Sequoia.Connective.Multiplicative
-() where
+( -- * Connectives
+  module Sequoia.Connective.Bottom
+, module Sequoia.Connective.One
+, module Sequoia.Connective.Par
+, module Sequoia.Connective.Tensor
+) where
+
+import Sequoia.Connective.Bottom
+import Sequoia.Connective.One
+import Sequoia.Connective.Par
+import Sequoia.Connective.Tensor
