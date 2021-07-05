d6d66a055305300aa61b41fafc4ba118ff3f0f3b
Rob Rix <rob.rix@me.com>
2021-07-05 09:14:11 -0400

    Eliminate with using sum.

--- src/Sequoia/Connective/Additive.hs
+++ src/Sequoia/Connective/Additive.hs
@@ -1,12 +1,21 @@
 module Sequoia.Connective.Additive
-( -- * Connectives
-  module Sequoia.Connective.Sum
+( -- * Duals
+  elimWith
+  -- * Connectives
+, module Sequoia.Connective.Sum
 , module Sequoia.Connective.Top
 , module Sequoia.Connective.With
 , module Sequoia.Connective.Zero
 ) where
 
+import Sequoia.Conjunction
+import Sequoia.Connective.Negation
 import Sequoia.Connective.Sum
 import Sequoia.Connective.Top
 import Sequoia.Connective.With
 import Sequoia.Connective.Zero
+import Sequoia.Continuation
+import Sequoia.Disjunction
+
+elimWith :: Representable k => a & b -> k -a ⊕ k -b -> Rep k
+elimWith = flip ((. exl) . (•) <--> (. exr) . (•))
