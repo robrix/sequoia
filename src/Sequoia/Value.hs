35bfc1732be42306415b7e6980fe35e96828c42e
Rob Rix <rob.rix@me.com>
2021-07-06 14:00:32 -0400

    Define wrappers for tabulate/index.

--- src/Sequoia/Value.hs
+++ src/Sequoia/Value.hs
@@ -1,6 +1,9 @@
 module Sequoia.Value
-( Value
+( -- * Values
+  Value
 , VRep
+, inV
+, exV
 ) where
 
 import Data.Functor.Rep
@@ -8,3 +11,10 @@ import Data.Functor.Rep
 class Representable v => Value v
 
 type VRep v = Rep v
+
+
+inV :: Value v => (VRep v -> a) -> v a
+inV = tabulate
+
+exV :: Value v => v a -> (VRep v -> a)
+exV = index
