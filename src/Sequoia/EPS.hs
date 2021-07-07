bf9fb15a183a0f1a90d22c3262ccbb7eb21f040f
Rob Rix <rob.rix@me.com>
2021-07-07 06:40:38 -0400

    Define a class of env-passing types.

--- src/Sequoia/EPS.hs
+++ src/Sequoia/EPS.hs
@@ -1,8 +1,18 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module Sequoia.EPS
 ( -- * EPS
   EPFn
+, EnvPassing(..)
 ) where
+
+import qualified Control.Category as Cat
+import           Data.Profunctor
+import           Sequoia.Value
 
 -- EPS
 
 type EPFn v a b = v a -> v b
+
+class (Cat.Category e, Value v, Profunctor e) => EnvPassing v e | e -> v where
+  inE :: EPFn v a b -> a `e` b
+  exE :: a `e` b    -> EPFn v a b
