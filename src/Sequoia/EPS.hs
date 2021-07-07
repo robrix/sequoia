7d1d4b1c925bbd4b874a12b957a8d336d7b8344d
Rob Rix <rob.rix@me.com>
2021-07-07 06:43:51 -0400

    Define a bijection out of and into EPS.

--- src/Sequoia/EPS.hs
+++ src/Sequoia/EPS.hs
@@ -3,10 +3,12 @@ module Sequoia.EPS
 ( -- * EPS
   EPFn
 , EnvPassing(..)
+, _E
 ) where
 
 import qualified Control.Category as Cat
 import           Data.Profunctor
+import           Sequoia.Bijection
 import           Sequoia.Value
 
 -- EPS
@@ -16,3 +18,7 @@ type EPFn v a b = v a -> v b
 class (Cat.Category e, Value v, Profunctor e) => EnvPassing v e | e -> v where
   inE :: EPFn v a b -> a `e` b
   exE :: a `e` b    -> EPFn v a b
+
+
+_E :: (EnvPassing v e, EnvPassing v' e') => Optic Iso (e a b) (e' a' b') (EPFn v a b) (EPFn v' a' b')
+_E = exE <-> inE
