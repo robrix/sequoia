eac0755356f008214cc73f8f5b5c603911cdb7cc
Rob Rix <rob.rix@me.com>
2021-07-08 18:01:55 -0400

    Derive some more instances for V.

--- src/Sequoia/Functor/V.hs
+++ src/Sequoia/Functor/V.hs
@@ -3,13 +3,22 @@ module Sequoia.Functor.V
 ( V(..)
 ) where
 
-import Data.Distributive
-import Data.Functor.Rep
-import Data.Profunctor
+import           Data.Distributive
+import           Data.Functor.Identity
+import           Data.Functor.Rep
+import           Data.Profunctor
+import qualified Data.Profunctor.Rep as Pro
+import           Data.Profunctor.Sieve
 
 newtype V s a = V { runV :: s -> a }
-  deriving (Applicative, Choice, Closed, Cochoice, Costrong, Functor, Monad, Monoid, Profunctor, Representable, Semigroup, Strong)
+  deriving (Applicative, Choice, Closed, Cochoice, Costrong, Functor, Monad, Monoid, Profunctor, Representable, Pro.Representable, Semigroup, Strong)
 
 instance Distributive (V s) where
   distribute = distributeRep
   collect = collectRep
+
+instance Sieve V Identity where
+  sieve = sieve . runV
+
+instance Cosieve V Identity where
+  cosieve = cosieve . runV
