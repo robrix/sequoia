4e7ba544e4486472ac7cd846851ba11ab0742b31
Rob Rix <rob.rix@me.com>
2021-07-08 17:51:02 -0400

    Derive a bunch of instances.

--- src/Sequoia/Functor/V.hs
+++ src/Sequoia/Functor/V.hs
@@ -2,4 +2,7 @@ module Sequoia.Functor.V
 ( V(..)
 ) where
 
+import Data.Profunctor
+
 newtype V s a = V { runV :: s -> a }
+  deriving (Applicative, Choice, Closed, Cochoice, Costrong, Functor, Monad, Monoid, Profunctor, Semigroup, Strong)
