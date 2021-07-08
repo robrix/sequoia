305ab4e2290c76ebc6ed6809599ec2fbb2813040
Rob Rix <rob.rix@me.com>
2021-07-08 06:32:38 -0400

    Derive a bunch of instances.

--- src/Sequoia/Functor/CorepRep.hs
+++ src/Sequoia/Functor/CorepRep.hs
@@ -3,4 +3,7 @@ module Sequoia.Functor.CorepRep
 ( CorepRep(..)
 ) where
 
+import Data.Profunctor
+
 newtype CorepRep p s a = CorepRep { getCorepRep :: p s a }
+  deriving (Choice, Closed, Cochoice, Costrong, Functor, Profunctor, Strong)
