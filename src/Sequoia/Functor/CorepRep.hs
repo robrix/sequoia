0c77b6eda3422d473e64cc305eadca755e8f0b42
Rob Rix <rob.rix@me.com>
2021-07-08 06:33:18 -0400

    Define a Cosieve instance.

--- src/Sequoia/Functor/CorepRep.hs
+++ src/Sequoia/Functor/CorepRep.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE UndecidableInstances #-}
 -- | 'Representable' functors from 'Corepresentable' 'Profunctor's.
 module Sequoia.Functor.CorepRep
 ( CorepRep(..)
 ) where
 
 import Data.Profunctor
+import Data.Profunctor.Sieve
 
 newtype CorepRep p s a = CorepRep { getCorepRep :: p s a }
   deriving (Choice, Closed, Cochoice, Costrong, Functor, Profunctor, Strong)
+
+instance Cosieve p r => Cosieve (CorepRep p) r where
+  cosieve = cosieve . getCorepRep
