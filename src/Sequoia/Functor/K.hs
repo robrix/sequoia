07f858284782275036f3082f7ed14c3b054b0b3b
Rob Rix <rob.rix@me.com>
2021-07-08 17:58:56 -0400

    Derive many instances.

--- src/Sequoia/Functor/K.hs
+++ src/Sequoia/Functor/K.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Sequoia.Functor.K
 ( K(..)
 ) where
 
+import Data.Functor.Contravariant
+import Data.Functor.Identity
+import Sequoia.Confunctor
+
 newtype K r a = K { runK :: a -> r }
+  deriving (Monoid, Semigroup)
+  deriving (Contravariant) via Flip (->) r
+  deriving (Confunctor, Contrachoice, Contraclosed, Contracochoice, Contracosieve Identity, Contracostrong, Contracorepresentable, Contrarepresentable, Contrasieve Identity, Contrastrong) via Flip (->)
