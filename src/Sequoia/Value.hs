7758d48a3dd5d6d761c13d452271d76ab022e8e7
Rob Rix <rob.rix@me.com>
2021-07-08 17:36:40 -0400

    Generalize the Value API over the contravariant type.

--- src/Sequoia/Value.hs
+++ src/Sequoia/Value.hs
@@ -32,28 +32,28 @@ type VRep v = Corep v
 type VFn v s a = VRep v s -> a
 
 
-_V :: (Value v, Value v') => Optic Iso (v () a) (v' () a') (VFn v () a) (VFn v' () a')
+_V :: (Value v, Value v') => Optic Iso (v s a) (v' s' a') (VFn v s a) (VFn v' s' a')
 _V = exV <-> inV
 
-inV0 :: Value v => a -> v () a
+inV0 :: Value v => a -> v s a
 inV0 = inV . const
 
-inV :: Value v => VFn v () a -> v () a
+inV :: Value v => VFn v s a -> v s a
 inV = cotabulate
 
-inV1 :: Value v => (VFn v () a -> VFn v () b) -> (v () a -> v () b)
+inV1 :: Value v => (VFn v s a -> VFn v s b) -> (v s a -> v s b)
 inV1 = under _V
 
-inV2 :: Value v => (VFn v () a -> VFn v () b -> VFn v () c) -> (v () a -> v () b -> v () c)
+inV2 :: Value v => (VFn v s a -> VFn v s b -> VFn v s c) -> (v s a -> v s b -> v s c)
 inV2 = dimap2 exV exV inV
 
-exV :: Value v => v () a -> VFn v () a
+exV :: Value v => v s a -> VFn v s a
 exV = cosieve
 
-exV1 :: Value v => (v () a -> v () b) -> (VFn v () a -> VFn v () b)
+exV1 :: Value v => (v s a -> v s b) -> (VFn v s a -> VFn v s b)
 exV1 = over _V
 
-exV2 :: Value v => (v () a -> v () b -> v () c) -> (VFn v () a -> VFn v () b -> VFn v () c)
+exV2 :: Value v => (v s a -> v s b -> v s c) -> (VFn v s a -> VFn v s b -> VFn v s c)
 exV2 = dimap2 inV inV exV
 
 
