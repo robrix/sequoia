a68dea4efac93556ea44aad15647d34d70241f6b
Rob Rix <rob.rix@me.com>
2021-07-08 18:35:28 -0400

    Derive a Representable instance for the cont functor.

--- src/Sequoia/Functor/K.hs
+++ src/Sequoia/Functor/K.hs
@@ -4,10 +4,11 @@ module Sequoia.Functor.K
 ) where
 
 import Data.Functor.Contravariant
+import Data.Functor.Contravariant.Rep
 import Data.Functor.Identity
 import Sequoia.Confunctor
 
 newtype K r a = K { runK :: a -> r }
   deriving (Monoid, Semigroup)
-  deriving (Contravariant) via Flip (->) r
+  deriving (Contravariant, Representable) via Flip (->) r
   deriving (Confunctor, Contrachoice, Contraclosed, Contracochoice, Contracosieve Identity, Contracostrong, Contracorepresentable, Contrarepresentable, Contrasieve Identity, Contrastrong) via Flip (->)
