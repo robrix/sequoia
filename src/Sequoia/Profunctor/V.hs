4edae9ffc25aa7460218e9e4cc7670960fec2614
Rob Rix <rob.rix@me.com>
2021-07-07 21:29:15 -0400

    Define a Distributive instance for V.

--- src/Sequoia/Profunctor/V.hs
+++ src/Sequoia/Profunctor/V.hs
@@ -3,6 +3,7 @@ module Sequoia.Profunctor.V
 ( V(..)
 ) where
 
+import           Data.Distributive
 import           Data.Functor.Const
 import           Data.Profunctor
 import qualified Data.Profunctor.Rep as Pro
@@ -10,6 +11,10 @@ import           Data.Profunctor.Sieve
 
 newtype V s a b = V { runV :: s -> b }
   deriving (Functor)
+
+instance Distributive (V s a) where
+  distribute r = V (\ s -> (`runV` s)     <$> r)
+  collect  f r = V (\ s -> (`runV` s) . f <$> r)
 
 instance Profunctor (V s) where
   dimap _ g = V . rmap g . runV
