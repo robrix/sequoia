fae3ecb4e3ca1537ede6ce51880e734d2583b7c7
Rob Rix <rob.rix@me.com>
2021-07-07 20:35:49 -0400

    Define a Profunctor instance for V.

--- src/Sequoia/Profunctor/V.hs
+++ src/Sequoia/Profunctor/V.hs
@@ -2,5 +2,10 @@ module Sequoia.Profunctor.V
 ( V(..)
 ) where
 
+import Data.Profunctor
+
 newtype V s a b = V { runV :: s -> b }
   deriving (Functor)
+
+instance Profunctor (V s) where
+  dimap _ g = V . rmap g . runV
