32abfcdf29a491e17fa6c413ededa18380f3f980
Rob Rix <rob.rix@me.com>
2021-07-08 15:08:41 -0400

    Give default definitions.

--- src/Sequoia/Confunctor.hs
+++ src/Sequoia/Confunctor.hs
@@ -77,7 +77,9 @@ class Confunctor p => Contracostrong p where
 
 class Confunctor p => Contrachoice p where
   conleft  :: p a b -> p (Either a c) (Either b c)
+  conleft = conmap (either Right Left) (either Right Left) . conright
   conright :: p a b -> p (Either c a) (Either c b)
+  conright = conmap (either Right Left) (either Right Left) . conleft
 
 
 -- Deriving
