d2fc6c5617ce4768ea153a5036e8c4e7da2f09ec
Rob Rix <rob.rix@me.com>
2021-07-08 17:51:53 -0400

    Derive Distributive/Representable instances.

--- src/Sequoia/Functor/V.hs
+++ src/Sequoia/Functor/V.hs
@@ -1,8 +1,15 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Sequoia.Functor.V
 ( V(..)
 ) where
 
+import Data.Distributive
+import Data.Functor.Rep
 import Data.Profunctor
 
 newtype V s a = V { runV :: s -> a }
-  deriving (Applicative, Choice, Closed, Cochoice, Costrong, Functor, Monad, Monoid, Profunctor, Semigroup, Strong)
+  deriving (Applicative, Choice, Closed, Cochoice, Costrong, Functor, Monad, Monoid, Profunctor, Representable, Semigroup, Strong)
+
+instance Distributive (V s) where
+  distribute = distributeRep
+  collect = collectRep
