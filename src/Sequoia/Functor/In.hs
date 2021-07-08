52e3315595a16533e4d39c93cb11475daf4e0ba4
Rob Rix <rob.rix@me.com>
2021-07-08 07:19:51 -0400

    Define a contravariant instance for In.

--- src/Sequoia/Functor/In.hs
+++ src/Sequoia/Functor/In.hs
@@ -3,4 +3,10 @@ module Sequoia.Functor.In
 ( In(..)
 ) where
 
+import Data.Functor.Contravariant
+import Data.Profunctor
+
 newtype In p r a = In { runIn :: p a r }
+
+instance Profunctor p => Contravariant (In p r) where
+  contramap f = In . lmap f . runIn
