4b3c2afcb1fa417e5bf2bf99f867ade03c64dbaf
Rob Rix <rob.rix@me.com>
2021-07-10 08:14:08 -0400

    Define a Continuation instance for KV.

--- src/Sequoia/Profunctor/D.hs
+++ src/Sequoia/Profunctor/D.hs
@@ -7,9 +7,9 @@ module Sequoia.Profunctor.D
 import qualified Control.Category as Cat
 import           Control.Monad ((<=<))
 import           Data.Functor.Contravariant
-import           Data.Functor.Contravariant.Rep
 import           Data.Profunctor
 import           Sequoia.Bijection
+import           Sequoia.Continuation
 
 _KV :: Optic Iso (KV s r a) (KV s' r' a') (a -> s -> r) (a' -> s' -> r')
 _KV = runKV <-> KV
@@ -27,3 +27,5 @@ instance Representable (KV s r) where
   type Rep (KV s r) = s -> r
   tabulate = KV
   index = runKV
+
+instance Continuation (KV s r)
