9a99ed52fd66ee3a18de6b02cb9feaac4a4e2b2f
Rob Rix <rob.rix@me.com>
2021-07-10 13:23:50 -0400

    Give minimality.

--- src/Sequoia/Profunctor/Adjunction.hs
+++ src/Sequoia/Profunctor/Adjunction.hs
@@ -6,6 +6,8 @@ module Sequoia.Profunctor.Adjunction
 import Data.Profunctor
 
 class (Profunctor f, Profunctor u) => Adjunction f u | f -> u, u -> f where
+  {-# MINIMAL (leftUnit | leftAdjunct), (rightUnit | rightAdjunct) #-}
+
   leftUnit :: a -> u b (f b a)
   rightUnit :: f b (u b a) -> a
 
