4bceee9d1332003ef5e55f79cccfe791afca9daa
Rob Rix <rob.rix@me.com>
2021-07-10 13:28:13 -0400

    u is Representable.

--- src/Sequoia/Profunctor/Adjunction.hs
+++ src/Sequoia/Profunctor/Adjunction.hs
@@ -3,9 +3,10 @@ module Sequoia.Profunctor.Adjunction
 ( Adjunction(..)
 ) where
 
-import Data.Profunctor
+import           Data.Profunctor
+import qualified Data.Profunctor.Rep as Pro
 
-class (Profunctor f, Profunctor u) => Adjunction f u | f -> u, u -> f where
+class (Profunctor f, Pro.Representable u) => Adjunction f u | f -> u, u -> f where
   {-# MINIMAL (leftUnit | leftAdjunct), (rightUnit | rightAdjunct) #-}
 
   leftUnit  :: a -> u b (f b a)
