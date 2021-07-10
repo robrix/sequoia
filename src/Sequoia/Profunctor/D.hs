faa8fa34d81f7457922ce78b9b9d61ee4add7583
Rob Rix <rob.rix@me.com>
2021-07-10 08:10:38 -0400

    Define an optic for KV.

--- src/Sequoia/Profunctor/D.hs
+++ src/Sequoia/Profunctor/D.hs
@@ -1,5 +1,11 @@
 module Sequoia.Profunctor.D
-( KV(..)
+( _KV
+, KV(..)
 ) where
+
+import Sequoia.Bijection
+
+_KV :: Optic Iso (KV s r a) (KV s' r' a') (a -> s -> r) (a' -> s' -> r')
+_KV = runKV <-> KV
 
 newtype KV s r a = KV { runKV :: a -> s -> r }
