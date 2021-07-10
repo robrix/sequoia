87232eb6dc70f6628d0886ddad3e9e472abc21c7
Rob Rix <rob.rix@me.com>
2021-07-10 13:23:38 -0400

    Give default definitions for the adjuncts.

--- src/Sequoia/Profunctor/Adjunction.hs
+++ src/Sequoia/Profunctor/Adjunction.hs
@@ -14,3 +14,6 @@ class (Profunctor f, Profunctor u) => Adjunction f u | f -> u, u -> f where
 
   leftAdjunct  :: (f b a -> c) -> (a -> u b c)
   rightAdjunct :: (c -> u a b) -> (f a c -> b)
+
+  leftAdjunct  f = rmap f . leftUnit
+  rightAdjunct f = rightUnit . rmap f
