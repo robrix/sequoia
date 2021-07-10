0eababec57c821759180b04a4bfc456dbc176e2a
Rob Rix <rob.rix@me.com>
2021-07-10 08:12:25 -0400

    Define a Contravariant instance for KV.

--- src/Sequoia/Profunctor/D.hs
+++ src/Sequoia/Profunctor/D.hs
@@ -5,6 +5,8 @@ module Sequoia.Profunctor.D
 
 import qualified Control.Category as Cat
 import           Control.Monad ((<=<))
+import           Data.Functor.Contravariant
+import           Data.Profunctor
 import           Sequoia.Bijection
 
 _KV :: Optic Iso (KV s r a) (KV s' r' a') (a -> s -> r) (a' -> s' -> r')
@@ -15,3 +17,6 @@ newtype KV s r a = KV { runKV :: a -> s -> r }
 instance Cat.Category (KV s) where
   id = KV const
   KV f . KV g = KV (g <=< f)
+
+instance Contravariant (KV s r) where
+  contramap f = under _KV (lmap f)
