413176b9e7f8c56d6d9d51b58f228cf4b034b269
Rob Rix <rob.rix@me.com>
2021-07-10 14:35:10 -0400

    Correct the Adjunction/Coadjunction superclasses.

--- src/Sequoia/Profunctor/Adjunction.hs
+++ src/Sequoia/Profunctor/Adjunction.hs
@@ -12,7 +12,7 @@ import           Data.Profunctor
 import qualified Data.Profunctor.Rep as Pro
 
 -- | A covariant adjunction between two profunctors.
-class (Profunctor f, Pro.Representable u) => Adjunction f u | f -> u, u -> f where
+class (Profunctor f, Pro.Corepresentable u) => Adjunction f u | f -> u, u -> f where
   {-# MINIMAL (leftUnit | leftAdjunct), (rightUnit | rightAdjunct) #-}
 
   leftUnit  :: a -> u b (f b a)
@@ -36,7 +36,7 @@ cotabulateAdjunction f = leftAdjunct f ()
 
 
 -- | A contravariant adjunction between two profunctors.
-class (Profunctor f, Pro.Corepresentable u) => Coadjunction f u | f -> u, u -> f where
+class (Profunctor f, Pro.Representable u) => Coadjunction f u | f -> u, u -> f where
   {-# MINIMAL (leftCounit | leftCoadjunct), (rightCounit | rightCoadjunct) #-}
 
   leftCounit  :: a -> u (f a b) b
