48a9920aad225053b12d2a5dd581759da16a9a38
Rob Rix <rob.rix@me.com>
2021-07-10 13:23:23 -0400

    Require f and u to be profunctorss.

--- src/Sequoia/Profunctor/Adjunction.hs
+++ src/Sequoia/Profunctor/Adjunction.hs
@@ -3,7 +3,9 @@ module Sequoia.Profunctor.Adjunction
 ( Adjunction(..)
 ) where
 
-class Adjunction f u | f -> u, u -> f where
+import Data.Profunctor
+
+class (Profunctor f, Profunctor u) => Adjunction f u | f -> u, u -> f where
   leftUnit :: a -> u b (f b a)
   rightUnit :: f b (u b a) -> a
 
