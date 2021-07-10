2e703e60b06f9b17b34f7624f6b1a0c41010d990
Rob Rix <rob.rix@me.com>
2021-07-10 14:19:39 -0400

    Redefine the Adjunction instance for Con in terms of Coadjunction.

--- src/Sequoia/Functor/Con.hs
+++ src/Sequoia/Functor/Con.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 -- | A contravariant functor over a profunctor’s input.
 module Sequoia.Functor.Con
 ( Con(..)
@@ -9,18 +10,18 @@ import           Data.Functor.Contravariant.Adjunction
 import           Data.Functor.Contravariant.Rep
 import           Data.Profunctor
 import qualified Data.Profunctor.Rep as Pro
-import           Data.Profunctor.Sieve
+import qualified Sequoia.Profunctor.Adjunction as Pro
 
 newtype Con p r a = Con { runCon :: p a r }
 
 instance Profunctor p => Contravariant (Con p r) where
   contramap f = Con . lmap f . runCon
 
-instance Pro.Representable p => Representable (Con p r) where
-  type Rep (Con p r) = Pro.Rep p r
-  tabulate = Con . Pro.tabulate
-  index = sieve . runCon
+instance (Pro.Coadjunction q p, Pro.Corep p ~ q ()) => Representable (Con p r) where
+  type Rep (Con p r) = Pro.Corep p r
+  tabulate = Con . Pro.cotabulateCoadjunction
+  index = Pro.cosieveCoadjunction . runCon
 
-instance Pro.Representable p => Adjunction (Con p r) (Con p r) where
-  leftAdjunct  f a = tabulate ((`index` a) . f)
-  rightAdjunct f a = tabulate ((`index` a) . f)
+instance (Pro.Coadjunction p q, Pro.Corep q ~ p ()) => Adjunction (Con p r) (Con q r) where
+  leftAdjunct  f a = Con (Pro.leftCoadjunct  (runCon . f) a)
+  rightAdjunct f a = Con (Pro.rightCoadjunct (runCon . f) a)
