1d8954e3c1bf8237179fe99ac6664a4bbd289dc4
Rob Rix <rob.rix@me.com>
2021-07-10 14:36:18 -0400

    Qualify an import.

--- src/Sequoia/Functor/Cov.hs
+++ src/Sequoia/Functor/Cov.hs
@@ -5,11 +5,11 @@ module Sequoia.Functor.Cov
 ( Cov(..)
 ) where
 
-import Data.Distributive
-import Data.Functor.Rep
-import Data.Profunctor
-import Data.Profunctor.Rep (Corepresentable(..))
-import Data.Profunctor.Sieve
+import           Data.Distributive
+import           Data.Functor.Rep
+import           Data.Profunctor
+import qualified Data.Profunctor.Rep as Pro
+import           Data.Profunctor.Sieve
 
 newtype Cov p s a = Cov { runCov :: p s a }
   deriving (Choice, Closed, Cochoice, Costrong, Profunctor, Strong)
@@ -17,11 +17,11 @@ newtype Cov p s a = Cov { runCov :: p s a }
 instance Profunctor p => Functor (Cov p s) where
   fmap f = Cov . rmap f . runCov
 
-instance Corepresentable p => Distributive (Cov p s) where
+instance Pro.Corepresentable p => Distributive (Cov p s) where
   distribute = distributeRep
   collect = collectRep
 
-instance Corepresentable p => Representable (Cov p s) where
-  type Rep (Cov p s) = Corep p s
-  tabulate = Cov . cotabulate
+instance Pro.Corepresentable p => Representable (Cov p s) where
+  type Rep (Cov p s) = Pro.Corep p s
+  tabulate = Cov . Pro.cotabulate
   index = cosieve . runCov
