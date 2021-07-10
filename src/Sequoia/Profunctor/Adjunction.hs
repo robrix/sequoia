3fcf2ee900e20b0f80166dc1f63451a2636deb5f
Rob Rix <rob.rix@me.com>
2021-07-10 13:24:59 -0400

    Align.

--- src/Sequoia/Profunctor/Adjunction.hs
+++ src/Sequoia/Profunctor/Adjunction.hs
@@ -8,7 +8,7 @@ import Data.Profunctor
 class (Profunctor f, Profunctor u) => Adjunction f u | f -> u, u -> f where
   {-# MINIMAL (leftUnit | leftAdjunct), (rightUnit | rightAdjunct) #-}
 
-  leftUnit :: a -> u b (f b a)
+  leftUnit  :: a -> u b (f b a)
   rightUnit :: f b (u b a) -> a
 
   leftUnit  = leftAdjunct id
