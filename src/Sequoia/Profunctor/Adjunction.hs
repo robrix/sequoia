e1daf6a09c20fa120171d7cc429765ffb93a77eb
Rob Rix <rob.rix@me.com>
2021-07-10 13:27:42 -0400

    Rename to line things up.

--- src/Sequoia/Profunctor/Adjunction.hs
+++ src/Sequoia/Profunctor/Adjunction.hs
@@ -14,8 +14,8 @@ class (Profunctor f, Profunctor u) => Adjunction f u | f -> u, u -> f where
   leftUnit  = leftAdjunct id
   rightUnit = rightAdjunct id
 
-  leftAdjunct  :: (f b a -> c    ) -> (a     -> u b c)
-  rightAdjunct :: (c     -> u a b) -> (f a c -> b    )
+  leftAdjunct  :: (f a b ->     c) -> (    b -> u a c)
+  rightAdjunct :: (    b -> u a c) -> (f a b ->     c)
 
   leftAdjunct  f = rmap f . leftUnit
   rightAdjunct f = rightUnit . rmap f
