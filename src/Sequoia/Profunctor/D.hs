eaf6ded24b60021bf569677921e934cd520b72ca
Rob Rix <rob.rix@me.com>
2021-07-10 08:21:35 -0400

    Define an EnvPassing instance for D under the assumption of a Value instance for VK (which doesn’t exist).

--- src/Sequoia/Profunctor/D.hs
+++ src/Sequoia/Profunctor/D.hs
@@ -16,6 +16,8 @@ import           Data.Profunctor
 import           Sequoia.Bijection
 import           Sequoia.CPS
 import           Sequoia.Continuation
+import           Sequoia.EPS
+import           Sequoia.Value
 
 _KV :: Optic Iso (KV s r a) (KV s' r' a') (a -> s -> r) (a' -> s' -> r')
 _KV = runKV <-> KV
@@ -67,3 +69,7 @@ instance Cat.Category (D r s) where
 instance ContPassing (KV s r) (D r s) where
   inC = D . over _KV
   exC = under _KV . runD
+
+instance Value (VK r s) => EnvPassing (VK r s) (D r s) where
+  inE = (_D % _DRep <~) . over _VK
+  exE = under _VK . (~> _D % _DRep)
