9e39ce5824d24b80235198ffe7e0a0e5780aef34
Rob Rix <rob.rix@me.com>
2021-07-10 13:22:01 -0400

    f and u determine one another.

--- src/Sequoia/Profunctor/Adjunction.hs
+++ src/Sequoia/Profunctor/Adjunction.hs
@@ -1,8 +1,9 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module Sequoia.Profunctor.Adjunction
 ( Adjunction(..)
 ) where
 
-class Adjunction f u where
+class Adjunction f u | f -> u, u -> f where
   leftUnit :: a -> u b (f b a)
   rightUnit :: f b (u b a) -> a
 
