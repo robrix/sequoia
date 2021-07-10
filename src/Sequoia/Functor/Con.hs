5aab15cb8301652378c96400d39744c376e987e0
Rob Rix <rob.rix@me.com>
2021-07-10 14:39:52 -0400

    Simplify the Con instances.

--- src/Sequoia/Functor/Con.hs
+++ src/Sequoia/Functor/Con.hs
@@ -10,6 +10,7 @@ import           Data.Functor.Contravariant.Adjunction
 import           Data.Functor.Contravariant.Rep
 import           Data.Profunctor
 import qualified Data.Profunctor.Rep as Pro
+import           Data.Profunctor.Sieve
 import qualified Sequoia.Profunctor.Adjunction as Pro
 
 newtype Con p r a = Con { runCon :: p a r }
@@ -17,11 +18,11 @@ newtype Con p r a = Con { runCon :: p a r }
 instance Profunctor p => Contravariant (Con p r) where
   contramap f = Con . lmap f . runCon
 
-instance (Pro.Coadjunction q p, Pro.Corep p ~ q ()) => Representable (Con p r) where
-  type Rep (Con p r) = Pro.Corep p r
-  tabulate = Con . Pro.tabulateCoadjunction
-  index = Pro.sieveCoadjunction . runCon
+instance Pro.Representable p => Representable (Con p r) where
+  type Rep (Con p r) = Pro.Rep p r
+  tabulate = Con . Pro.tabulate
+  index = sieve . runCon
 
-instance (Pro.Coadjunction p q, Pro.Corep q ~ p ()) => Adjunction (Con p r) (Con q r) where
+instance Pro.Coadjunction p q => Adjunction (Con p r) (Con q r) where
   leftAdjunct  f a = Con (Pro.leftCoadjunct  (runCon . f) a)
   rightAdjunct f a = Con (Pro.rightCoadjunct (runCon . f) a)
