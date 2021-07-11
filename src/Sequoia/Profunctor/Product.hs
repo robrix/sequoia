3d03ebf61ca3f909e7d7a01eae46b88b185b2549
Rob Rix <rob.rix@me.com>
2021-07-11 12:17:02 -0400

    Define a Strong instance for :*:.

--- src/Sequoia/Profunctor/Product.hs
+++ src/Sequoia/Profunctor/Product.hs
@@ -11,3 +11,7 @@ infixr 6 :*:
 
 instance (Profunctor p, Profunctor q) => Profunctor (p :*: q) where
   dimap f g = Product . (dimap f g *** dimap f g) . runProduct
+
+instance (Strong p, Strong q) => Strong (p :*: q) where
+  first'  = Product . (first'  *** first')  . runProduct
+  second' = Product . (second' *** second') . runProduct
