45f595f6128a36fa9f27441458f3061ec7312fa7
Rob Rix <rob.rix@me.com>
2021-07-11 15:22:01 -0400

    Qualify the import of +++.

--- src/Sequoia/Profunctor/Sum.hs
+++ src/Sequoia/Profunctor/Sum.hs
@@ -3,36 +3,36 @@ module Sequoia.Profunctor.Sum
 ( (:+:)(..)
 ) where
 
-import Control.Arrow ((+++))
-import Data.Functor.Sum
-import Data.Profunctor
-import Data.Profunctor.Sieve
+import qualified Control.Arrow as A ((+++))
+import           Data.Functor.Sum
+import           Data.Profunctor
+import           Data.Profunctor.Sieve
 
 newtype (p :+: q) a b = Sum { runSum :: Either (p a b) (q a b) }
 
 infixr 5 :+:
 
 instance (Profunctor p, Profunctor q) => Profunctor (p :+: q) where
-  dimap f g = Sum . (dimap f g +++ dimap f g) . runSum
+  dimap f g = Sum . (dimap f g A.+++ dimap f g) . runSum
 
 instance (Strong p, Strong q) => Strong (p :+: q) where
-  first'  = Sum . (first'  +++ first' ) . runSum
-  second' = Sum . (second' +++ second') . runSum
+  first'  = Sum . (first'  A.+++ first' ) . runSum
+  second' = Sum . (second' A.+++ second') . runSum
 
 instance (Costrong p, Costrong q) => Costrong (p :+: q) where
-  unfirst  = Sum . (unfirst  +++ unfirst ) . runSum
-  unsecond = Sum . (unsecond +++ unsecond) . runSum
+  unfirst  = Sum . (unfirst  A.+++ unfirst ) . runSum
+  unsecond = Sum . (unsecond A.+++ unsecond) . runSum
 
 instance (Choice p, Choice q) => Choice (p :+: q) where
-  left'  = Sum . (left'  +++ left' ) . runSum
-  right' = Sum . (right' +++ right') . runSum
+  left'  = Sum . (left'  A.+++ left' ) . runSum
+  right' = Sum . (right' A.+++ right') . runSum
 
 instance (Cochoice p, Cochoice q) => Cochoice (p :+: q) where
-  unleft  = Sum . (unleft  +++ unleft ) . runSum
-  unright = Sum . (unright +++ unright) . runSum
+  unleft  = Sum . (unleft  A.+++ unleft ) . runSum
+  unright = Sum . (unright A.+++ unright) . runSum
 
 instance (Closed p, Closed q) => Closed (p :+: q) where
-  closed = Sum . (closed +++ closed) . runSum
+  closed = Sum . (closed A.+++ closed) . runSum
 
 instance (Sieve p f, Sieve q g) => Sieve (p :+: q) (Sum f g) where
   sieve (Sum s) a = either (InL . (`sieve` a)) (InR . (`sieve` a)) s
