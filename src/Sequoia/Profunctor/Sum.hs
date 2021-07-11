3dec0a782aac1958811d6de577d8b152fb1d67a4
Rob Rix <rob.rix@me.com>
2021-07-11 12:48:11 -0400

    Define a Profunctor instance for :+:.

--- src/Sequoia/Profunctor/Sum.hs
+++ src/Sequoia/Profunctor/Sum.hs
@@ -2,6 +2,12 @@ module Sequoia.Profunctor.Sum
 ( (:+:)(..)
 ) where
 
+import Control.Arrow ((+++))
+import Data.Profunctor
+
 newtype (p :+: q) a b = Sum { runSum :: Either (p a b) (q a b) }
 
 infixr 5 :+:
+
+instance (Profunctor p, Profunctor q) => Profunctor (p :+: q) where
+  dimap f g = Sum . (dimap f g +++ dimap f g) . runSum
