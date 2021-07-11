61c49da96e291aa1e3b039bb43e7f743fed1263f
Rob Rix <rob.rix@me.com>
2021-07-11 12:14:33 -0400

    Define a Profunctor instance for :*:.

--- src/Sequoia/Profunctor/Product.hs
+++ src/Sequoia/Profunctor/Product.hs
@@ -2,6 +2,12 @@ module Sequoia.Profunctor.Product
 ( (:*:)(..)
 ) where
 
+import Control.Arrow ((***))
+import Data.Profunctor
+
 newtype (p :*: q) a b = Product { runProduct :: (p a b, q a b) }
 
 infixr 6 :*:
+
+instance (Profunctor p, Profunctor q) => Profunctor (p :*: q) where
+  dimap f g = Product . (dimap f g *** dimap f g) . runProduct
