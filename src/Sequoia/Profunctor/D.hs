74a15dcbe050286e816c1bbaf61eb093c8a24642
Rob Rix <rob.rix@me.com>
2021-07-12 19:36:46 -0400

    Sort the environment to the end.

--- src/Sequoia/Profunctor/D.hs
+++ src/Sequoia/Profunctor/D.hs
@@ -36,25 +36,25 @@ import           Sequoia.Value as V
 
 -- Dual profunctor
 
-newtype D r s a b = D { exD :: s -> V s a -> K r b -> r }
+newtype D r s a b = D { exD :: V s a -> K r b -> (s -> r) }
 
 instance Profunctor (D r s) where
-  dimap f g = D . rmap (dimap (rmap f) (lmap (contramap g))) . exD
+  dimap f g = D . dimap (rmap f) (lmap (contramap g)) . exD
 
 instance Cat.Category (D r s) where
-  id = D (\ e a b -> b • e ∘ a)
-  D f . D g = D (\ e a c -> g e a (K (\ b -> f e (inV0 b) c)))
+  id = D (\ a b e -> b • e ∘ a)
+  D f . D g = D (\ a c e -> g a (K (\ b -> f (inV0 b) c e)) e)
 
 instance Functor (D r s c) where
   fmap = rmap
 
 instance Applicative (D r s a) where
-  pure a = D (\ _ _ b -> b • a)
+  pure a = D (\ _ b _ -> b • a)
 
-  D df <*> D da = D (\ e a b -> df e a (K (da e a . (b •<<))))
+  D df <*> D da = D (\ a b e -> df a (K (\ f -> da a (contramap f b) e)) e)
 
 instance Monad (D r s a) where
-  D m >>= f = D (\ e a c -> m e a (K (\ b -> exD (f b) e a c)))
+  D m >>= f = D (\ a c e -> m a (K (\ b -> exD (f b) a c e)) e)
 
 
 -- Mixfix notation
@@ -69,22 +69,22 @@ infixr 5 |->
 -- Construction
 
 inD' :: (a -> b) -> a --|D r s|-> b
-inD' f = D (\ e a b -> b • f (e ∘ a))
+inD' f = D (\ a b e -> b • f (e ∘ a))
 
 inDK :: (K r b -> K r a) -> a --|D r s|-> b
-inDK f = D (\ e a b -> f b • e ∘ a)
+inDK f = D (\ a b e -> f b • e ∘ a)
 
 inDV :: (V s a -> V s b) -> a --|D r s|-> b
-inDV f = D (\ e a b -> b • e ∘ f a)
+inDV f = D (\ a b e -> b • e ∘ f a)
 
 
 -- Elimination
 
 exDK :: a --|D r s|-> b -> V s (K r b -> K r a)
-exDK f = V (\ e k -> K (\ a -> exD f e (inV0 a) k))
+exDK f = V (\ e k -> K (\ a -> exD f (inV0 a) k e))
 
 exDV :: K r (V s a -> V s b) -> K r (a --|D b s|-> b)
-exDV k = K (\ f -> k • inV . \ a e -> exD f e a idK)
+exDV k = K (\ f -> k • inV . \ a -> exD f a idK)
 
 evalD :: a --|D r s|-> r -> V s (K r a)
 evalD = (idK ↓)
@@ -93,7 +93,7 @@ evalD = (idK ↓)
 -- Computation
 
 (↑) :: a --|D r s|-> b -> V s a -> V s (K r (K r b))
-f ↑ a = V (\ e -> K (exD f e a))
+f ↑ a = V (K . flip (exD f a))
 
 infixl 7 ↑
 
@@ -103,12 +103,12 @@ f <↑ a = f Cat.<<< inD' (inlr a)
 infixl 7 <↑
 
 (↓) :: K r b -> a --|D r s|-> b -> V s (K r a)
-k ↓ f = V (\ e -> K (\ a -> exD f e (inV0 a) k))
+k ↓ f = V (\ e -> K (\ a -> exD f (inV0 a) k e))
 
 infixl 8 ↓
 
 -- FIXME: this is quite limited by the need for the continuation to return locally at r.
 (↓>) :: Disj d => K r c -> a --|D r s|-> (b `d` c) -> a --|D r s|-> b
-c ↓> f = D (\ e v k -> (k <••> c) • e ∘ v) <<< f
+c ↓> f = D (\ v k e -> (k <••> c) • e ∘ v) <<< f
 
 infixr 9 ↓>
