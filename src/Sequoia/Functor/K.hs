1ba1ea32ca3b08fcf2fee35406a2e3f924faa9fb
Rob Rix <rob.rix@me.com>
2021-07-11 23:34:42 -0400

    Define an Adjunction instance for K.

--- src/Sequoia/Functor/K.hs
+++ src/Sequoia/Functor/K.hs
@@ -4,6 +4,7 @@ module Sequoia.Functor.K
 ) where
 
 import Data.Functor.Contravariant
+import Data.Functor.Contravariant.Adjunction
 import Data.Functor.Contravariant.Rep
 import Data.Functor.Identity
 import Sequoia.Confunctor
@@ -12,3 +13,7 @@ newtype K r a = K { runK :: a -> r }
   deriving (Monoid, Semigroup)
   deriving (Contravariant, Representable) via Flip (->) r
   deriving (Confunctor, Contrachoice, Contraclosed, Contracochoice, Contracosieve Identity, Contracostrong, Contracorepresentable, Contrarepresentable, Contrasieve Identity, Contrastrong) via Flip (->)
+
+instance Adjunction (K r) (K r) where
+  leftAdjunct  f a = K ((`runK` a) . f)
+  rightAdjunct f b = K ((`runK` b) . f)
