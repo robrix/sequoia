57c0e1656e4fec958b988161ce994141fa11798e
Rob Rix <rob.rix@me.com>
2021-07-12 09:50:16 -0400

    Give a default for contraliftA2.

--- src/Sequoia/Functor/Applicative.hs
+++ src/Sequoia/Functor/Applicative.hs
@@ -6,5 +6,7 @@ import Data.Functor.Contravariant
 
 class Contravariant k => Contrapplicative k where
   contraliftA2 :: (c -> Either a b) -> k a -> k b -> k c
+  contraliftA2 f a b = contramap f (contrap a b)
+
   contrap :: k a -> k b -> k (Either a b)
   contrap = contraliftA2 id
