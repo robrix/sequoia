6f39aed33f1b78b981f4c6ef5d843313568cbbf4
Rob Rix <rob.rix@me.com>
2021-07-12 09:45:08 -0400

    Define a contraliftA2 method for Contrapplicative.

--- src/Sequoia/Functor/Applicative.hs
+++ src/Sequoia/Functor/Applicative.hs
@@ -1,7 +1,8 @@
 module Sequoia.Functor.Applicative
-( Contrapplicative
+( Contrapplicative(..)
 ) where
 
 import Data.Functor.Contravariant
 
 class Contravariant k => Contrapplicative k where
+  contraliftA2 :: (c -> Either a b) -> k a -> k b -> k c
