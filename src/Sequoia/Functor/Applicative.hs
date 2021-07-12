c0c4c493ebc6c71d4a5288ebdf10a9364da56432
Rob Rix <rob.rix@me.com>
2021-07-12 09:49:16 -0400

    Define a contrap method to Contrapplicative.

--- src/Sequoia/Functor/Applicative.hs
+++ src/Sequoia/Functor/Applicative.hs
@@ -6,3 +6,4 @@ import Data.Functor.Contravariant
 
 class Contravariant k => Contrapplicative k where
   contraliftA2 :: (c -> Either a b) -> k a -> k b -> k c
+  contrap :: k a -> k b -> k (Either a b)
