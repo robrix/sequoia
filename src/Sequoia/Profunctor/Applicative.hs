c5042d99012af92ab441c3fa4c26ed007c0544e0
Rob Rix <rob.rix@me.com>
2021-07-12 09:24:39 -0400

    Define a Coapplicative class.

--- src/Sequoia/Profunctor/Applicative.hs
+++ src/Sequoia/Profunctor/Applicative.hs
@@ -1,2 +1,8 @@
 module Sequoia.Profunctor.Applicative
-() where
+( Coapplicative(..)
+) where
+
+import Data.Profunctor
+
+class Profunctor p => Coapplicative p where
+  coliftA2 :: (c -> Either a b) -> p a z -> p b z -> p c z
