53556e6ae3ad13bd64ff19bb2accee614647a646
Rob Rix <rob.rix@me.com>
2021-07-12 09:25:40 -0400

    Give a default for coliftA2.

--- src/Sequoia/Profunctor/Applicative.hs
+++ src/Sequoia/Profunctor/Applicative.hs
@@ -6,5 +6,7 @@ import Data.Profunctor
 
 class Profunctor p => Coapplicative p where
   coliftA2 :: (c -> Either a b) -> p a z -> p b z -> p c z
+  coliftA2 f a b = lmap f (coap a b)
+
   coap :: p a z -> p b z -> p (Either a b) z
   coap = coliftA2 id
