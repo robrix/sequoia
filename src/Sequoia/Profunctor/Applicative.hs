2684a3ad34d38109b49e93600c579ea302a95c4c
Rob Rix <rob.rix@me.com>
2021-07-12 09:26:11 -0400

    Give minimality for Coapplicative.

--- src/Sequoia/Profunctor/Applicative.hs
+++ src/Sequoia/Profunctor/Applicative.hs
@@ -5,6 +5,8 @@ module Sequoia.Profunctor.Applicative
 import Data.Profunctor
 
 class Profunctor p => Coapplicative p where
+  {-# MINIMAL coliftA2 | coap #-}
+
   coliftA2 :: (c -> Either a b) -> p a z -> p b z -> p c z
   coliftA2 f a b = lmap f (coap a b)
 
