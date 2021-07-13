26734c0cdc6ffcf768d9ef2ffc263c03cf053f67
Rob Rix <rob.rix@me.com>
2021-07-13 15:14:27 -0400

    Go back to parameterizing D by 0 and 1.

--- src/Sequoia/Profunctor/D.hs
+++ src/Sequoia/Profunctor/D.hs
@@ -52,32 +52,34 @@ import           Sequoia.Bijection
 import           Sequoia.Conjunction
 import           Sequoia.Continuation as K
 import           Sequoia.Disjunction
+import           Sequoia.Functor.K
+import           Sequoia.Functor.V
 import           Sequoia.Profunctor.Applicative
 import           Sequoia.Value as V
 
 -- Dual profunctor
 
-newtype D k v a b = D { runD :: v a -> k b -> Control (K.Rep k) (V.Rep v) }
+newtype D r s a b = D { runD :: V s a -> K r b -> Control r s }
 
-instance (Contravariant k, Functor v) => Profunctor (D k v) where
+instance Profunctor (D r s) where
   dimap f g = D . dimap (fmap f) (lmap (contramap g)) . runD
 
-instance (Continuation r k, Value s v) => Cat.Category (D k v) where
+instance Cat.Category (D r s) where
   id = D (flip (•∘))
   D f . D g = D (\ a c -> liftKWith (\ _K -> g a (_K (\ b -> f (inV0 b) c))))
 
-instance Contravariant k => Functor (D k v c) where
+instance Functor (D r s c) where
   fmap f = D . fmap (lmap (contramap f)) . runD
 
-instance (Continuation r k, V.Representable v) => Applicative (D k v a) where
+instance Applicative (D r s a) where
   pure a = D (\ _ b -> b •• a)
 
   D df <*> D da = D (\ a b -> liftKWith (\ _K -> df a (_K (\ f -> da a (contramap f b)))))
 
-instance (Continuation r k, V.Representable v) => Monad (D k v a) where
+instance Monad (D r s a) where
   D m >>= f = D (\ a c -> liftKWith (\ _K -> m a (_K (\ b -> runD (f b) a c))))
 
-instance (Continuation r k, Value s v) => Coapply (D k v) where
+instance Coapply (D r s) where
   coliftA2 f a b = D (\ v k -> withVal ((flip (exD a) k . inV0 <--> flip (exD b) k . inV0) . f) v)
 
 
@@ -92,82 +94,82 @@ infixr 5 |->
 
 -- Dual profunctor abstraction
 
-_D :: Dual k v d => d a b <-> (v a -> k b -> Control (K.Rep k) (V.Rep v))
+_D :: Dual r s d => d a b <-> (V s a -> K r b -> Control r s)
 _D = exD <-> inD
 
-class (Continuation (K.Rep k) k, Value (V.Rep v) v, Cat.Category d, Profunctor d) => Dual k v d | d -> k v where
-  inD :: (v a -> k b -> Control (K.Rep k) (V.Rep v)) -> d a b
-  exD :: d a b -> v a -> k b -> Control (K.Rep k) (V.Rep v)
+class (Cat.Category d, Profunctor d) => Dual r s d | d -> r s where
+  inD :: (V s a -> K r b -> Control r s) -> d a b
+  exD :: d a b -> V s a -> K r b -> Control r s
 
-instance (Continuation r k, Value s v) => Dual k v (D k v) where
+instance Dual r s (D r s) where
   inD = D
   exD = runD
 
 
 -- Construction
 
-inD' :: Dual k v d => (a -> b) -> a --|d|-> b
+inD' :: Dual r s d => (a -> b) -> a --|d|-> b
 inD' f = inD (\ a b -> b •∘ (f <$> a))
 
-inDK :: Dual k v d => (k b -> k a) -> a --|d|-> b
+inDK :: Dual r s d => (K r b -> K r a) -> a --|d|-> b
 inDK f = inD (\ a b -> f b •∘ a)
 
-inDV :: Dual k v d => (v a -> v b) -> a --|d|-> b
+inDV :: Dual r s d => (V s a -> V s b) -> a --|d|-> b
 inDV f = inD (\ a b -> b •∘ f a)
 
 
 -- Elimination
 
-exDK :: Dual k v d => a --|d|-> b -> v (k b -> k a)
+exDK :: Dual r s d => a --|d|-> b -> V s (K r b -> K r a)
 exDK f = inV (\ e k -> inK (\ a -> evalControl (exD f (inV0 a) k) e))
 
-exDV :: (K.Representable k', Dual k v d) => k' (v a -> v (K.Rep k)) -> k' (a --|d|-> K.Rep k)
+exDV :: Dual r s d => K r' (V s a -> V s r) -> K r' (a --|d|-> r)
 exDV k = inK (\ f -> k • inV . \ a -> evalControl (exD f a idK))
 
-evalD :: Dual k v d => a --|d|-> K.Rep k -> Consumer d a
+evalD :: Dual r s d => a --|d|-> r -> Consumer d a
 evalD = (idK ↓)
 
 
 -- Computation
 
-(↑) :: Dual k v d => a --|d|-> b -> v a -> Producer d b
+(↑) :: Dual r s d => a --|d|-> b -> V s a -> Producer d b
 f ↑ a = inD (const (exD f a))
 
 infixl 7 ↑
 
-(<↑) :: Dual k v d => Conj c => (a `c` _Γ) --|d|-> _Δ -> a -> _Γ --|d|-> _Δ
+(<↑) :: Dual r s d => Conj c => (a `c` _Γ) --|d|-> _Δ -> a -> _Γ --|d|-> _Δ
 f <↑ a = f <<< inD' (inlr a)
 
 infixl 7 <↑
 
-(↓) :: Dual k v d => k b -> a --|d|-> b -> Consumer d a
+(↓) :: Dual r s d => K r b -> a --|d|-> b -> Consumer d a
 k ↓ f = inD (const . flip (exD f) k)
 
 infixl 8 ↓
 
 -- FIXME: this is quite limited by the need for the continuation to return locally at r.
-(↓>) :: (Dual k v d, Disj s) => k c -> a --|d|-> (b `s` c) -> a --|d|-> b
+(↓>) :: (Dual r s d, Disj p) => K r c -> a --|d|-> (b `p` c) -> a --|d|-> b
 c ↓> f = inD (\ v k -> (k <••> c) •∘ v) <<< f
 
 infixr 9 ↓>
 
-dnE :: Dual k v d => k **(a --|d|-> b) -> a --|d|-> b
+dnE :: Dual r s d => K r **(a --|d|-> b) -> a --|d|-> b
 dnE k = inD (\ a b -> liftKWith (\ _K -> k •• _K (\ f -> exD f a b)))
 
 
 -- Composition
 
-(↓↓) :: Dual k v d => Consumer d b -> a --|d|-> b -> Consumer d a
+(↓↓) :: Dual r s d => Consumer d b -> a --|d|-> b -> Consumer d a
 k ↓↓ f = inD (\ a b -> liftKWith (\ _K -> exD f a (_K (flip (exD k) b . inV0))))
 
 infixl 8 ↓↓
 
-(↑↑) :: Dual k v d => a --|d|-> b -> Producer d a -> Producer d b
+(↑↑) :: Dual r s d => a --|d|-> b -> Producer d a -> Producer d b
 f ↑↑ v = inD (\ a b -> liftKWith (\ _K -> exD v a (_K (\ a -> exD f (inV0 a) b))))
 
 infixr 7 ↑↑
 
-(↓↑) :: Dual k v d => Consumer d a -> Producer d a -> Control (K.Rep k) (V.Rep v)
+(↓↑) :: Dual r s d => Consumer d a -> Producer d a -> Control r s
 k ↓↑ v = liftKWith (\ _K -> exD v (inV0 ()) (_K (flip (exD k) (inK absurd) . inV0)))
 
 infix 9 ↓↑
