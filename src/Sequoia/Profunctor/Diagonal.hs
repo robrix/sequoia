3abd28862ae649e821d5bf917ad8413bd96fe7cc
Rob Rix <rob.rix@me.com>
2021-07-16 08:36:35 -0400

    Define a codiagonal profunctor.

--- src/Sequoia/Profunctor/Diagonal.hs
+++ src/Sequoia/Profunctor/Diagonal.hs
@@ -1,8 +1,12 @@
 module Sequoia.Profunctor.Diagonal
 ( Diagonal(..)
+, Codiagonal(..)
 ) where
 
 import Data.Profunctor
 
 class Profunctor p => Diagonal p where
   dup :: a `p` (a, a)
+
+class Profunctor p => Codiagonal p where
+  dedup :: Either a a `p` a
