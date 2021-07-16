f1fe953bd621b753d4960f0da3c97524c07daacf
Rob Rix <rob.rix@me.com>
2021-07-16 08:39:22 -0400

    Define an instance of Diagonal for ->.

--- src/Sequoia/Profunctor/Diagonal.hs
+++ src/Sequoia/Profunctor/Diagonal.hs
@@ -3,10 +3,15 @@ module Sequoia.Profunctor.Diagonal
 , Codiagonal(..)
 ) where
 
+import Control.Monad (join)
 import Data.Profunctor
 
 class Profunctor p => Diagonal p where
   dup :: a `p` (a, a)
+
+instance Diagonal (->) where
+  dup = join (,)
+
 
 class Profunctor p => Codiagonal p where
   dedup :: Either a a `p` a
