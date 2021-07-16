2bfc1cc6b450f5eb07efee99a2a76926444da7e4
Rob Rix <rob.rix@me.com>
2021-07-16 17:53:17 -0400

    Define Diagonal & Codiagonal instances for Kleisli.

--- src/Sequoia/Profunctor/Diagonal.hs
+++ src/Sequoia/Profunctor/Diagonal.hs
@@ -3,6 +3,7 @@ module Sequoia.Profunctor.Diagonal
 , Codiagonal(..)
 ) where
 
+import Control.Arrow (Kleisli(..))
 import Data.Profunctor
 
 class Profunctor p => Diagonal p where
@@ -11,9 +12,15 @@ class Profunctor p => Diagonal p where
 instance Diagonal (->) where
   dup a = (a, a)
 
+instance Monad m => Diagonal (Kleisli m) where
+  dup = Kleisli (pure . dup)
+
 
 class Profunctor p => Codiagonal p where
   dedup :: Either a a `p` a
 
 instance Codiagonal (->) where
   dedup = either id id
+
+instance Monad m => Codiagonal (Kleisli m) where
+  dedup = Kleisli (pure . dedup)
