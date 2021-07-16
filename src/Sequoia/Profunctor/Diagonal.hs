1b183d4953ed76630cd31fac4604579ea93a0f91
Rob Rix <rob.rix@me.com>
2021-07-16 18:16:59 -0400

    Define Diagonal & Codiagonal instances for Star & Costar.

--- src/Sequoia/Profunctor/Diagonal.hs
+++ src/Sequoia/Profunctor/Diagonal.hs
@@ -4,6 +4,7 @@ module Sequoia.Profunctor.Diagonal
 ) where
 
 import Control.Arrow (Kleisli(..))
+import Control.Comonad
 import Data.Profunctor
 import Data.Profunctor.Strong
 
@@ -22,6 +23,12 @@ instance Diagonal p => Diagonal (Pastro p) where
 instance Diagonal p => Diagonal (Copastro p) where
   dup = Copastro ($ dup)
 
+instance Applicative f => Diagonal (Star f) where
+  dup = Star (pure . dup)
+
+instance Comonad f => Diagonal (Costar f) where
+  dup = Costar (dup . extract)
+
 
 class Profunctor p => Codiagonal p where
   dedup :: Either a a `p` a
@@ -37,3 +44,9 @@ instance Codiagonal p => Codiagonal (Pastro p) where
 
 instance Codiagonal p => Codiagonal (Copastro p) where
   dedup = Copastro ($ dedup)
+
+instance Applicative f => Codiagonal (Star f) where
+  dedup = Star (pure . dedup)
+
+instance Comonad f => Codiagonal (Costar f) where
+  dedup = Costar (dedup . extract)
