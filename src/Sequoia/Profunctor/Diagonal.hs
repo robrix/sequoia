374bfa911b9c7bb09fc707183cbfd31fd878c346
Rob Rix <rob.rix@me.com>
2021-07-16 08:40:33 -0400

    Give direct definitions of dup and dedup.

--- src/Sequoia/Profunctor/Diagonal.hs
+++ src/Sequoia/Profunctor/Diagonal.hs
@@ -3,18 +3,17 @@ module Sequoia.Profunctor.Diagonal
 , Codiagonal(..)
 ) where
 
-import Control.Monad (join)
 import Data.Profunctor
 
 class Profunctor p => Diagonal p where
   dup :: a `p` (a, a)
 
 instance Diagonal (->) where
-  dup = join (,)
+  dup a = (a, a)
 
 
 class Profunctor p => Codiagonal p where
   dedup :: Either a a `p` a
 
 instance Codiagonal (->) where
-  dedup = join either id
+  dedup = either id id
