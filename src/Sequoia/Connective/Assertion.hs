9d01e68a0a49375811124a1be2237e3fcc0425d5
Rob Rix <rob.rix@me.com>
2021-07-18 09:13:58 -0400

    Derive some instances for Yes.

--- src/Sequoia/Connective/Assertion.hs
+++ src/Sequoia/Connective/Assertion.hs
@@ -1,13 +1,20 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Sequoia.Connective.Assertion
 ( -- * Yes
   Yes(..)
 ) where
 
+import Data.Distributive
 import Sequoia.Functor.V
 import Sequoia.Polarity
+import Sequoia.Value
 
 -- Yes
 
 newtype Yes e a = Yes { getYes :: V e a }
+  deriving (Functor, Representable, Value)
+
+instance Distributive (Yes e) where
+  distribute = Yes . distribute . fmap getYes
 
 instance Pos a => Polarized P (Yes e a) where
