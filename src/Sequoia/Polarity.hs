32e688fc30a5d61992e13b5b2e2f937a9854b734
Rob Rix <rob.rix@me.com>
2021-07-17 20:12:57 -0400

    Define covariant Adjunction instances for the polarities.

--- src/Sequoia/Polarity.hs
+++ src/Sequoia/Polarity.hs
@@ -12,6 +12,7 @@ module Sequoia.Polarity
 ) where
 
 import Data.Distributive
+import Data.Functor.Adjunction as Co
 import Data.Functor.Identity
 import Data.Functor.Rep as Co
 import Data.Kind (Type)
@@ -25,12 +26,24 @@ newtype N a = N { getN :: a }
 instance Distributive N where
   distribute = N . fmap getN
 
+instance Co.Adjunction N P where
+  unit   =    P .    N
+  counit = getP . getN
+  leftAdjunct  f a =    P (f (   N a))
+  rightAdjunct f a = getP (f (getN a))
+
 newtype P a = P { getP :: a }
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
   deriving (Applicative, Monad, Co.Representable) via Identity
 
 instance Distributive P where
   distribute = P . fmap getP
+
+instance Co.Adjunction P N where
+  unit   =    N .    P
+  counit = getN . getP
+  leftAdjunct  f a =    N (f (   P a))
+  rightAdjunct f a = getN (f (getP a))
 
 
 -- Polarization
