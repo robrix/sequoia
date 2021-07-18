3904dc485df84c0c234ea8027df32bc9620341cb
Rob Rix <rob.rix@me.com>
2021-07-18 09:11:05 -0400

    Yes is positive.

--- src/Sequoia/Connective/Assertion.hs
+++ src/Sequoia/Connective/Assertion.hs
@@ -4,7 +4,10 @@ module Sequoia.Connective.Assertion
 ) where
 
 import Sequoia.Functor.V
+import Sequoia.Polarity
 
 -- Yes
 
 newtype Yes e a = Yes { getYes :: V e a }
+
+instance Pos a => Polarized P (Yes e a) where
