af7f4127841c5fc0eabefa60a995caa6d34e6851
Rob Rix <rob.rix@me.com>
2021-07-19 16:07:50 -0400

    Map contravariantly over the first field.

--- src/Sequoia/Bicontravariant.hs
+++ src/Sequoia/Bicontravariant.hs
@@ -1,9 +1,13 @@
 module Sequoia.Bicontravariant
 ( -- * Bicontravariant functors
   Bicontravariant(..)
+, contrafirst
 ) where
 
 -- Bicontravariant functors
 
 class Bicontravariant p where
   contrabimap :: (a' -> a) -> (b' -> b) -> a `p` b -> a' `p` b'
+
+contrafirst :: Bicontravariant p => (a' -> a) -> a `p` b -> a' `p` b
+contrafirst = (`contrabimap` id)
