8dadd74ca11a654e5dcffecf33c377f9dbaaf344
Rob Rix <rob.rix@me.com>
2021-07-18 22:41:54 -0400

    Define a class and left rule for NotUntrue.

--- src/Sequoia/Calculus/NotUntrue.hs
+++ src/Sequoia/Calculus/NotUntrue.hs
@@ -1,6 +1,20 @@
 module Sequoia.Calculus.NotUntrue
-( -- * Connectives
-  module Sequoia.Connective.Assertion
+( -- * NotUntrue
+  NotUntrueIntro(..)
+  -- * Connectives
+, module Sequoia.Connective.Assertion
 ) where
 
+import Sequoia.Calculus.Context
+import Sequoia.Calculus.Core
 import Sequoia.Connective.Assertion
+import Sequoia.Polarity
+
+-- NotUntrue
+
+class Core s => NotUntrueIntro s where
+  notUntrueL
+    :: Neg a
+    =>    a < _Γ -|s e r|- _Δ
+    -- ----------------------
+    -> r ≁a < _Γ -|s e r|- _Δ
