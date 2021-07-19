97b0bbc0d4a507a55b184571bd16a7e80560bca9
Rob Rix <rob.rix@me.com>
2021-07-18 22:42:06 -0400

    Re-export NotUntrue.

--- src/Sequoia/Calculus/Assertion.hs
+++ src/Sequoia/Calculus/Assertion.hs
@@ -3,9 +3,11 @@ module Sequoia.Calculus.Assertion
 ( -- * Assertion
   AssertionIntro
   -- * Re-exports
+, module Sequoia.Calculus.NotUntrue
 , module Sequoia.Calculus.True
 ) where
 
+import Sequoia.Calculus.NotUntrue
 import Sequoia.Calculus.True
 
 type AssertionIntro s = TrueIntro s
