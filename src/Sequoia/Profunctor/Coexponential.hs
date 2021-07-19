6cd1bfab5d1abe146f746642fad52dd78ab4b30f
Rob Rix <rob.rix@me.com>
2021-07-19 12:22:15 -0400

    Define a Profunctor instance for Coexp.

--- src/Sequoia/Profunctor/Coexponential.hs
+++ src/Sequoia/Profunctor/Coexponential.hs
@@ -7,10 +7,15 @@ module Sequoia.Profunctor.Coexponential
 , runCoexp
 ) where
 
+import Data.Profunctor
+
 -- Coexponential profunctor
 
 data Coexp e r a b = Coexp { coexpIn :: e -> b, coexpOut :: a -> r }
   deriving (Functor)
+
+instance Profunctor (Coexp e r) where
+  dimap f g c = Coexp (g . coexpIn c) (coexpOut c . f)
 
 
 -- Construction
