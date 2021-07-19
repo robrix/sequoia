6b16b23cd5d519ab3e1281f2301ad7519f733165
Rob Rix <rob.rix@me.com>
2021-07-19 15:35:46 -0400

    Define a view function.

--- src/Sequoia/Optic/Getter.hs
+++ src/Sequoia/Optic/Getter.hs
@@ -1,6 +1,7 @@
 module Sequoia.Optic.Getter
 ( -- * Getters
   Getter
+, view
 ) where
 
 import Data.Profunctor
@@ -9,3 +10,6 @@ import Sequoia.Bijection
 -- Getters
 
 type Getter s t a b = forall p . (Cochoice p, Strong p) => Optic p s t a b
+
+view :: Optic (Forget a) s t a b -> (s -> a)
+view = (`views` id)
