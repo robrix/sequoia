cbf39d8194dc1d4a73477783dd6514b77acc26ff
Rob Rix <rob.rix@me.com>
2021-07-19 17:32:01 -0400

    Define a simpler type synonym.

--- src/Sequoia/Optic/Review.hs
+++ src/Sequoia/Optic/Review.hs
@@ -1,5 +1,7 @@
 module Sequoia.Optic.Review
-( Review
+( -- * Reviews
+  Review
+, Review'
 ) where
 
 import Data.Bifunctor
@@ -9,3 +11,5 @@ import Sequoia.Bijection
 -- Reviews
 
 type Review s t a b = forall p . (Bifunctor p, Profunctor p) => Optic p s t a b
+
+type Review' s a = forall p . (Bifunctor p, Profunctor p) => Optic p s s a a
