a7497fe3003ea318ca877759ea919af45da6b1b0
Rob Rix <rob.rix@me.com>
2021-07-19 16:05:05 -0400

    Define a Bicontravariant class.

--- src/Sequoia/Bicontravariant.hs
+++ src/Sequoia/Bicontravariant.hs
@@ -1,2 +1,9 @@
 module Sequoia.Bicontravariant
-() where
+( -- * Bicontravariant functors
+  Bicontravariant(..)
+) where
+
+-- Bicontravariant functors
+
+class Bicontravariant p where
+  contrabimap :: (a' -> a) -> (b' -> b) -> a `p` b -> a' `p` b'
