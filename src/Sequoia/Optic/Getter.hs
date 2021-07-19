75e9f9a606cc0c94a7c5e20cdd7a26aa5f917518
Rob Rix <rob.rix@me.com>
2021-07-19 16:43:59 -0400

    Define a constructor for Getter.

--- src/Sequoia/Optic/Getter.hs
+++ src/Sequoia/Optic/Getter.hs
@@ -1,15 +1,28 @@
 module Sequoia.Optic.Getter
 ( -- * Getters
   Getter
+  -- * Construction
+, to
+  -- * Elimination
 , view
 ) where
 
 import Data.Profunctor
+import Sequoia.Bicontravariant
 import Sequoia.Bijection
 
 -- Getters
 
-type Getter s a = forall p . (Cochoice p, Strong p) => Optic' p s a
+type Getter s a = forall p . (Bicontravariant p, Profunctor p) => Optic' p s a
+
+
+-- Construction
+
+to :: (s -> a) -> Getter s a
+to f = lmap f . rphantom
+
+
+-- Elimination
 
 view :: Optic (Forget a) s t a b -> (s -> a)
 view = (`views` id)
