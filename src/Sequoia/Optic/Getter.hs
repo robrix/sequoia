c1a432a735117c8b3ddc718e8114799f188598ce
Rob Rix <rob.rix@me.com>
2021-07-19 15:34:11 -0400

    Define a Getter type.

--- src/Sequoia/Optic/Getter.hs
+++ src/Sequoia/Optic/Getter.hs
@@ -1,2 +1,11 @@
 module Sequoia.Optic.Getter
-() where
+( -- * Getters
+  Getter
+) where
+
+import Data.Profunctor
+import Sequoia.Bijection
+
+-- Getters
+
+type Getter s t a b = forall p . (Cochoice p, Strong p) => Optic p s t a b
