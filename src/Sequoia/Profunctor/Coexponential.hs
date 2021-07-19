8e5b7e39c539f268ab945920a2ef3de926d257b7
Rob Rix <rob.rix@me.com>
2021-07-19 12:17:33 -0400

    Define an eliminator for coexponentials.

--- src/Sequoia/Profunctor/Coexponential.hs
+++ src/Sequoia/Profunctor/Coexponential.hs
@@ -1,6 +1,10 @@
 module Sequoia.Profunctor.Coexponential
-( Coexp(..)
+( runCoexp
+, Coexp(..)
 ) where
+
+runCoexp :: Coexp e r b a -> (a -> b) -> (e -> r)
+runCoexp (Coexp a b) = (b .) . (. a)
 
 data Coexp e r b a = Coexp { coexpA :: e -> a, coexpB :: b -> r }
   deriving (Functor)
