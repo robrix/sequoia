6c4325da256e4da1b5c61261c3d2446b3b88c0c4
Rob Rix <rob.rix@me.com>
2021-07-19 16:09:26 -0400

    Align.

--- src/Sequoia/Bicontravariant.hs
+++ src/Sequoia/Bicontravariant.hs
@@ -10,8 +10,8 @@ module Sequoia.Bicontravariant
 class Bicontravariant p where
   contrabimap :: (a' -> a) -> (b' -> b) -> a `p` b -> a' `p` b'
 
-contrafirst :: Bicontravariant p => (a' -> a) -> a `p` b -> a' `p` b
-contrafirst = (`contrabimap` id)
+contrafirst  :: Bicontravariant p => (a' -> a) -> a `p` b -> a' `p` b
+contrafirst  = (`contrabimap` id)
 
 contrasecond :: Bicontravariant p => (b' -> b) -> a `p` b -> a `p` b'
 contrasecond = (id `contrabimap`)
