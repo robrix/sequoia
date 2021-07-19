2013d61d686ea3e7121959907bd5b06a567941c5
Rob Rix <rob.rix@me.com>
2021-07-19 12:18:34 -0400

    Organize.

--- src/Sequoia/Profunctor/Coexponential.hs
+++ src/Sequoia/Profunctor/Coexponential.hs
@@ -1,10 +1,17 @@
 module Sequoia.Profunctor.Coexponential
-( runCoexp
-, Coexp(..)
+( -- * Coexponential profunctor
+  Coexp(..)
+  -- * Elimination
+, runCoexp
 ) where
 
-runCoexp :: Coexp e r b a -> (a -> b) -> (e -> r)
-runCoexp (Coexp a b) = (b .) . (. a)
+-- Coexponential profunctor
 
 data Coexp e r b a = Coexp { coexpA :: e -> a, coexpB :: b -> r }
   deriving (Functor)
+
+
+-- Elimination
+
+runCoexp :: Coexp e r b a -> (a -> b) -> (e -> r)
+runCoexp (Coexp a b) = (b .) . (. a)
