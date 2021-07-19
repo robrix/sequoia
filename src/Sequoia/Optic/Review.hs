d555f2afc41d238cf3a3cbcea64864dd9d9b4764
Rob Rix <rob.rix@me.com>
2021-07-19 17:34:41 -0400

    Define a constructor for Review.

--- src/Sequoia/Optic/Review.hs
+++ src/Sequoia/Optic/Review.hs
@@ -2,10 +2,13 @@ module Sequoia.Optic.Review
 ( -- * Reviews
   Review
 , Review'
+  -- * Construction
+, unto
 ) where
 
 import Data.Bifunctor
 import Data.Profunctor
+import Sequoia.Bicontravariant (lphantom)
 import Sequoia.Bijection
 
 -- Reviews
@@ -13,3 +16,9 @@ import Sequoia.Bijection
 type Review s t a b = forall p . (Bifunctor p, Profunctor p) => Optic p s t a b
 
 type Review' s a = forall p . (Bifunctor p, Profunctor p) => Optic p s s a a
+
+
+-- Construction
+
+unto :: (b -> t) -> Review s t a b
+unto f = lphantom . rmap f
