e973bcb0481197399f2f4137f18e54ce19aeaea4
Rob Rix <rob.rix@me.com>
2021-07-19 09:24:05 -0400

    Define/derive Distributive & Representable instances for Recall.

--- src/Sequoia/Profunctor/Recall.hs
+++ src/Sequoia/Profunctor/Recall.hs
@@ -1,6 +1,14 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Sequoia.Profunctor.Recall
 ( Recall(..)
 ) where
 
+import Data.Distributive
+import Data.Functor.Rep
+
 newtype Recall e a b = Recall { runRecall :: e -> b }
-  deriving (Applicative, Functor, Monad)
+  deriving (Applicative, Functor, Monad, Representable)
+
+instance Distributive (Recall s a) where
+  distribute = distributeRep
+  collect = collectRep
