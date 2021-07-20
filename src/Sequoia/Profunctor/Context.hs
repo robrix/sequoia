8abc0d8c261a1e1c11e44f403c536f2793bdebb8
Rob Rix <rob.rix@me.com>
2021-07-19 21:29:31 -0400

    Define a Distributive instance for C.

--- src/Sequoia/Profunctor/Context.hs
+++ src/Sequoia/Profunctor/Context.hs
@@ -2,5 +2,10 @@ module Sequoia.Profunctor.Context
 ( C(..)
 ) where
 
+import Data.Distributive
+
 newtype C e r = C { runC :: e -> r }
   deriving (Applicative, Functor, Monad)
+
+instance Distributive (C e) where
+  distribute r = C (\ e -> (`runC` e) <$> r)
