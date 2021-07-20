20e4ea96aa8b9743013aae7a746f1ecbbf632947
Rob Rix <rob.rix@me.com>
2021-07-19 21:34:45 -0400

    Define a Sieve instance for C.

--- src/Sequoia/Profunctor/Context.hs
+++ src/Sequoia/Profunctor/Context.hs
@@ -3,10 +3,12 @@ module Sequoia.Profunctor.Context
 ( C(..)
 ) where
 
-import Control.Category as Cat
+import Control.Category as Cat (Category)
 import Data.Distributive
+import Data.Functor.Identity
 import Data.Functor.Rep as Co
 import Data.Profunctor
+import Data.Profunctor.Sieve
 import Data.Profunctor.Traversing
 
 newtype C e r = C { runC :: e -> r }
@@ -15,3 +17,6 @@ newtype C e r = C { runC :: e -> r }
 instance Distributive (C e) where
   distribute = distributeRep
   collect = collectRep
+
+instance Sieve C Identity where
+  sieve = fmap Identity . runC
