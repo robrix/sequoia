bbe7bfd343f446e45874f92a193bf90d3f96b9c0
Rob Rix <rob.rix@me.com>
2021-07-19 23:54:43 -0400

    Map over the continuations in Snk.

--- src/Sequoia/Functor/Sink.hs
+++ src/Sequoia/Functor/Sink.hs
@@ -1,11 +1,14 @@
 module Sequoia.Functor.Sink
 ( -- * Sinks
   Snk(..)
+  -- * Computation
+, mapSnkK
 ) where
 
 import Data.Functor.Contravariant
 import Sequoia.Disjunction
 import Sequoia.Functor.Applicative
+import Sequoia.Functor.K
 import Sequoia.Functor.V
 import Sequoia.Profunctor.Context
 import Sequoia.Value
@@ -19,3 +22,9 @@ instance Contravariant (Snk e r) where
 
 instance Contrapply (Snk e r) where
   contraliftA2 f a b = Snk (val ((runSnk a . inV0 <--> runSnk b . inV0) . f))
+
+
+-- Computation
+
+mapSnkK :: (forall x . K r x -> K r' x) -> (Snk e r a -> Snk e r' a)
+mapSnkK f = Snk . fmap (mapCK f) . runSnk
