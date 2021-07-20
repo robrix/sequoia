df530dbd591d5f1bb6f4602afba5ba184f9f33d8
Rob Rix <rob.rix@me.com>
2021-07-20 13:25:48 -0400

    Define an eliminator for Traversals.

--- src/Sequoia/Optic/Traversal.hs
+++ src/Sequoia/Optic/Traversal.hs
@@ -1,11 +1,20 @@
 module Sequoia.Optic.Traversal
 ( -- * Traversals
   Traversal
+  -- * Elimination
+, traverseOf
 ) where
 
+import Data.Profunctor
 import Data.Profunctor.Traversing
 import Sequoia.Optic.Optic
 
 -- Traversals
 
 type Traversal s t a b = forall p . Traversing p => Optic p s t a b
+
+
+-- Elimination
+
+traverseOf :: Applicative f => Traversal s t a b -> ((a -> f b) -> (s -> f t))
+traverseOf o = runStar . o . Star
