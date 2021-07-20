5341963a57e6728b087d7ffc93d83e9156684936
Rob Rix <rob.rix@me.com>
2021-07-20 08:28:05 -0400

    Define an identity Exchange.

--- src/Sequoia/Profunctor/Exchange.hs
+++ src/Sequoia/Profunctor/Exchange.hs
@@ -1,11 +1,22 @@
 module Sequoia.Profunctor.Exchange
-( Exchange(..)
+( -- * Exchange profunctor
+  Exchange(..)
+  -- * Construction
+, idExchange
 ) where
 
 import Data.Profunctor
+
+-- Exchange profunctor
 
 data Exchange a b s t = Exchange (s -> a) (b -> t)
   deriving (Functor)
 
 instance Profunctor (Exchange a b) where
   dimap f g (Exchange sa bt) = Exchange (sa . f) (g . bt)
+
+
+-- Construction
+
+idExchange :: Exchange a b a b
+idExchange = Exchange id id
