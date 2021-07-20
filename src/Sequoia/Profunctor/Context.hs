2b3d72f20b55e61a67d2a632059e27e57fb2a48d
Rob Rix <rob.rix@me.com>
2021-07-19 21:30:06 -0400

    Derive a Representable instance for C.

--- src/Sequoia/Profunctor/Context.hs
+++ src/Sequoia/Profunctor/Context.hs
@@ -1,11 +1,13 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Sequoia.Profunctor.Context
 ( C(..)
 ) where
 
 import Data.Distributive
+import Data.Functor.Rep as Co
 
 newtype C e r = C { runC :: e -> r }
-  deriving (Applicative, Functor, Monad)
+  deriving (Applicative, Functor, Monad, Co.Representable)
 
 instance Distributive (C e) where
   distribute r = C (\ e -> (`runC` e) <$> r)
