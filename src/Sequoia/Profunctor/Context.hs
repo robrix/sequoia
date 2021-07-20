0e94ee50770b92a0f7489c536ff49de1096c39e6
Rob Rix <rob.rix@me.com>
2021-07-19 21:32:45 -0400

    Derive a number of other instances.

--- src/Sequoia/Profunctor/Context.hs
+++ src/Sequoia/Profunctor/Context.hs
@@ -3,11 +3,14 @@ module Sequoia.Profunctor.Context
 ( C(..)
 ) where
 
+import Control.Category as Cat
 import Data.Distributive
 import Data.Functor.Rep as Co
+import Data.Profunctor
+import Data.Profunctor.Traversing
 
 newtype C e r = C { runC :: e -> r }
-  deriving (Applicative, Functor, Monad, Co.Representable)
+  deriving (Applicative, Cat.Category, Choice, Closed, Cochoice, Costrong, Functor, Mapping, Monad, Profunctor, Co.Representable, Strong, Traversing)
 
 instance Distributive (C e) where
   distribute = distributeRep
