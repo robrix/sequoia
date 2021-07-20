aa939bbe42ab1163da96504cc971afd861f57633
Rob Rix <rob.rix@me.com>
2021-07-20 09:22:23 -0400

    Define Exchange negatively.

--- src/Sequoia/Profunctor/Exchange.hs
+++ src/Sequoia/Profunctor/Exchange.hs
@@ -2,33 +2,33 @@ module Sequoia.Profunctor.Exchange
 ( -- * Exchange profunctor
   Exchange(..)
   -- * Construction
+, exchange
 , idExchange
   -- * Elimination
 , runExchange
-, withExchange
 ) where
 
 import Data.Profunctor
 
 -- Exchange profunctor
 
-data Exchange a b s t = Exchange (s -> a) (b -> t)
+newtype Exchange a b s t = Exchange { withExchange :: forall r . ((s -> a) -> (b -> t) -> r) -> r }
   deriving (Functor)
 
 instance Profunctor (Exchange a b) where
-  dimap f g (Exchange sa bt) = Exchange (sa . f) (g . bt)
+  dimap f g e = withExchange e (\ sa bt -> exchange (sa . f) (g . bt))
 
 
 -- Construction
 
+exchange :: (s -> a) -> (b -> t) -> Exchange a b s t
+exchange v r = Exchange (\ f -> f v r)
+
 idExchange :: Exchange a b a b
-idExchange = Exchange id id
+idExchange = exchange id id
 
 
 -- Elimination
 
 runExchange :: Exchange a b s t -> ((a -> b) -> (s -> t))
 runExchange e = withExchange e (\ sa bt -> (bt .) . (. sa))
-
-withExchange :: Exchange a b s t -> (((s -> a) -> (b -> t) -> r) -> r)
-withExchange (Exchange sa bt) f = f sa bt
