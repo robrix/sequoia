b027a6cf0141a069152b0c9f8c4371b110c3282d
Rob Rix <rob.rix@me.com>
2021-07-19 22:20:32 -0400

    Define a Res1 instance for Src.

--- src/Sequoia/Functor/Source.hs
+++ src/Sequoia/Functor/Source.hs
@@ -3,6 +3,7 @@ module Sequoia.Functor.Source
   Src(..)
 ) where
 
+import Sequoia.Continuation
 import Sequoia.Functor.K
 import Sequoia.Profunctor.Context
 import Sequoia.Value
@@ -13,3 +14,7 @@ newtype Src e r b = Src { runSrc :: K r b -> C e r }
 
 instance Env1 Src where
   env1 f = Src (\ k -> env ((`runSrc` k) . f))
+
+instance Res1 Src where
+  res1 = Src . const . res
+  liftRes1 f = Src (\ k -> liftRes (\ run -> runSrc (f (run . (`runSrc` k))) k))
