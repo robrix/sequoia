9c525038ff1afc17a06a459dd6943f5d93867629
Rob Rix <rob.rix@me.com>
2021-07-20 08:27:02 -0400

    Define a Profunctor instance for Exchange.

--- src/Sequoia/Profunctor/Exchange.hs
+++ src/Sequoia/Profunctor/Exchange.hs
@@ -2,5 +2,10 @@ module Sequoia.Profunctor.Exchange
 ( Exchange(..)
 ) where
 
+import Data.Profunctor
+
 data Exchange a b s t = Exchange (s -> a) (b -> t)
   deriving (Functor)
+
+instance Profunctor (Exchange a b) where
+  dimap f g (Exchange sa bt) = Exchange (sa . f) (g . bt)
