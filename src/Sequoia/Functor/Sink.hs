93ec4de499f64a27f4d4293f3f6cf13c0a542df2
Rob Rix <rob.rix@me.com>
2021-07-20 12:18:44 -0400

    Define an optic converting between Snk and Exp.

--- src/Sequoia/Functor/Sink.hs
+++ src/Sequoia/Functor/Sink.hs
@@ -5,10 +5,13 @@ module Sequoia.Functor.Sink
   -- * Computation
 , mapSnkK
 , mapSnkV
+  -- * Optics
+, _SnkExp
 ) where
 
 import Data.Functor.Contravariant
 import Data.Profunctor
+import Sequoia.Continuation
 import Sequoia.Disjunction
 import Sequoia.Functor.Applicative
 import Sequoia.Functor.K
@@ -18,6 +21,7 @@ import Sequoia.Optic.Iso
 import Sequoia.Optic.Review
 import Sequoia.Optic.Setter
 import Sequoia.Profunctor.Context
+import Sequoia.Profunctor.Exponential
 import Sequoia.Value
 
 -- Sinks
@@ -41,3 +45,9 @@ mapSnkK f = over _Snk (fmap (mapCK f))
 
 mapSnkV :: (forall x . V e x <-> V e' x) -> (Snk e r a -> Snk e' r a)
 mapSnkV b = over _Snk (dimap (review b) (mapCV (view b)))
+
+
+-- Optics
+
+_SnkExp :: (Exponential f, Exponential f') => Iso (Snk e r a) (Snk e' r' a') (f e r a r) (f' e' r' a' r')
+_SnkExp = _Snk.from (_Exponential.rmapping (constant idK))
