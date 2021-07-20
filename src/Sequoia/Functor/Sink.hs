7f2e31a46a66b2ed6e6da870b4c91e2a23216665
Rob Rix <rob.rix@me.com>
2021-07-19 23:47:58 -0400

    Define a Contravariant instance for Snk.

--- src/Sequoia/Functor/Sink.hs
+++ src/Sequoia/Functor/Sink.hs
@@ -3,9 +3,13 @@ module Sequoia.Functor.Sink
   Snk(..)
 ) where
 
+import Data.Functor.Contravariant
 import Sequoia.Functor.V
 import Sequoia.Profunctor.Context
 
 -- Sinks
 
 newtype Snk e r a = Snk { runSnk :: V e a -> C e r }
+
+instance Contravariant (Snk e r) where
+  contramap f = Snk . (. fmap f) . runSnk
