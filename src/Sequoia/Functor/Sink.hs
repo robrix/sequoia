fc593a54e2e036916c746da4589c1a4a4de76ccf
Rob Rix <rob.rix@me.com>
2021-07-19 23:51:40 -0400

    Define a Contrapply instance for Snk.

--- src/Sequoia/Functor/Sink.hs
+++ src/Sequoia/Functor/Sink.hs
@@ -4,8 +4,11 @@ module Sequoia.Functor.Sink
 ) where
 
 import Data.Functor.Contravariant
+import Sequoia.Disjunction
+import Sequoia.Functor.Applicative
 import Sequoia.Functor.V
 import Sequoia.Profunctor.Context
+import Sequoia.Value
 
 -- Sinks
 
@@ -13,3 +16,6 @@ newtype Snk e r a = Snk { runSnk :: V e a -> C e r }
 
 instance Contravariant (Snk e r) where
   contramap f = Snk . (. fmap f) . runSnk
+
+instance Contrapply (Snk e r) where
+  contraliftA2 f a b = Snk (val ((runSnk a . inV0 <--> runSnk b . inV0) . f))
