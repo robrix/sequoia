f14f46d41da3b9b78bafb590866d4297829524f8
Rob Rix <rob.rix@me.com>
2021-07-19 22:16:53 -0400

    Define an Env1 instance for Src.

--- src/Sequoia/Functor/Source.hs
+++ src/Sequoia/Functor/Source.hs
@@ -5,7 +5,11 @@ module Sequoia.Functor.Source
 
 import Sequoia.Functor.K
 import Sequoia.Profunctor.Context
+import Sequoia.Value
 
 -- Sources
 
 newtype Src e r b = Src { runSrc :: K r b -> C e r }
+
+instance Env1 Src where
+  env1 f = Src (\ k -> env ((`runSrc` k) . f))
