8ca3f0d8d0900435c3f1077363ca601407842c7a
Rob Rix <rob.rix@me.com>
2021-07-21 19:03:31 -0400

    :fire: dead code in Continuation.

--- src/Sequoia/Continuation.hs
+++ src/Sequoia/Continuation.hs
@@ -12,48 +12,22 @@ module Sequoia.Continuation
 , inK
 , inK1
 , inK1'
-, inK2
-, negK
 , negK2
   -- ** Elimination
 , exK
-, exK1
-, exK2
 , (•)
   -- ** Coercion
 , _K
-, coerceKWith
-, coercedK
-, coerceK
-, coerceK1
-, coerceK2
   -- ** Category
 , idK
-, composeK
   -- ** Composition
-, (<<•)
-, (•>>)
 , (<••>)
 , (<•••>)
-, (>>-)
-, (-<<)
-  -- ** Computation
-, mapKRep
   -- * Double negation
 , type (**)
-, ContFn
-, _DN
 , mapDN
-, hoistDN
   -- ** Construction
 , liftDN
-, inDN
-, inDN1
-, inDN2
-  -- ** Elimination
-, exDN
-, exDN1
-, exDN2
   -- * Ambient control
 , Res(..)
 , cont
@@ -69,9 +43,7 @@ import Data.Functor.Contravariant.Rep
 import Data.Profunctor
 import Sequoia.Disjunction
 import Sequoia.Functor.K
-import Sequoia.Optic.Getter
 import Sequoia.Optic.Iso
-import Sequoia.Optic.Review
 import Sequoia.Optic.Setter
 import Sequoia.Profunctor.Recall
 
@@ -101,10 +73,6 @@ inK2 :: Representable k => (KFn k a -> KFn k b -> KFn k c) -> (k a -> k b -> k c
 inK2 f a b = inK (exK a `f` exK b)
 
 
--- | Negate a unary function by translating it to operate on continuations.
-negK :: Contravariant k => (a -> b) -> (k b -> k a)
-negK = contramap
-
 -- | Negate a binary function by translating it to operate on continuations.
 negK2 :: Contravariant k => (a -> b -> c) -> (k (k c -> k b) -> k a)
 negK2 = contramap . (contramap .)
@@ -112,17 +80,8 @@ negK2 = contramap . (contramap .)
 
 -- Elimination
 
-exK :: Representable k =>       k a -> KFn k a
+exK, (•) :: Representable k => k a -> KFn k a
 exK = index
-
-exK1 :: Representable k => (k a -> k b) -> (KFn k a -> KFn k b)
-exK1 = under _K
-
-exK2 :: Representable k => (k a -> k b -> k c) -> (KFn k a -> KFn k b -> KFn k c)
-exK2 f a b = exK (inK a `f` inK b)
-
-
-(•) :: Representable k => k a -> KFn k a
 (•) = index
 
 infixl 7 •
@@ -134,41 +93,13 @@ _K :: (Representable k, Representable k') => Iso (k a) (k' a') (KFn k a) (KFn k'
 _K = from contratabulated
 
 
-coerceKWith :: (Representable k1, Representable k2) => (KFn k1 a -> KFn k2 b) -> (k1 a -> k2 b)
-coerceKWith = over _K
-
-coercedK :: (Representable k1, Representable k2, Representable k1', Representable k2', Rep k1 ~ Rep k2, Rep k1' ~ Rep k2') => Iso (k1 a) (k1' a') (k2 a) (k2' a')
-coercedK = coerceK <-> coerceK
-
-coerceK :: (Representable k1, Representable k2, Rep k1 ~ Rep k2) => (k1 a -> k2 a)
-coerceK = inK . exK
-
-coerceK1 :: (Representable k1, Representable k2, Rep k1 ~ Rep k2) => (k1 a -> k1 b) -> (k2 a -> k2 b)
-coerceK1 = inK1 . exK1
-
-coerceK2 :: (Representable k1, Representable k2, Rep k1 ~ Rep k2) => (k1 a -> k1 b -> k1 c) -> (k2 a -> k2 b -> k2 c)
-coerceK2 = inK2 . exK2
-
-
 -- Category
 
 idK :: Representable k => k (KRep k)
 idK = inK id
 
-composeK :: (Representable j, Representable k) => j a -> k (KRep j) -> k a
-composeK j k = inK (exK k . exK j)
-
 
 -- Composition
-
-(<<•) :: (Representable j, Representable k) => (KRep j -> KRep k) -> (j a -> k a)
-f <<• k = inK (f . exK k)
-
-(•>>) :: (Representable j, Representable k) => j a -> (KRep j -> KRep k) -> k a
-k •>> f = inK (f . exK k)
-
-infixr 1 <<•, •>>
-
 
 (<••>) :: (Disj d, Representable k) => k a -> k b -> k (a `d` b)
 (<••>) = inK2 (<-->)
@@ -180,23 +111,6 @@ infix 3 <••>
 (<•••>) = liftA2 (<••>)
 
 infix 3 <•••>
-
-
-(>>-) :: Representable k => a -> (b -> k a) -> k b
-a >>- f = inK ((• a) . f)
-
-infixl 1 >>-
-
-(-<<) :: Representable k => (b -> k a) -> (a -> k b)
-f -<< a = inK ((• a) . f)
-
-infixr 1 -<<
-
-
--- Computation
-
-mapKRep :: (Representable k, Representable k') => (KRep k -> KRep k') -> (k a -> k' a)
-mapKRep f = inK . (f .) . exK
 
 
 -- Double negation
@@ -216,9 +130,6 @@ _DN = inDN <-> exDN
 mapDN :: Representable k => (a -> b) -> (k **a -> k **b)
 mapDN f = inK1 (lmap (contramap f))
 
-hoistDN :: Representable j => (forall x . j x <-> k x) -> (j **a -> k **a)
-hoistDN b = (^. b) . contramap (review b)
-
 
 -- Construction
 
@@ -228,23 +139,11 @@ liftDN = inK . flip exK
 inDN :: Representable k => ContFn k a -> k **a
 inDN = inK . lmap exK
 
-inDN1 :: Representable k => (ContFn k a -> ContFn k b) -> (k **a -> k **b)
-inDN1 = dimap exDN inDN
-
-inDN2 :: Representable k => (ContFn k a -> ContFn k b -> ContFn k c) -> (k **a -> k **b -> k **c)
-inDN2 f a b = inDN (exDN a `f` exDN b)
-
 
 -- Elimination
 
 exDN :: Representable k => k **a -> ContFn k a
 exDN = lmap inK . exK
-
-exDN1 :: Representable k => (k **a -> k **b) -> (ContFn k a -> ContFn k b)
-exDN1 = dimap inDN exDN
-
-exDN2 :: Representable k => (k **a -> k **b -> k **c) -> (ContFn k a -> ContFn k b -> ContFn k c)
-exDN2 f a b = exDN (inDN a `f` inDN b)
 
 
 -- Ambient control
