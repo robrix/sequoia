f124df2f3bacfd625bbe13b9505b63ca19fbd276
Rob Rix <rob.rix@me.com>
2021-07-21 23:43:57 -0400

    Define an Env class for the V profunctor.

--- src/Sequoia/Profunctor/Value.hs
+++ src/Sequoia/Profunctor/Value.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE UndecidableInstances #-}
 module Sequoia.Profunctor.Value
 ( -- * Value profunctor
   V(..)
   -- * Coercion
 , _V
+  -- * Ambient environment
+, Env(..)
+, val
 ) where
 
 import Control.Category (Category)
+import Control.Monad (join)
 import Data.Distributive
 import Data.Functor.Identity
 import Data.Functor.Rep as Co
@@ -15,11 +20,12 @@ import Data.Profunctor.Rep as Pro
 import Data.Profunctor.Sieve
 import Data.Profunctor.Traversing
 import Sequoia.Optic.Iso
+import Sequoia.Profunctor.Recall
 
 -- Value profunctor
 
 newtype V e a = V { (∘) :: e -> a }
-  deriving (Applicative, Category, Choice, Closed, Cochoice, Pro.Corepresentable, Costrong, Functor, Mapping, Monad, Profunctor, Co.Representable, Pro.Representable, Strong, Traversing)
+  deriving (Applicative, Category, Choice, Closed, Cochoice, Pro.Corepresentable, Costrong, Env e, Functor, Mapping, Monad, Profunctor, Co.Representable, Pro.Representable, Strong, Traversing)
 
 infix 8 ∘
 
@@ -38,3 +44,21 @@ instance Cosieve V Identity where
 
 _V :: Iso (V e a) (V e' a') (e -> a) (e' -> a')
 _V = coerced
+
+
+-- Ambient environment
+
+class Env e c | c -> e where
+  env :: (e -> c) -> c
+
+instance Env e (e -> a) where
+  env = join
+
+instance Env e (Forget r e b) where
+  env = Forget . (runForget =<<)
+
+instance Env e (Recall e a b) where
+  env = Recall . (runRecall =<<)
+
+val :: Env e c => (a -> c) -> (V e a -> c)
+val f v = env (f . (v ∘))
