8928b7f3850ce5bf348540d9b3a5d4112567244b
Rob Rix <rob.rix@me.com>
2021-07-22 12:49:38 -0400

    :fire: parens.

--- src/Sequoia/Profunctor/Exponential.hs
+++ src/Sequoia/Profunctor/Exponential.hs
@@ -167,7 +167,7 @@ appExp2 f = V (\ e a b -> K (\ c -> exExp f a (K (\ g -> exExp g b c <== e)) <==
 runExp :: Exponential f => e ∘ a -> b • r -> a --|f e r|-> b -> e ==> r
 runExp v k f = exExp f v k
 
-elimExp :: (Exponential f) => a --|f e r|-> b -> Coexp e r b a -> e ==> r
+elimExp :: Exponential f => a --|f e r|-> b -> Coexp e r b a -> e ==> r
 elimExp f = unCoexp (exExp f)
 
 
