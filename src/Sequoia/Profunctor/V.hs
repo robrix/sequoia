6ec3716e1967c6e1b4de3a760b71de4d70d62e29
Rob Rix <rob.rix@me.com>
2021-07-21 20:14:41 -0400

    Redefine the V profunctor as an identity profunctor.

--- src/Sequoia/Profunctor/V.hs
+++ src/Sequoia/Profunctor/V.hs
@@ -4,40 +4,23 @@ module Sequoia.Profunctor.V
 ( V(..)
 ) where
 
-import           Data.Distributive
-import           Data.Functor.Const
-import           Data.Functor.Rep
-import           Data.Profunctor
-import qualified Data.Profunctor.Rep as Pro
-import           Data.Profunctor.Sieve
+import Data.Distributive
+import Data.Functor.Identity
+import Data.Functor.Rep as Co
+import Data.Profunctor
+import Data.Profunctor.Rep as Pro
+import Data.Profunctor.Sieve
+import Data.Profunctor.Traversing
 
-newtype V s a b = V { runV :: s -> b }
-  deriving (Applicative, Functor, Monad, Representable)
+newtype V e a = V { runV :: e -> a }
+  deriving (Applicative, Choice, Closed, Cochoice, Pro.Corepresentable, Costrong, Functor, Mapping, Monad, Profunctor, Co.Representable, Pro.Representable, Strong, Traversing)
 
-instance Distributive (V s a) where
-  distribute r = V (\ s -> (`runV` s)     <$> r)
-  collect  f r = V (\ s -> (`runV` s) . f <$> r)
+instance Distributive (V e) where
+  distribute = distributeRep
+  collect = collectRep
 
-instance Profunctor (V s) where
-  dimap _ g = V . rmap g . runV
+instance Sieve V Identity where
+  sieve = rmap Identity . runV
 
-instance Costrong (V s) where
-  unfirst  = V . fmap fst . runV
-  unsecond = V . fmap snd . runV
-
-instance Choice (V s) where
-  left'  = V . fmap Left  . runV
-  right' = V . fmap Right . runV
-
-instance Closed (V s) where
-  closed = V . fmap const . runV
-
-instance Sieve (V s) ((->) s) where
-  sieve = const . runV
-
-instance Cosieve (V s) (Const s) where
-  cosieve = lmap getConst . runV
-
-instance Pro.Corepresentable (V s) where
-  type Corep (V s) = Const s
-  cotabulate = V . lmap Const
+instance Cosieve V Identity where
+  cosieve = lmap runIdentity . runV
