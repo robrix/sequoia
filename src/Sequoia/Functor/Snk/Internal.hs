b61e50d05c265596535ae6f364af8a7d62fdbfab
Rob Rix <rob.rix@me.com>
2021-07-23 14:11:58 -0400

    :fire: the dependency on Disj.

--- src/Sequoia/Functor/Snk/Internal.hs
+++ src/Sequoia/Functor/Snk/Internal.hs
@@ -3,7 +3,6 @@ module Sequoia.Functor.Snk.Internal
 ) where
 
 import Data.Functor.Contravariant
-import Sequoia.Disjunction
 import Sequoia.Functor.Applicative
 import Sequoia.Profunctor.Context
 import Sequoia.Profunctor.Value
@@ -14,4 +13,4 @@ instance Contravariant (Snk e r) where
   contramap f = Snk . (. fmap f) . runSnk
 
 instance Contrapply (Snk e r) where
-  contraliftA2 f a b = Snk (val ((runSnk a . pure <--> runSnk b . pure) . f))
+  contraliftA2 f a b = Snk (val (either (runSnk a . pure) (runSnk b . pure) . f))
