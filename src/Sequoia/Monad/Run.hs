ad10c8dc5a158d42ca1f5f185dddb2c95826b2dd
Rob Rix <rob.rix@me.com>
2021-07-24 09:59:14 -0400

    Define a MonadRun class.

--- src/Sequoia/Monad/Run.hs
+++ src/Sequoia/Monad/Run.hs
@@ -1,2 +1,10 @@
+-- | Lowering (running) “pure” monads to a value.
 module Sequoia.Monad.Run
-() where
+( -- * Lowering
+  MonadRun(..)
+) where
+
+-- Lowering
+
+class Monad m => MonadRun m where
+  withRun :: ((m r -> r) -> m a) -> m a
