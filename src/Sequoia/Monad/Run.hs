e80410031cc9b2aef0466129eef61f806ee4ba73
Rob Rix <rob.rix@me.com>
2021-07-24 13:47:34 -0400

    Give a default definition of distribute in terms of MonadRun.

--- src/Sequoia/Monad/Run.hs
+++ src/Sequoia/Monad/Run.hs
@@ -4,6 +4,8 @@ module Sequoia.Monad.Run
   MonadRun(..)
   -- * Construction
 , fn
+  -- * Defaults
+, distributeRun
 ) where
 
 import Data.Functor.Identity
@@ -24,3 +26,9 @@ instance MonadRun Identity where
 
 fn :: MonadRun m => (a -> m r) -> m (a -> r)
 fn b = withRun (\ run -> pure (run . b))
+
+
+-- Defaults
+
+distributeRun :: (MonadRun f, Functor g) => g (f a) -> f (g a)
+distributeRun g = withRun (\ run -> pure (run <$> g))
