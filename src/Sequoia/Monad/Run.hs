2d39d99321f97fb9fabe2f3dcdf1856f04dea26b
Rob Rix <rob.rix@me.com>
2021-07-24 10:00:33 -0400

    Define a MonadRun instance for (->) a.

--- src/Sequoia/Monad/Run.hs
+++ src/Sequoia/Monad/Run.hs
@@ -4,7 +4,12 @@ module Sequoia.Monad.Run
   MonadRun(..)
 ) where
 
+import Data.Function
+
 -- Lowering
 
 class Monad m => MonadRun m where
   withRun :: ((m r -> r) -> m a) -> m a
+
+instance MonadRun ((->) a) where
+  withRun b = b =<< (&)
