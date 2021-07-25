f04768f92547ab6b0294f6783f5ef8e6e77ac2a7
Rob Rix <rob.rix@me.com>
2021-07-25 06:41:17 -0400

    Rename parameters.

--- src/Sequoia/Monad/Trans/It.hs
+++ src/Sequoia/Monad/Trans/It.hs
@@ -17,23 +17,23 @@ import Control.Monad.Trans.Class
 newtype ItT r m a = ItT { getItT :: forall s . (a -> m s) -> ((r -> ItT r m a) -> m s) -> m s }
 
 instance Functor (ItT r m) where
-  fmap f r = ItT (\ a k -> getItT r (a . f) (k . fmap (fmap f)))
+  fmap f i = ItT (\ k r -> getItT i (k . f) (r . fmap (fmap f)))
 
 instance Applicative (ItT r m) where
   pure = doneItT
   (<*>) = ap
 
 instance Monad (ItT r m) where
-  r >>= f = go r where go r = ItT (\ a k -> runItT (runItT a k . f) (k . fmap go) r)
+  i >>= f = go i where go i = ItT (\ k r -> runItT (runItT k r . f) (r . fmap go) i)
 
 instance MonadTrans (ItT r) where
-  lift m = ItT (\ a _ -> m >>= a)
+  lift m = ItT (\ k _ -> m >>= k)
 
 
 -- Construction
 
 itT :: (r -> ItT r m a) -> ItT r m a
-itT k = ItT (const ($ k))
+itT r = ItT (const ($ r))
 
 doneItT :: a -> ItT r m a
 doneItT a = ItT (const . ($ a))
@@ -42,7 +42,7 @@ doneItT a = ItT (const . ($ a))
 -- Elimination
 
 runItT :: (a -> m s) -> ((r -> ItT r m a) -> m s) -> (ItT r m a -> m s)
-runItT p k i = getItT i p k
+runItT k r i = getItT i k r
 
 foldItT :: (a -> m s) -> ((r -> m s) -> m s) -> (ItT r m a -> m s)
-foldItT p k = go where go = runItT p (k . fmap go)
+foldItT k r = go where go = runItT k (r . fmap go)
