4d78ba16f59d1d849f335b32e56b01b748bb7818
Rob Rix <rob.rix@me.com>
2021-07-25 17:54:29 -0400

    Flip Sig.

--- src/Sequoia/Signal.hs
+++ src/Sequoia/Signal.hs
@@ -32,33 +32,33 @@ import           Sequoia.Profunctor.Value as V
 
 -- Signals
 
-_Sig :: Iso (Sig e r a b) (Sig e' r' a' b') (e ∘ a -> b • r -> e ==> r) (e' ∘ a' -> b' • r' -> e' ==> r')
+_Sig :: Iso (Sig e r a b) (Sig e' r' a' b') (b • r -> e ∘ a -> e ==> r) (b' • r' -> e' ∘ a' -> e' ==> r')
 _Sig = coerced
 
-newtype Sig e r a b = Sig { runSig :: e ∘ a -> b • r -> e ==> r }
+newtype Sig e r a b = Sig { runSig :: b • r -> e ∘ a -> e ==> r }
 
 instance Cat.Category (Sig e r) where
-  id = Sig (flip (↓↑))
-  Sig f . Sig g = Sig (\ a c -> cont (\ _K -> g a (_K ((`f` c) . pure))))
+  id = Sig (↓↑)
+  Sig f . Sig g = Sig (\ c a -> cont (\ _K -> g (_K (f c . pure)) a))
 
 instance Profunctor (Sig e r) where
-  dimap f g = Sig . dimap (fmap f) (lmap (lmap g)) . runSig
+  dimap f g = Sig . dimap (lmap g) (lmap (fmap f)) . runSig
 
 instance Functor (Sig e r a) where
   fmap = rmap
 
 instance Applicative (Sig e r a) where
-  pure a = Sig (flip (ckv (const a)))
+  pure a = Sig (ckv (const a))
   (<*>) = ap
 
 instance Monad (Sig e r a) where
-  Sig m >>= f = Sig (\ a b -> cont (\ _K -> m a (_K (\ a' -> runSig (f a') a b))))
+  Sig m >>= f = Sig (\ b a -> cont (\ _K -> m (_K (\ a' -> runSig (f a') b a)) a))
 
 mapKSig :: (forall x . x • r <-> x • r') -> (Sig e r a b -> Sig e r' a b)
-mapKSig b = Sig . fmap (dimap (review b) (mapCK (view b))) . runSig
+mapKSig b = Sig . dimap (review b) (fmap (mapCK (view b))) . runSig
 
 mapVSig :: (forall x . e ∘ x <-> e' ∘ x) -> (Sig e r a b -> Sig e' r a b)
-mapVSig b = Sig . dimap (review b) (rmap (mapCV (view b))) . runSig
+mapVSig b = Sig . fmap (dimap (review b) (mapCV (view b))) . runSig
 
 
 -- Conversions
@@ -81,7 +81,7 @@ srcSig
   ::      Src e r |- b
             <->
      e -| Sig e r |- b
-srcSig = _Src.from (_Sig.constantWith (V id) (flip ((.) . (∘>>))))
+srcSig = _Src.from (_Sig.rmapping (constantWith (V id) (<<∘)))
 
 composeSrcSig :: Src e r a -> Sig e r a b -> Src e r b
 composeSrcSig src sig = review srcSig (sig <<< view srcSig src)
@@ -91,7 +91,7 @@ snkSig
   :: a -| Snk e r
             <->
      a -| Sig e r |- r
-snkSig = _Snk.from (_Sig.rmapping (constantWith (K id) (>>•)))
+snkSig = _Snk.from (_Sig.constantWith (K id) (flip ((.) . (•<<))))
 
 composeSigSnk :: Sig e r a b -> Snk e r b -> Snk e r a
 composeSigSnk sig snk = review snkSig (view snkSig snk <<< sig)
@@ -101,7 +101,7 @@ solSig
   ::      e ==> r
             <->
      e -| Sig e r |- r
-solSig = Sig . const . const <-> ($ K id) . ($ V id) . runSig
+solSig = Sig . const . const <-> ($ V id) . ($ K id) . runSig
 
 
 composeSrcSnk :: Src e r a -> Snk e r a -> e ==> r
