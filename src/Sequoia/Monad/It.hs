9dd99cdbc80f133296e907779e69c74381d9398a
Rob Rix <rob.rix@me.com>
2021-07-24 20:20:57 -0400

    Define a general fold for It.

--- src/Sequoia/Monad/It.hs
+++ src/Sequoia/Monad/It.hs
@@ -1,9 +1,17 @@
 module Sequoia.Monad.It
 ( -- * Iteratees
   It(..)
+  -- * Elimination
+, foldIt
 ) where
 
 -- Iteratees
 
 -- | Böhm-Berarducci–encoded iteratee, based loosely on the one in @trifecta@.
 newtype It r a = It (forall s . (a -> s) -> (a -> (r -> s) -> s) -> s)
+
+
+-- Elimination
+
+foldIt :: (a -> o) -> (a -> (r -> o) -> o) -> It r a -> o
+foldIt p k (It r) = r p k
