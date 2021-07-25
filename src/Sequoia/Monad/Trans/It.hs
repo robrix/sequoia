1b5a41bd3b3fbab08078dfd7f71c87100a38df1d
Rob Rix <rob.rix@me.com>
2021-07-25 06:15:36 -0400

    Define a constructor for ItT.

--- src/Sequoia/Monad/Trans/It.hs
+++ src/Sequoia/Monad/Trans/It.hs
@@ -1,8 +1,16 @@
 module Sequoia.Monad.Trans.It
 ( -- * Iteratees
   ItT(..)
+  -- * Construction
+, itT
 ) where
 
 -- Iteratees
 
 newtype ItT r m a = ItT { getItT :: forall s . (a -> s) -> ((r -> m (ItT r m a)) -> s) -> s }
+
+
+-- Construction
+
+itT :: (r -> m (ItT r m a)) -> ItT r m a
+itT k = ItT (const ($ k))
