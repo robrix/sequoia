fdcc520d02bc176d7120aea01d44e9a4ca79cb26
Rob Rix <rob.rix@me.com>
2021-07-24 20:19:26 -0400

    Heading.

--- src/Sequoia/Monad/It.hs
+++ src/Sequoia/Monad/It.hs
@@ -1,6 +1,9 @@
 module Sequoia.Monad.It
-( It(..)
+( -- * Iteratees
+  It(..)
 ) where
+
+-- Iteratees
 
 -- | Böhm-Berarducci–encoded iteratee, based loosely on the one in @trifecta@.
 newtype It r a = It (forall s . (a -> s) -> (a -> (r -> s) -> s) -> s)
