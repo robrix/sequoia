fa62209515541ad6f199a336faf33377988be23e
Rob Rix <rob.rix@me.com>
2021-07-26 10:54:52 -0400

    Define a Semigroup instance for Span.

--- src/Sequoia/Monad/It.hs
+++ src/Sequoia/Monad/It.hs
@@ -118,6 +118,9 @@ data Pos = Pos { line :: {-# UNPACK #-} !Int, col :: {-# UNPACK #-} !Int }
 data Span = Span { start :: {-# UNPACK #-} !Pos, end :: {-# UNPACK #-} !Pos }
   deriving (Eq, Ord, Show)
 
+instance Semigroup Span where
+  Span s1 e1 <> Span s2 e2 = Span (min s1 s2) (max e1 e2)
+
 data Line = Line { lineContents :: String, lineEnding :: Maybe Newline }
   deriving (Eq, Ord, Show)
 
