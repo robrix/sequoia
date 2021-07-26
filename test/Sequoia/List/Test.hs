b56af5125db559442d722fb254a09d941de452a9
Rob Rix <rob.rix@me.com>
2021-07-26 14:51:15 -0400

    Test that zip has the minimum length of its inputs.

--- test/Sequoia/List/Test.hs
+++ test/Sequoia/List/Test.hs
@@ -6,7 +6,7 @@ module Sequoia.List.Test
 import           Hedgehog
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
-import           Sequoia.List
+import           Sequoia.List as List
 
 tests :: IO Bool
 tests = checkParallel $$(discover)
@@ -14,6 +14,11 @@ tests = checkParallel $$(discover)
 prop_semigroup_associativity = property $ do
   (a, b, c) <- forAll ((,,) <$> genList Gen.alpha <*> genList Gen.alpha <*> genList Gen.alpha)
   ((a <> b) <> c) === (a <> (b <> c))
+
+
+prop_zip_length_minimum = property $ do
+  (as, bs) <- forAll ((,) <$> genList Gen.alpha <*> genList Gen.alpha)
+  length (List.zip as bs) === length as `min` length bs
 
 
 genList :: Gen a -> Gen (List a)
