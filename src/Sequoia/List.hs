d5c3919a1fb3cbc87037155b8d0f85b3743ff056
Rob Rix <rob.rix@me.com>
2021-07-26 13:45:22 -0400

    Rename foldList to toFoldr.

--- src/Sequoia/List.hs
+++ src/Sequoia/List.hs
@@ -35,7 +35,7 @@ type Foldl r a = (r -> a -> r) -> r -> r
 type Foldr r a = (a -> r -> r) -> r -> r
 type FoldMap m a = (m -> m -> m) -> (a -> m) -> m -> m
 
-newtype List a = List { foldList :: forall r . Foldr r a }
+newtype List a = List { toFoldr :: forall r . Foldr r a }
 
 instance Show1 List where
   liftShowsPrec _ showList _ = showList . runList
@@ -44,19 +44,19 @@ instance Show a => Show (List a) where
   showsPrec = showsPrec1
 
 instance Semigroup (List a) where
-  a <> b = fromFoldr (\ cons -> foldList a cons . foldList b cons)
+  a <> b = fromFoldr (\ cons -> toFoldr a cons . toFoldr b cons)
 
 instance Monoid (List a) where
   mempty = nil
 
 instance Functor List where
-  fmap f l = fromFoldr (foldList l . (. f))
+  fmap f l = fromFoldr (toFoldr l . (. f))
 
 instance Foldable List where
-  foldr cons nil list = foldList list cons nil
-  foldMap f list = foldList list ((<>) . f) mempty
+  foldr cons nil list = toFoldr list cons nil
+  foldMap f list = toFoldr list ((<>) . f) mempty
   toList = runList
-  null list = foldList list (const (const False)) True
+  null list = toFoldr list (const (const False)) True
 
 instance IsList (List a) where
   type Item (List a) = a
@@ -73,7 +73,7 @@ nil :: List a
 nil = fromFoldr (const id)
 
 cons :: a -> List a -> List a
-cons h t = fromFoldr (\ cons -> cons h . foldList t cons)
+cons h t = fromFoldr (\ cons -> cons h . toFoldr t cons)
 
 list :: [a] -> List a
 list as = fromFoldr (\ cons nil -> foldr cons nil as)
@@ -82,31 +82,31 @@ list as = fromFoldr (\ cons nil -> foldr cons nil as)
 -- Elimination
 
 runList :: List a -> [a]
-runList l = foldList l (:) []
+runList l = toFoldr l (:) []
 
 
 -- Computation
 
 take :: Int -> List a -> List a
-take n l = fromFoldr (\ cons nil -> foldList l (\ h t n -> if n <= 0 then nil else cons h (t (n - 1))) (const nil) n)
+take n l = fromFoldr (\ cons nil -> toFoldr l (\ h t n -> if n <= 0 then nil else cons h (t (n - 1))) (const nil) n)
 
 drop :: Int -> List a -> List a
-drop n l = fromFoldr (\ cons nil -> foldList l (\ h t n -> if n <= 0 then cons h (t n) else t (n - 1)) (const nil) n)
+drop n l = fromFoldr (\ cons nil -> toFoldr l (\ h t n -> if n <= 0 then cons h (t n) else t (n - 1)) (const nil) n)
 
 takeWhile :: (a -> Bool) -> (List a -> List a)
-takeWhile p l = fromFoldr (\ cons nil -> foldList l (\ h t -> if p h then cons h t else nil) nil)
+takeWhile p l = fromFoldr (\ cons nil -> toFoldr l (\ h t -> if p h then cons h t else nil) nil)
 
 dropWhile :: (a -> Bool) -> (List a -> List a)
-dropWhile p l = fromFoldr (\ cons nil -> foldList l (\ h t done -> if done then cons h (t done) else if p h then t done else cons h (t True)) (const nil) False)
+dropWhile p l = fromFoldr (\ cons nil -> toFoldr l (\ h t done -> if done then cons h (t done) else if p h then t done else cons h (t True)) (const nil) False)
 
 filter :: (a -> Bool) -> (List a -> List a)
-filter p l = fromFoldr (\ cons -> foldList l (\ h t -> if p h then cons h t else t))
+filter p l = fromFoldr (\ cons -> toFoldr l (\ h t -> if p h then cons h t else t))
 
 zip :: List a -> List b -> List (a, b)
 zip = zipWith (,)
 
 zipWith :: (a -> b -> c) -> (List a -> List b -> List c)
-zipWith f a b = fromFoldr (\ cons nil -> foldList a (\ ha t b -> foldList b (\ hb _ -> cons (f ha hb) (t (drop 1 b))) nil) (const nil) b)
+zipWith f a b = fromFoldr (\ cons nil -> toFoldr a (\ ha t b -> toFoldr b (\ hb _ -> cons (f ha hb) (t (drop 1 b))) nil) (const nil) b)
 
 
 data These a b
