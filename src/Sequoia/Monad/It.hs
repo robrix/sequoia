42ff26b2edfb5523b96b09d8fb7f0f39cf863bc7
Rob Rix <rob.rix@me.com>
2021-07-25 22:43:21 -0400

    :fire: incremental results.

--- src/Sequoia/Monad/It.hs
+++ src/Sequoia/Monad/It.hs
@@ -6,8 +6,6 @@ module Sequoia.Monad.It
 , input
   -- * Construction
 , needIt
-, wantIt
-, tabulateIt
 , toList
   -- * Elimination
 , foldIt
@@ -29,11 +27,8 @@ module Sequoia.Monad.It
 ) where
 
 import Control.Applicative (Alternative(..))
-import Control.Comonad
 import Control.Monad ((<=<))
-import Data.Functor.Identity
 import Data.Profunctor
-import Data.Profunctor.Sieve
 import Foreign.C.String
 import Foreign.Marshal.Alloc
 import Prelude hiding (any, take)
@@ -44,27 +39,14 @@ import System.IO
 -- | Iteratees, based loosely on the one in @trifecta@.
 data It m r a
   = Done a
-  | Roll a (Input r -> m (It m r a))
+  | Roll (Input r -> m (It m r a))
 
 instance Functor m => Profunctor (It m) where
   dimap f g = go
     where
     go = \case
-      Done a   -> Done (g a)
-      Roll a r -> Roll (g a) (fmap go . r . fmap f)
-
-instance Functor m => Costrong (It m) where
-  unfirst = \case
-    Done (b, _)   -> pure b
-    Roll (b, d) k -> Roll b (fmap unfirst . k . fmap (,d))
-  unsecond = \case
-    Done (_, b)   -> pure b
-    Roll (d, b) k -> Roll b (fmap unsecond . k . fmap (d,))
-
-instance Comonad m => Cosieve (It m) Identity where
-  cosieve = \case
-    Done a   -> const a
-    Roll _ k -> extract . extract . k . Input . runIdentity
+      Done a -> Done (g a)
+      Roll r -> Roll (fmap go . r . fmap f)
 
 instance Functor m => Functor (It m r) where
   fmap = rmap
@@ -72,36 +54,19 @@ instance Functor m => Functor (It m r) where
 instance Functor m => Applicative (It m r) where
   pure = Done
   f <*> a = case f of
-    Done f   -> f <$> a
-    Roll f k -> Roll (extract (f <$> a)) (fmap (<*> a) . k)
+    Done f -> f <$> a
+    Roll k -> Roll (fmap (<*> a) . k)
 
 instance Monad m => Monad (It m r) where
   m >>= f = go m
     where
     go = \case
       Done a   -> f a
-      Roll a k -> Roll (extract (f a)) $ \ r -> do
+      Roll k -> Roll $ \ r -> do
         kr' <- k r
         case kr' of
-          Done a'    -> simplifyIt (f a') r
-          Roll a' k' -> do
-            a'' <- indexIt (f a') r
-            pure $ Roll a'' (pure . go <=< k')
-
-instance Functor m => Comonad (It m r) where
-  extract = \case
-    Done a   -> a
-    Roll a _ -> a
-
-  duplicate = \case
-    i@Done{}     -> Done i
-    i@(Roll _ k) -> Roll i (fmap duplicate . k)
-
-  extend f = go
-    where
-    go = \case
-      i@Done{}     -> Done (f i)
-      i@(Roll _ k) -> Roll (f i) (fmap go . k)
+          Done a' -> simplifyIt (f a') r
+          Roll k' -> pure $ Roll (pure . go <=< k')
 
 
 -- Input
@@ -140,54 +105,41 @@ input e i = \case
 
 -- Construction
 
-needIt :: Applicative m => a -> (r -> m (Maybe a)) -> It m r a
-needIt a f = i where i = Roll a (input (pure i) (fmap (maybe i Done) . f))
-
-wantIt :: Applicative m => a -> (r -> m (Either a a)) -> It m r a
-wantIt a f = Roll a k where k = input (pure (pure a)) (fmap (either Done (`Roll` k)) . f)
-
-
-tabulateIt :: Applicative m => a -> (Input r -> a) -> It m r a
-tabulateIt a f = Roll a (pure . Done . f)
+needIt :: Applicative m =>  (r -> m (Maybe a)) -> It m r a
+needIt f = i where i = Roll (input (pure i) (fmap (maybe i Done) . f))
 
 
 toList :: Applicative m => It m a [a]
 toList = ($ []) <$> go id
   where
-  go as = i where i = Roll as (input (pure i) (\ a -> pure (go (as . (a:)))))
+  go as = i where i = Roll (input (pure i) (\ a -> pure (go (as . (a:)))))
 
 
 -- Elimination
 
-foldIt :: Monad m => (a -> m s) -> (a -> (Input r -> m s) -> m s) -> (It m r a -> m s)
-foldIt p k = go where go = runIt p (\ a -> k a . fmap (>>= go))
+foldIt :: Monad m => (a -> m s) -> ((Input r -> m s) -> m s) -> (It m r a -> m s)
+foldIt p k = go where go = runIt p (k . fmap (>>= go))
 
-runIt :: (a -> m s) -> (a -> (Input r -> m (It m r a)) -> m s) -> (It m r a -> m s)
+runIt :: (a -> m s) -> ((Input r -> m (It m r a)) -> m s) -> (It m r a -> m s)
 runIt p k = \case
-  Done a   -> p a
-  Roll a r -> k a r
+  Done a -> p a
+  Roll r -> k r
 
 
-evalIt :: Monad m => It m r a -> m a
+evalIt :: Monad m => It m r a -> m (Maybe a)
 evalIt = \case
-  Done a   -> pure a
-  Roll _ k -> k End >>= \case
-    Done a   -> pure a
-    Roll a _ -> pure a
-
-
-indexIt :: Applicative m => It m r a -> Input r -> m a
-indexIt = \case
-  Done a   -> const (pure a)
-  Roll _ k -> fmap extract . k
+  Done a -> pure (Just a)
+  Roll k -> k End >>= \case
+    Done a -> pure (Just a)
+    Roll _ -> pure Nothing
 
 
 -- Computation
 
 simplifyIt :: Applicative m => It m r a -> Input r -> m (It m r a)
 simplifyIt i r = case i of
-  Done{}   -> pure i
-  Roll _ k -> k r
+  Done{} -> pure i
+  Roll k -> k r
 
 
 -- Parsing
@@ -195,7 +147,7 @@ simplifyIt i r = case i of
 getLineIt :: Applicative m => It m Char String
 getLineIt = loop id
   where
-  loop = Roll "" . fmap pure . \ acc -> \case
+  loop = Roll . fmap pure . \ acc -> \case
     Input c | c /= '\n' -> loop (acc . (c:))
     _                   -> Done (acc [])
 
@@ -215,7 +167,7 @@ enumerateList :: Monad m => [r] -> Enumerator r m a
 enumerateList = go
   where
   go []     = pure
-  go (c:cs) = \ i -> runIt (const (pure i)) (\ _ k -> go cs =<< k (Input c)) i
+  go (c:cs) = \ i -> runIt (const (pure i)) (\ k -> go cs =<< k (Input c)) i
 
 enumerateFile :: FilePath -> Enumerator Char IO a
 enumerateFile path = withFile path ReadMode . flip enumerateHandle
@@ -223,7 +175,7 @@ enumerateFile path = withFile path ReadMode . flip enumerateHandle
 enumerateHandle :: Handle -> Enumerator Char IO a
 enumerateHandle handle = \case
   i@Done{} -> pure i
-  Roll _ k -> allocaBytes size (loop k)
+  Roll k   -> allocaBytes size (loop k)
   where
   size = 4096
   loop k p = do
@@ -244,4 +196,4 @@ take = go
     | n <= 0    = pure
     | otherwise = \case
       i@Done{} -> pure i
-      Roll a r -> Roll (pure a) (fmap (go (n - 1)) . r)
+      Roll r   -> Roll (fmap (go (n - 1)) . r)
