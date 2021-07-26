152181278884a104f784804064cd61ca80d4f4fa
Rob Rix <rob.rix@me.com>
2021-07-26 11:18:36 -0400

    Define an IsList instance for List.

--- src/Sequoia/List/Diff.hs
+++ src/Sequoia/List/Diff.hs
@@ -1,16 +1,25 @@
+{-# LANGUAGE TypeFamilies #-}
 module Sequoia.List.Diff
 ( -- * Difference lists
   List(..)
+  -- * Construction
+, fromList
   -- * Elimination
 , runList
 ) where
 
 import Data.Monoid (Endo(..))
+import GHC.Exts (IsList(..))
 
 -- Difference lists
 
 newtype List a = List (Endo [a])
   deriving (Monoid, Semigroup)
+
+instance IsList (List a) where
+  type Item (List a) = a
+  fromList = List . Endo . (++)
+  toList = runList
 
 
 -- Elimination
