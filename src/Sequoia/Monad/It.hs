d97815988270bdaaf45ae602e7504643282b7800
Rob Rix <rob.rix@me.com>
2021-07-26 21:17:02 -0400

    Simplify munfoldIt.

--- src/Sequoia/Monad/It.hs
+++ src/Sequoia/Monad/It.hs
@@ -85,7 +85,7 @@ unfoldIt :: (s -> Either a (Maybe r -> s)) -> (s -> It r a)
 unfoldIt coalg = go where go = munfoldIt ((. coalg) . fmap . (.))
 
 munfoldIt :: (forall x . (s -> x) -> (s -> Either a (Maybe r -> x))) -> (s -> It r a)
-munfoldIt coalg = go where go z = It (\ a k -> either a k (coalg go z))
+munfoldIt coalg = go where go = either doneIt rollIt . coalg go
 
 
 needIt :: (r -> Maybe a) -> It r a
