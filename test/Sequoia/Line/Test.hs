8bb8de578f86323de2a7fa96031763bb973c968f
Rob Rix <rob.rix@me.com>
2021-07-27 07:00:19 -0400

    Test that the LineEnding Semigroup instance is associative.
    Spoiler: it isn’t.

--- test/Sequoia/Line/Test.hs
+++ test/Sequoia/Line/Test.hs
@@ -3,7 +3,18 @@ module Sequoia.Line.Test
 ( tests
 ) where
 
-import Hedgehog
+import           Hedgehog
+import qualified Hedgehog.Gen as Gen
+import           Sequoia.Line
 
 tests :: IO Bool
 tests = checkParallel $$(discover)
+
+
+prop_line_ending_semigroup_associativity = property $ do
+  (a, b, c) <- forAll ((,,) <$> genLineEnding <*> genLineEnding <*> genLineEnding)
+  ((a <> b) <> c) === (a <> (b <> c))
+
+
+genLineEnding :: Gen LineEnding
+genLineEnding = Gen.enumBounded
