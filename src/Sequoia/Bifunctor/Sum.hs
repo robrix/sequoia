d6932460b37424a16a182ff7de4e4c456c54d0b5
Rob Rix <rob.rix@me.com>
2021-07-27 21:12:50 -0400

    Define a lambda-encoded + type.

--- src/Sequoia/Bifunctor/Sum.hs
+++ src/Sequoia/Bifunctor/Sum.hs
@@ -1,2 +1,11 @@
 module Sequoia.Bifunctor.Sum
-() where
+( -- * Sum type
+  type (+)(..)
+) where
+
+-- Sum type
+
+newtype a + b = S { getS :: forall t . (a -> t) -> (b -> t) -> t }
+  deriving (Functor)
+
+infixr 6 +
