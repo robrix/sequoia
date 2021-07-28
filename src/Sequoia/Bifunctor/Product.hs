25e151379e8aac1758d35f175db709fbf27965c4
Rob Rix <rob.rix@me.com>
2021-07-27 22:13:37 -0400

    Define a lambda-encoded product type.

--- src/Sequoia/Bifunctor/Product.hs
+++ src/Sequoia/Bifunctor/Product.hs
@@ -1,2 +1,11 @@
 module Sequoia.Bifunctor.Product
-() where
+( -- * Product type
+  type (×)(..)
+) where
+
+-- Product type
+
+newtype a × b = P { getP :: forall t . (a -> b -> t) -> t }
+  deriving (Functor)
+
+infixr 7 ×
