5ba6335ffb735f9bd509cf2e7fa78644f4edd98f
Rob Rix <rob.rix@me.com>
2021-07-27 22:09:08 -0400

    Define an eliminator for +.

--- src/Sequoia/Bifunctor/Sum.hs
+++ src/Sequoia/Bifunctor/Sum.hs
@@ -1,6 +1,8 @@
 module Sequoia.Bifunctor.Sum
 ( -- * Sum type
   type (+)(..)
+  -- * Elimination
+, runS
 ) where
 
 import Data.Bifunctor
@@ -14,3 +16,9 @@ infixr 6 +
 
 instance Bifunctor (+) where
   bimap f g (S s) = S (\ l r -> s (l . f) (r . g) )
+
+
+-- Elimination
+
+runS :: (a -> t) -> (b -> t) -> (a + b -> t)
+runS f g s = getS s f g
