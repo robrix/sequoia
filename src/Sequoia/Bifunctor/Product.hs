5b78fd3dafda870bb1251a142e589971597195b7
Rob Rix <rob.rix@me.com>
2021-07-27 22:14:08 -0400

    Define a Bifunctor instance for ×.

--- src/Sequoia/Bifunctor/Product.hs
+++ src/Sequoia/Bifunctor/Product.hs
@@ -3,9 +3,14 @@ module Sequoia.Bifunctor.Product
   type (×)(..)
 ) where
 
+import Data.Bifunctor
+
 -- Product type
 
 newtype a × b = P { getP :: forall t . (a -> b -> t) -> t }
   deriving (Functor)
 
 infixr 7 ×
+
+instance Bifunctor (×) where
+  bimap f g (P p) = P (\ lr -> p (\ l r -> lr (f l) (g r)))
