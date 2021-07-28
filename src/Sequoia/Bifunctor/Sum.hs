4c17f5c51b9b38e19a5aa147200003b4b213542d
Rob Rix <rob.rix@me.com>
2021-07-27 22:08:02 -0400

    Define a Bifunctor instance for +.

--- src/Sequoia/Bifunctor/Sum.hs
+++ src/Sequoia/Bifunctor/Sum.hs
@@ -3,9 +3,14 @@ module Sequoia.Bifunctor.Sum
   type (+)(..)
 ) where
 
+import Data.Bifunctor
+
 -- Sum type
 
 newtype a + b = S { getS :: forall t . (a -> t) -> (b -> t) -> t }
   deriving (Functor)
 
 infixr 6 +
+
+instance Bifunctor (+) where
+  bimap f g (S s) = S (\ l r -> s (l . f) (r . g) )
