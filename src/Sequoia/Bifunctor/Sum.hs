d5abdb3acaf10b53e85516afb94343f3e632102e
Rob Rix <rob.rix@me.com>
2021-07-27 22:10:49 -0400

    Define a Disj instance for +.

--- src/Sequoia/Bifunctor/Sum.hs
+++ src/Sequoia/Bifunctor/Sum.hs
@@ -6,6 +6,7 @@ module Sequoia.Bifunctor.Sum
 ) where
 
 import Data.Bifunctor
+import Sequoia.Disjunction
 
 -- Sum type
 
@@ -16,6 +17,11 @@ infixr 6 +
 
 instance Bifunctor (+) where
   bimap f g (S s) = S (\ l r -> s (l . f) (r . g) )
+
+instance Disj (+) where
+  inl a = S (\ f _ -> f a)
+  inr b = S (\ _ g -> g b)
+  (<-->) = runS
 
 
 -- Elimination
