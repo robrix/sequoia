f114d14b8cca74934288dcdc192381bd992953c4
Rob Rix <rob.rix@me.com>
2021-07-29 15:17:57 -0400

    Define Bottom as an identity functor.

--- src/Sequoia/Connective/Bottom.hs
+++ src/Sequoia/Connective/Bottom.hs
@@ -1,16 +1,12 @@
 module Sequoia.Connective.Bottom
 ( -- * Negative falsity
-  Bottom
-, absurdN
+  Bottom(..)
 ) where
 
 import Sequoia.Polarity
 
 -- Negative falsity
 
-data Bottom r
+newtype Bottom r = Bottom { absurdN :: r }
 
 instance Polarized N (Bottom r) where
-
-absurdN :: Bottom r -> a
-absurdN = \case
