737702bb58483c43e7533ed0a0c2f09f68108fb1
Rob Rix <rob.rix@me.com>
2021-07-29 15:38:34 -0400

    :fire: the Representable & Adjunction instances for Negate & Not.

--- src/Sequoia/Connective/Negation.hs
+++ src/Sequoia/Connective/Negation.hs
@@ -19,8 +19,6 @@ module Sequoia.Connective.Negation
 ) where
 
 import Data.Functor.Contravariant
-import Data.Functor.Contravariant.Adjunction
-import Data.Functor.Contravariant.Rep
 import Data.Profunctor
 import Sequoia.Polarity
 import Sequoia.Profunctor.Continuation
@@ -32,18 +30,7 @@ newtype Not r a = Not { getNot :: a • r }
 instance Contravariant (Not r) where
   contramap f = Not . lmap f . getNot
 
-instance Representable (Not r) where
-  type Rep (Not r) = r
-  tabulate = Not . K
-  index = (•¬)
-
 instance Pos a => Polarized N (Not r a) where
-
-instance Adjunction (Negate r) (Not r) where
-  unit   a = Not    (K (•- a))
-  counit a = Negate (K (•¬ a))
-  leftAdjunct  f a = Not    (K ((•- a) . f))
-  rightAdjunct f a = Negate (K ((•¬ a) . f))
 
 
 type (¬) = Not
@@ -64,18 +51,7 @@ newtype Negate r a = Negate { getNegate :: a • r }
 instance Contravariant (Negate r) where
   contramap f = Negate . lmap f . getNegate
 
-instance Representable (Negate r) where
-  type Rep (Negate r) = r
-  tabulate = Negate . K
-  index = (•-)
-
 instance Neg a => Polarized P (Negate r a) where
-
-instance Adjunction (Not r) (Negate r) where
-  unit   a = Negate (K (•¬ a))
-  counit a = Not    (K (•- a))
-  leftAdjunct  f a = Negate (K ((•¬ a) . f))
-  rightAdjunct f a = Not    (K ((•- a) . f))
 
 
 type (-) = Negate
