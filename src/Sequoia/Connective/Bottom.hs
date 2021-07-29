0037bd64414ba4704f8f26b394de9f7a64333f0b
Rob Rix <rob.rix@me.com>
2021-07-29 15:18:48 -0400

    Derive quite a few instances for Bottom.

--- src/Sequoia/Connective/Bottom.hs
+++ src/Sequoia/Connective/Bottom.hs
@@ -3,10 +3,13 @@ module Sequoia.Connective.Bottom
   Bottom(..)
 ) where
 
+import Data.Functor.Identity
 import Sequoia.Polarity
 
 -- Negative falsity
 
 newtype Bottom r = Bottom { absurdN :: r }
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+  deriving (Applicative, Monad) via Identity
 
 instance Polarized N (Bottom r) where
