f724538800a5b09b86e903a4ae844f1da634b819
Rob Rix <rob.rix@me.com>
2021-08-02 19:43:42 -0400

    Give a default definition of fmap in terms of Conj.

--- src/Sequoia/Conjunction.hs
+++ src/Sequoia/Conjunction.hs
@@ -28,6 +28,7 @@ module Sequoia.Conjunction
 , lensConj
   -- * Defaults
 , foldMapConj
+, fmapConj
 , traverseConj
 , bifoldMapConj
 , bimapConj
@@ -174,6 +175,9 @@ lensConj get = lens get . curryConj
 
 foldMapConj :: Conj p => (b -> m) -> (a `p` b -> m)
 foldMapConj f = f . exr
+
+fmapConj :: Conj p => (b -> b') -> (a `p` b) -> (a `p` b')
+fmapConj = exlrC inlr id
 
 traverseConj :: (Conj p, Applicative m) => (b -> m b') -> (a `p` b) -> m (a `p` b')
 traverseConj = exlrC (liftA2 inlr) pure
