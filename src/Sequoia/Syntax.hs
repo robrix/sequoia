d47e79dc8f9d54084ff82e7a661172ac25f1358e
Rob Rix <rob.rix@me.com>
2021-08-01 21:19:57 -0400

    Add a bunch of eliminations.

--- src/Sequoia/Syntax.hs
+++ src/Sequoia/Syntax.hs
@@ -5,13 +5,18 @@ module Sequoia.Syntax
 import Sequoia.Polarity
 
 class Expr r where
-  top :: r N
-  with :: r N -> r N -> r N
-  par :: r N -> r N -> r N
-  not :: r P -> r N
+  intop :: r N
+  inwith :: r N -> r N -> r N
+  exwithL :: r N -> r N
+  exwithR :: r N -> r N
+  inpar :: r N -> r N -> r N
+  expar :: (r N -> r o) -> (r N -> r o) -> (r N -> r o)
+  innot :: r P -> r N
 
-  one :: r P
-  sumL :: r P -> r P
-  sumR :: r P -> r P
-  tensor :: r P -> r P -> r P
-  negate :: r N -> r P
+  inone :: r P
+  insumL :: r P -> r P
+  insumR :: r P -> r P
+  exsum :: (r P -> r o) -> (r P -> r o) -> (r P -> r o)
+  intensor :: r P -> r P -> r P
+  extensor :: (r P -> r P -> r o) -> (r P -> r o)
+  innegate :: r N -> r P
