bf2bcdf1ffe606a16731985422b220b11ee6d3ae
Rob Rix <rob.rix@me.com>
2021-08-01 21:15:54 -0400

    Define a class representing some expressions.

--- src/Sequoia/Syntax.hs
+++ src/Sequoia/Syntax.hs
@@ -1,2 +1,17 @@
 module Sequoia.Syntax
-() where
+( Expr(..)
+) where
+
+import Sequoia.Polarity
+
+class Expr r where
+  top :: r N
+  with :: r N -> r N -> r N
+  par :: r N -> r N -> r N
+  not :: r P -> r N
+
+  one :: r P
+  sumL :: r P -> r P
+  sumR :: r P -> r P
+  tensor :: r P -> r P -> r P
+  negate :: r N -> r P
