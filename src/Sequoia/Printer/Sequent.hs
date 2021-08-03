5678aa4658fe726e5e412ca6ec614b78b8637880
Rob Rix <rob.rix@me.com>
2021-08-03 14:03:02 -0400

    Define a printable sequent type.

--- src/Sequoia/Printer/Sequent.hs
+++ src/Sequoia/Printer/Sequent.hs
@@ -1,2 +1,10 @@
 module Sequoia.Printer.Sequent
-() where
+( -- * Printable sequents
+  Seq(..)
+) where
+
+import Sequoia.Printer
+
+-- Printable sequents
+
+newtype Seq e r _Γ _Δ = Seq { runSeq :: Printer _Δ -> Printer _Γ }
