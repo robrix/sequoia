929c5fe25b8191aae0399a16d9b980146a3bbd74
Rob Rix <rob.rix@me.com>
2021-08-03 14:04:46 -0400

    Define an eliminator for Seq.

--- src/Sequoia/Printer/Sequent.hs
+++ src/Sequoia/Printer/Sequent.hs
@@ -1,10 +1,13 @@
 module Sequoia.Printer.Sequent
 ( -- * Printable sequents
   Seq(..)
+  -- * Elimination
+, appSeq
 ) where
 
 import Data.Functor.Contravariant
 import Data.Profunctor
+import Prelude hiding (print)
 import Sequoia.Printer
 
 -- Printable sequents
@@ -16,3 +19,9 @@ instance Profunctor (Seq e r) where
 
 instance Functor (Seq e r _Γ) where
   fmap = rmap
+
+
+-- Elimination
+
+appSeq :: Seq e r _Γ _Δ -> _Γ -> Printer _Δ -> Doc
+appSeq s _Γ _Δ = print (runSeq s _Δ) _Γ
