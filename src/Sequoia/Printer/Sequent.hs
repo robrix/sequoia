261bb870e86eca1d42784b865e5b0692ec68da71
Rob Rix <rob.rix@me.com>
2021-08-03 14:03:25 -0400

    Seq is a Profunctor.

--- src/Sequoia/Printer/Sequent.hs
+++ src/Sequoia/Printer/Sequent.hs
@@ -3,8 +3,13 @@ module Sequoia.Printer.Sequent
   Seq(..)
 ) where
 
+import Data.Functor.Contravariant
+import Data.Profunctor
 import Sequoia.Printer
 
 -- Printable sequents
 
 newtype Seq e r _Γ _Δ = Seq { runSeq :: Printer _Δ -> Printer _Γ }
+
+instance Profunctor (Seq e r) where
+  dimap f g = Seq . dimap (contramap g) (contramap f) . runSeq
