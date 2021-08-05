04776bb5be98fa2980b6f5ba9df5b23aa09bb2b2
Rob Rix <rob.rix@me.com>
2021-08-05 19:02:43 -0400

    Use the composition operator for runExists.

--- src/Sequoia/Connective/Exists.hs
+++ src/Sequoia/Connective/Exists.hs
@@ -5,8 +5,8 @@ module Sequoia.Connective.Exists
 , runExists
 ) where
 
-import Data.Profunctor
 import Sequoia.Polarity
+import Sequoia.Profunctor
 import Sequoia.Profunctor.Continuation
 
 -- Universal quantification
@@ -16,4 +16,4 @@ data Exists r p f = forall x . Polarized p x => Exists (f x •• r)
 instance Polarized P (Exists r p f)
 
 runExists :: (forall x . Polarized p x => f x -> a) -> Exists r p f -> a •• r
-runExists f (Exists r) = lmap (lmap f) r
+runExists f (Exists r) = r <<^ (<<^ f)
