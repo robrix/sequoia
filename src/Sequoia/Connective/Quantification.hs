283c0fcb6259d7119fd2d152903f3f026a585239
Rob Rix <rob.rix@me.com>
2021-08-05 19:34:45 -0400

    Define a right adjunct for the quantifiers.

--- src/Sequoia/Connective/Quantification.hs
+++ src/Sequoia/Connective/Quantification.hs
@@ -4,6 +4,7 @@
 module Sequoia.Connective.Quantification
 ( -- * Adjunction
   leftAdjunct
+, rightAdjunct
   -- * Quantified constraints
 , type (==>)
   -- * Connectives
@@ -15,11 +16,15 @@ import Data.Kind (Constraint)
 import Sequoia.Connective.Exists
 import Sequoia.Connective.ForAll
 import Sequoia.Polarity
+import Sequoia.Profunctor.Continuation
 
 -- Adjunction
 
 leftAdjunct :: (forall x . Exists r p a -> b x) -> (forall x . Polarized p x => a x -> ForAll r p b)
 leftAdjunct f a = forAll (f (exists a))
+
+rightAdjunct :: (forall x . a x -> ForAll r p b) -> (forall x . Polarized p x => Exists r p a -> b x •• r)
+rightAdjunct f = runExistsM (runForAll . f)
 
 
 -- Quantified constraints
