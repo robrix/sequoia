cba03b11a30afe3535d1d09f74f727f1adba05c9
Rob Rix <rob.rix@me.com>
2021-08-05 18:50:27 -0400

    Define ^>> as a synonym of lmap.

--- src/Sequoia/Profunctor.hs
+++ src/Sequoia/Profunctor.hs
@@ -1,6 +1,13 @@
 module Sequoia.Profunctor
 ( -- * Profunctors
   module Data.Profunctor
+  -- * Composition
+, (^>>)
 ) where
 
 import Data.Profunctor
+
+(^>>) :: Profunctor p => (a' -> a) -> (a `p` b) -> (a' `p` b)
+(^>>) = lmap
+
+infixr 1 ^>>
