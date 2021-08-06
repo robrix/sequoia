bd4f58043ab8fdea3773d5728946a1884992ade1
Rob Rix <rob.rix@me.com>
2021-08-06 13:06:49 -0400

    Define a simple constructor for Exponentials.

--- src/Sequoia/Profunctor/Exp/Class.hs
+++ src/Sequoia/Profunctor/Exp/Class.hs
@@ -1,11 +1,16 @@
 module Sequoia.Profunctor.Exp.Class
 ( -- Exponentials
   Exponential(..)
+, exp'
 ) where
 
 import Data.Profunctor
+import Prelude hiding (exp)
 
 -- Exponentials
 
 class Profunctor ex => Exponential ex where
   exp :: ((b -> r) -> (a -> r)) -> ex a b
+
+exp' :: Exponential ex => (a -> b) -> ex a b
+exp' f = exp (. f)
