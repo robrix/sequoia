57c0d7f74adf9de06a02e2ec38ea3cd8c74e4c83
Rob Rix <rob.rix@me.com>
2021-08-06 11:32:38 -0400

    Define a Profunctor instance for -->.

--- src/Sequoia/Profunctor/Exp/Quantified.hs
+++ src/Sequoia/Profunctor/Exp/Quantified.hs
@@ -3,5 +3,10 @@ module Sequoia.Profunctor.Exp.Quantified
   type (-->)(..)
 ) where
 
+import Data.Profunctor
+
 newtype a --> b = Exp { getExp :: forall r . (b -> r) -> (a -> r) }
   deriving (Functor)
+
+instance Profunctor (-->) where
+  dimap f g (Exp r) = Exp (\ k -> r (k . g) . f)
