8b3529e8e95dc73afe23a84cdb8592a57f096b9d
Rob Rix <rob.rix@me.com>
2021-08-06 19:23:46 -0400

    bicollect only needs one function.

--- src/Sequoia/Bidistributive.hs
+++ src/Sequoia/Bidistributive.hs
@@ -7,6 +7,6 @@ import Data.Bifunctor
 
 class Bifunctor p => Bidistributive p where
   bidistribute :: Functor f => f (p b c) -> p (f b) (f c)
-  bidistribute = bicollect id id
+  bidistribute = bicollect id
 
-  bicollect :: Functor f => (a -> p b c) -> (a -> p b c) -> f a -> p (f b) (f c)
+  bicollect :: Functor f => (a -> p b c) -> f a -> p (f b) (f c)
