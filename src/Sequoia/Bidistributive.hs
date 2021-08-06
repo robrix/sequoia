ea77d5eb483fe11ef11a2bbac83a84a54dceb55c
Rob Rix <rob.rix@me.com>
2021-08-06 19:21:12 -0400

    Define a Bidistributive class.

--- src/Sequoia/Bidistributive.hs
+++ src/Sequoia/Bidistributive.hs
@@ -1,2 +1,9 @@
 module Sequoia.Bidistributive
-() where
+( -- * Bidistributive
+  Bidistributive(..)
+) where
+
+import Data.Bifunctor
+
+class Bifunctor p => Bidistributive p where
+  bicollect :: Functor f => (a -> p b c) -> (a -> p b c) -> f a -> p (f b) (f c)
