72c4002d4cec93c813837f02987d5d10499d7ae2
Rob Rix <rob.rix@me.com>
2021-08-06 18:50:40 -0400

    Define a Contravariant instance for Join.

--- src/Sequoia/Bifunctor/Join.hs
+++ src/Sequoia/Bifunctor/Join.hs
@@ -3,8 +3,13 @@ module Sequoia.Bifunctor.Join
 ) where
 
 import Data.Bifunctor
+import Data.Functor.Contravariant
+import Sequoia.Bicontravariant
 
 newtype Join p a = Join { runJoin :: p a a }
 
 instance Bifunctor p => Functor (Join p) where
   fmap f = Join . bimap f f . runJoin
+
+instance Bicontravariant p => Contravariant (Join p) where
+  contramap f = Join . contrabimap f f . runJoin
