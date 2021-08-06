01c572182b3929a32728462c8b3363ed2d57971c
Rob Rix <rob.rix@me.com>
2021-08-06 18:53:46 -0400

    Move tensor into the multiplicative module.

--- src/Sequoia/Connective/Multiplicative.hs
+++ src/Sequoia/Connective/Multiplicative.hs
@@ -4,11 +4,12 @@ module Sequoia.Connective.Multiplicative
   -- * Adjunction
 , leftAdjunct
 , rightAdjunct
+  -- * Positive conjunction
+, type (⊗)(..)
   -- * Connectives
 , module Sequoia.Connective.Bottom
 , module Sequoia.Connective.One
 , module Sequoia.Connective.Par
-, module Sequoia.Connective.Tensor
 ) where
 
 import Sequoia.Conjunction
@@ -16,8 +17,8 @@ import Sequoia.Connective.Bottom
 import Sequoia.Connective.Negation
 import Sequoia.Connective.One
 import Sequoia.Connective.Par
-import Sequoia.Connective.Tensor
 import Sequoia.Disjunction
+import Sequoia.Polarity
 
 elimPar :: a ⅋ b -> Negate e a r ⊗ Negate e b r -> r
 elimPar = (. exl) . flip (•-) <--> (. exr) . flip (•-)
@@ -33,3 +34,18 @@ leftAdjunct f = f . inl >---< f . inr
 
 rightAdjunct :: (a -> b ⊗ b) -> (a ⅋ a -> b)
 rightAdjunct f = exl . f <--> exr . f
+
+
+-- Positive conjunction
+
+data a ⊗ b = !a :⊗ !b
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+infixr 7 ⊗, :⊗
+
+instance (Pos a, Pos b) => Polarized P (a ⊗ b) where
+
+instance Conj (⊗) where
+  (>--<) = (:⊗)
+  exl (l :⊗ _) = l
+  exr (_ :⊗ r) = r
--- src/Sequoia/Connective/Tensor.hs
+++ src/Sequoia/Connective/Tensor.hs
@@ -3,19 +3,4 @@ module Sequoia.Connective.Tensor
   type (⊗)(..)
 ) where
 
-import Sequoia.Conjunction
-import Sequoia.Polarity
-
--- Positive conjunction
-
-data a ⊗ b = !a :⊗ !b
-  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
-
-infixr 7 ⊗, :⊗
-
-instance (Pos a, Pos b) => Polarized P (a ⊗ b) where
-
-instance Conj (⊗) where
-  (>--<) = (:⊗)
-  exl (l :⊗ _) = l
-  exr (_ :⊗ r) = r
+import Sequoia.Connective.Multiplicative
