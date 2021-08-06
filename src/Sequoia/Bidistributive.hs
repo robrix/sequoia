9fe84efd16df0c34572f4680197e7b9e4c3db7ae
Rob Rix <rob.rix@me.com>
2021-08-06 19:22:07 -0400

    Give a default definition of bidistribute.

--- src/Sequoia/Bidistributive.hs
+++ src/Sequoia/Bidistributive.hs
@@ -7,4 +7,6 @@ import Data.Bifunctor
 
 class Bifunctor p => Bidistributive p where
   bidistribute :: Functor f => f (p b c) -> p (f b) (f c)
+  bidistribute = bicollect id id
+
   bicollect :: Functor f => (a -> p b c) -> (a -> p b c) -> f a -> p (f b) (f c)
