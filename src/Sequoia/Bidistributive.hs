5ede130584a227671cefe5d299ffed8916955f81
Rob Rix <rob.rix@me.com>
2021-08-06 19:21:50 -0400

    Define a simple unparameterized distributive method.

--- src/Sequoia/Bidistributive.hs
+++ src/Sequoia/Bidistributive.hs
@@ -6,4 +6,5 @@ module Sequoia.Bidistributive
 import Data.Bifunctor
 
 class Bifunctor p => Bidistributive p where
+  bidistribute :: Functor f => f (p b c) -> p (f b) (f c)
   bicollect :: Functor f => (a -> p b c) -> (a -> p b c) -> f a -> p (f b) (f c)
