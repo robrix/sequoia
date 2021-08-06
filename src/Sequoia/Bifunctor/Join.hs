0aa7d2092ebb7dd4ef75484ecd4948745f6b2b7f
Rob Rix <rob.rix@me.com>
2021-08-06 19:17:58 -0400

    Define a Foldable instance for Join.

--- src/Sequoia/Bifunctor/Join.hs
+++ src/Sequoia/Bifunctor/Join.hs
@@ -2,11 +2,15 @@ module Sequoia.Bifunctor.Join
 ( Join(..)
 ) where
 
+import Data.Bifoldable
 import Data.Bifunctor
 import Data.Functor.Contravariant
 import Sequoia.Bicontravariant
 
 newtype Join p a = Join { runJoin :: p a a }
+
+instance Bifoldable p => Foldable (Join p) where
+  foldMap f = bifoldMap f f . runJoin
 
 instance Bifunctor p => Functor (Join p) where
   fmap f = Join . bimap f f . runJoin
