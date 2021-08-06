9ee494ed7310c10c6652d1651a8839ebcf56a765
Rob Rix <rob.rix@me.com>
2021-08-06 18:49:57 -0400

    Define a Functor instance for Join.

--- src/Sequoia/Bifunctor/Join.hs
+++ src/Sequoia/Bifunctor/Join.hs
@@ -2,4 +2,9 @@ module Sequoia.Bifunctor.Join
 ( Join(..)
 ) where
 
+import Data.Bifunctor
+
 newtype Join p a = Join { runJoin :: p a a }
+
+instance Bifunctor p => Functor (Join p) where
+  fmap f = Join . bimap f f . runJoin
