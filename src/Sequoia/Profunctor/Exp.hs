eafdaf65ffe93c5b272179a6b33dae74dd807b70
Rob Rix <rob.rix@me.com>
2021-08-05 23:08:20 -0400

    Define a Functor instance.

--- src/Sequoia/Profunctor/Exp.hs
+++ src/Sequoia/Profunctor/Exp.hs
@@ -11,3 +11,6 @@ newtype Exp r a b = Exp { runExp :: (b -> r) -> (a -> r) }
 
 instance Profunctor (Exp r) where
   dimap f g = Exp . dimap (<<^ g) (<<^ f) . runExp
+
+instance Functor (Exp r a) where
+  fmap = rmap
