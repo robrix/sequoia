b94b678506528a91ff717f4808a9985faee3e522
Rob Rix <rob.rix@me.com>
2021-08-06 13:02:13 -0400

    Define an exponential class.

--- src/Sequoia/Profunctor/Exp/Class.hs
+++ src/Sequoia/Profunctor/Exp/Class.hs
@@ -1,2 +1,11 @@
 module Sequoia.Profunctor.Exp.Class
-() where
+( -- Exponentials
+  Exponential(..)
+) where
+
+import Data.Profunctor
+
+-- Exponentials
+
+class Profunctor ex => Exponential ex where
+  exp :: (a -> b) -> ex a b
