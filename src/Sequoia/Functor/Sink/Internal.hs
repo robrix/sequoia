69806d2863fe10f8502422075e5eec9a9d111650
Rob Rix <rob.rix@me.com>
2021-08-06 10:51:43 -0400

    Snk uses the env-respecting Coexp.

--- src/Sequoia/Functor/Sink/Internal.hs
+++ src/Sequoia/Functor/Sink/Internal.hs
@@ -4,12 +4,14 @@ module Sequoia.Functor.Sink.Internal
 
 import Data.Functor.Contravariant
 import Sequoia.Functor.Applicative
-import Sequoia.Profunctor.Exp
+import Sequoia.Profunctor.Coexponential
+import Sequoia.Profunctor.Continuation
+import Sequoia.Profunctor.Value
 
 newtype Snk e r a = Snk { runSnkFn :: (e -> a) -> (e -> r) }
 
 instance Contravariant (Snk e r) where
   contramap f = Snk . (. fmap f) . runSnkFn
 
-instance Contrapply (Coexp r) (Snk e r) where
-  coliftC2 f a b = Snk (\ v e -> runSnkFn a (\ e -> f (flip (runSnkFn b) e . const :>- v e)) e)
+instance Contrapply (Coexp e r) (Snk e r) where
+  coliftC2 f a b = Snk (\ v e -> runSnkFn a (\ e -> f (V v -< K (flip (runSnkFn b) e . const))) e)
