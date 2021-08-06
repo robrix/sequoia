c3a5390d6668fa7cb07b8f348a041e1ccdc906c8
Rob Rix <rob.rix@me.com>
2021-08-06 19:42:26 -0400

    Define a Birepresentable instance for (,).

--- src/Sequoia/Birepresentable.hs
+++ src/Sequoia/Birepresentable.hs
@@ -10,3 +10,10 @@ class Bidistributive p => Birepresentable p where
   type Birep p
   bitabulate :: (Birep p -> a) -> (Birep p -> b) -> p a b
   biindex :: p a b -> (Birep p -> Either a b)
+
+instance Birepresentable (,) where
+  type Birep (,) = Bool
+  bitabulate f g = (f False, g True)
+  biindex p = \case
+    False -> Left  (fst p)
+    True  -> Right (snd p)
