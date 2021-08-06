4a5c15f97b6ac59aa942adb994cd82ad64c89109
Rob Rix <rob.rix@me.com>
2021-08-06 11:34:09 -0400

    Define a Category instance for -->.

--- src/Sequoia/Profunctor/Exp/Quantified.hs
+++ src/Sequoia/Profunctor/Exp/Quantified.hs
@@ -3,12 +3,17 @@ module Sequoia.Profunctor.Exp.Quantified
   type (-->)(..)
 ) where
 
-import Data.Profunctor
+import qualified Control.Category as Cat
+import           Data.Profunctor
 
 -- Exponentials
 
 newtype a --> b = Exp { getExp :: forall r . (b -> r) -> (a -> r) }
   deriving (Functor)
+
+instance Cat.Category (-->) where
+  id = Exp id
+  f . g = Exp (getExp g . getExp f)
 
 instance Profunctor (-->) where
   dimap f g (Exp r) = Exp (\ k -> r (k . g) . f)
