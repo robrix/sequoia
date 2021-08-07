a1db1b1cf283af60da3b8fee02df61108fe2ca29
Rob Rix <rob.rix@me.com>
2021-08-07 08:12:14 -0400

    Define an adjunction between True and Not.

--- src/Sequoia/Connective/Final.hs
+++ src/Sequoia/Connective/Final.hs
@@ -1,8 +1,21 @@
 module Sequoia.Connective.Final
-( -- * Connectives
-  module Sequoia.Connective.Not
+( -- * Assertion
+  leftAdjunct
+, rightAdjunct
+  -- * Connectives
+, module Sequoia.Connective.Not
 , module Sequoia.Connective.True
 ) where
 
+import Control.Category ((<<<))
 import Sequoia.Connective.Not
 import Sequoia.Connective.True
+import Sequoia.Profunctor.Continuation
+
+-- Adjunction
+
+leftAdjunct :: (True r a -> b • r) -> (a -> Not b r)
+leftAdjunct f a = Not (f (true a))
+
+rightAdjunct :: (a -> Not b r) -> (True r a -> b • r)
+rightAdjunct f a = trueK a <<< getNot (f (trueA a))
