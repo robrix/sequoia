7d6afee0755f8fa7ae2944d5b720fe8bf9b45ce3
Rob Rix <rob.rix@me.com>
2021-08-07 13:23:17 -0400

    Define an exponential type around par..

--- src/Sequoia/Profunctor/Exp/Par.hs
+++ src/Sequoia/Profunctor/Exp/Par.hs
@@ -1,2 +1,9 @@
 module Sequoia.Profunctor.Exp.Par
-() where
+( Exp(..)
+) where
+
+import Sequoia.Calculus.Not
+import Sequoia.Calculus.NotUntrue
+import Sequoia.Calculus.Par
+
+newtype Exp e r a b = Exp { getExp :: a ¬ r ⅋ e ≁ b }
