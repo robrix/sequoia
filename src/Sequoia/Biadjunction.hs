099df1555e8055f9a20a71c0b5f2e59086e986e7
Rob Rix <rob.rix@me.com>
2021-08-06 21:26:44 -0400

    Define a Biadjunction class.

--- src/Sequoia/Biadjunction.hs
+++ src/Sequoia/Biadjunction.hs
@@ -1,2 +1,14 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module Sequoia.Biadjunction
-() where
+( -- * Biadjunctions
+  Biadjunction(..)
+) where
+
+import Data.Bifunctor
+import Sequoia.Birepresentable
+
+-- Biadjunctions
+
+class (Bifunctor f, Birepresentable u) => Biadjunction f u | f -> u, u -> f where
+  bileftAdjunct :: (f a a -> b) -> (a -> u b b)
+  birightAdjunct :: (a -> u b b) -> (f a a -> b)
