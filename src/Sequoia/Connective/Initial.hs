45341356f993dce22413c2609a3b96581203b834
Rob Rix <rob.rix@me.com>
2021-08-07 08:08:58 -0400

    Define an adjunction between Negate and NotUntrue.

--- src/Sequoia/Connective/Initial.hs
+++ src/Sequoia/Connective/Initial.hs
@@ -1,8 +1,21 @@
 module Sequoia.Connective.Initial
-( -- * Connectives
-  module Sequoia.Connective.NotUntrue
+( -- * Adjunction
+  leftAdjunct
+, rightAdjunct
+  -- * Connectives
+, module Sequoia.Connective.NotUntrue
 , module Sequoia.Connective.Negate
 ) where
 
 import Sequoia.Connective.Negate
 import Sequoia.Connective.NotUntrue
+import Sequoia.Profunctor.Continuation
+import Sequoia.Profunctor.Value
+
+-- Adjunction
+
+leftAdjunct :: (Negate e a r -> b) -> (a • r -> NotUntrue e b)
+leftAdjunct f a = NotUntrue (V (\ e -> f (Negate e a)))
+
+rightAdjunct :: (a • r -> NotUntrue e b) -> (Negate e a r -> b)
+rightAdjunct f a = negateE a ∘ runNotUntrue (f (negateK a))
