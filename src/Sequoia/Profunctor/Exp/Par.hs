dc07fc8dc978b82147ff605abd68ffd7a77cccd7
Rob Rix <rob.rix@me.com>
2021-08-07 16:46:02 -0400

    Represent using Either under a double negation.

--- src/Sequoia/Profunctor/Exp/Par.hs
+++ src/Sequoia/Profunctor/Exp/Par.hs
@@ -9,31 +9,35 @@ module Sequoia.Profunctor.Exp.Par
 , runExp
 ) where
 
+import Data.Bifunctor
 import Prelude hiding (exp)
+import Sequoia.Disjunction
 import Sequoia.Profunctor
 import Sequoia.Profunctor.Context
+import Sequoia.Profunctor.Continuation
+import Sequoia.Profunctor.Value
 
 -- Exponentials
 
-newtype Exp env res a b = Exp { getExp :: a -> ((env -> b) -> res) -> res }
+newtype Exp env res a b = Exp { getExp :: ((a • res) `Either` (env ∘ b)) •• res }
 
 instance Functor (Exp env res a) where
   fmap = rmap
 
 instance Profunctor (Exp e r) where
-  dimap f g = Exp . dimap f (lmap (lmap (rmap g))) . getExp
+  dimap f g = Exp . lmap (lmap (bimap (lmap f) (rmap g))) . getExp
 
 
 -- Construction
 
-exp :: (a -> ((env -> b) -> res) -> res) -> Exp env res a b
-exp = Exp
+exp :: (a -> (env ∘ b) • res -> res) -> Exp env res a b
+exp f = Exp (K (\ k -> inlL k • K (\ a -> f a (inrL k))))
 
 exp' :: (a -> b) -> Exp env res a b
-exp' f = Exp (\ a kb -> kb (pure (f a)))
+exp' f = exp (\ a kb -> kb • pure (f a))
 
 
 -- Elimination
 
-runExp :: Exp env res a b -> (b -> res) -> a -> env ==> res
-runExp (Exp r) k a = C (\ env -> r a (\ b -> k (b env)))
+runExp :: Exp env res a b -> b • res -> a -> env ==> res
+runExp (Exp r) k a = C (\ env -> r • (dn a <••> K (\ b -> k • env ∘ b)))
