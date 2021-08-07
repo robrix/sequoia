8d690ddeeb33b871bf201dccd3a92322532fe917
Rob Rix <rob.rix@me.com>
2021-08-07 14:33:51 -0400

    Define a constructor from simple functions.

--- src/Sequoia/Profunctor/Exp/Par.hs
+++ src/Sequoia/Profunctor/Exp/Par.hs
@@ -1,18 +1,27 @@
 module Sequoia.Profunctor.Exp.Par
 ( -- * Exponentials
   Exp(..)
+  -- * Construction
+, exp'
 ) where
 
-import Data.Bifunctor (bimap)
-import Data.Profunctor
+import Data.Bifunctor
 import Sequoia.Calculus.Not
 import Sequoia.Calculus.NotUntrue
-import Sequoia.Calculus.Par
+import Sequoia.Connective.Par.Parameterized
+import Sequoia.Profunctor
+import Sequoia.Profunctor.Continuation
 
 -- Exponentials
 
-newtype Exp e r a b = Exp { getExp :: a ¬ r ⅋ e ≁ b }
+newtype Exp env res a b = Exp { getExp :: Par res (a ¬ res) (env ≁ b) }
   deriving (Functor)
 
 instance Profunctor (Exp e r) where
   dimap f g = Exp . bimap (lmap f) (rmap g) . getExp
+
+
+-- Construction
+
+exp' :: (a -> b) -> Exp env res a b
+exp' f = Exp (Par (K (\ (ka, kb) -> ka • Not (K (\ a -> kb • pure (f a))))))
