5799774164f61643705f048efe882bbff9f53d22
Rob Rix <rob.rix@me.com>
2021-08-07 13:58:33 -0400

    Polarize Par.

--- src/Sequoia/Connective/Par/Parameterized.hs
+++ src/Sequoia/Connective/Par/Parameterized.hs
@@ -3,8 +3,11 @@ module Sequoia.Connective.Par.Parameterized
   Par(..)
 ) where
 
+import Sequoia.Polarity
 import Sequoia.Profunctor.Continuation
 
 -- Par
 
 newtype Par r a b = Par ((a • r, b • r) • r)
+
+instance (Neg a, Neg b) => Polarized N (Par r a b) where
