f72f926a85bce1ab4f1d2ea93fd520da7c7b8639
Rob Rix <rob.rix@me.com>
2021-08-06 21:56:35 -0400

    Define a nullary adjunction class.

--- src/Sequoia/Nulladjunction.hs
+++ src/Sequoia/Nulladjunction.hs
@@ -1,2 +1,11 @@
+{-# LANGUAGE FunctionalDependencies #-}
 module Sequoia.Nulladjunction
-() where
+( -- * Nullary adjunctions
+  Nulladjunction(..)
+) where
+
+-- Nullary adjunctions
+
+class Nulladjunction f u | f -> u, u -> f where
+  nullleftAdjunct :: (f -> b) -> (a -> u)
+  nullrightAdjunct :: (a -> u) -> (f -> b)
