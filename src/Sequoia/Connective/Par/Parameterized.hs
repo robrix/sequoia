9aa14c3be1425f364e4955705363732460862371
Rob Rix <rob.rix@me.com>
2021-08-07 14:02:17 -0400

    Define an eliminator for Par.

--- src/Sequoia/Connective/Par/Parameterized.hs
+++ src/Sequoia/Connective/Par/Parameterized.hs
@@ -1,6 +1,8 @@
 module Sequoia.Connective.Par.Parameterized
 ( -- * Par
   Par(..)
+  -- * Elimination
+, runPar
 ) where
 
 import Sequoia.Polarity
@@ -11,3 +13,9 @@ import Sequoia.Profunctor.Continuation
 newtype Par r a b = Par ((a • r, b • r) • r)
 
 instance (Neg a, Neg b) => Polarized N (Par r a b) where
+
+
+-- Elimination
+
+runPar :: (a • r, b • r) -> Par r a b • r
+runPar e = K (\ (Par r) -> r • e)
