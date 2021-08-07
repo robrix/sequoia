93f0ef32802280d0c698b8c2ce8cdcd356aa9f3d
Rob Rix <rob.rix@me.com>
2021-08-07 16:29:46 -0400

    Curry Exp.

--- src/Sequoia/Profunctor/Exp/Par.hs
+++ src/Sequoia/Profunctor/Exp/Par.hs
@@ -8,7 +8,6 @@ module Sequoia.Profunctor.Exp.Par
 , runExp
 ) where
 
-import Data.Bifunctor
 import Prelude hiding (exp)
 import Sequoia.Calculus.NotUntrue
 import Sequoia.Profunctor
@@ -18,25 +17,25 @@ import Sequoia.Profunctor.Value
 
 -- Exponentials
 
-newtype Exp env res a b = Exp { getExp :: (a, (env ≁ b) • res) -> res }
+newtype Exp env res a b = Exp { getExp :: a -> (env ≁ b) • res -> res }
 
 instance Functor (Exp env res a) where
   fmap = rmap
 
 instance Profunctor (Exp e r) where
-  dimap f g = Exp . lmap (bimap f (lmap (rmap g))) . getExp
+  dimap f g = Exp . dimap f (lmap (lmap (rmap g))) . getExp
 
 
 -- Construction
 
 exp :: (a -> (env ≁ b) • res -> res) -> Exp env res a b
-exp = Exp . uncurry
+exp = Exp
 
 exp' :: (a -> b) -> Exp env res a b
-exp' f = Exp (\ (a, kb) -> kb • NotUntrue (pure (f a)))
+exp' f = Exp (\ a kb -> kb • NotUntrue (pure (f a)))
 
 
 -- Elimination
 
 runExp :: Exp env res a b -> b • res -> a -> env ==> res
-runExp (Exp r) k a = C (\ env -> r (a, K (\ b -> k • env ∘ runNotUntrue b)))
+runExp (Exp r) k a = C (\ env -> r a (K (\ b -> k • env ∘ runNotUntrue b)))
