3f475ad48c5d4449f051742f06dc72601ca46b38
Rob Rix <rob.rix@me.com>
2021-08-07 15:06:46 -0400

    Eliminate Exp.

--- src/Sequoia/Profunctor/Exp/Par.hs
+++ src/Sequoia/Profunctor/Exp/Par.hs
@@ -3,6 +3,8 @@ module Sequoia.Profunctor.Exp.Par
   Exp(..)
   -- * Construction
 , exp'
+  -- * Elimination
+, runExp
 ) where
 
 import Data.Bifunctor
@@ -10,7 +12,9 @@ import Sequoia.Calculus.Not
 import Sequoia.Calculus.NotUntrue
 import Sequoia.Connective.Par.Parameterized
 import Sequoia.Profunctor
+import Sequoia.Profunctor.Context
 import Sequoia.Profunctor.Continuation
+import Sequoia.Profunctor.Value
 
 -- Exponentials
 
@@ -25,3 +29,9 @@ instance Profunctor (Exp e r) where
 
 exp' :: (a -> b) -> Exp env res a b
 exp' f = Exp (Par (K (\ (ka, kb) -> ka • Not (K (\ a -> kb • pure (f a))))))
+
+
+-- Elimination
+
+runExp :: Exp env res a b -> b • res -> a -> env ==> res
+runExp (Exp (Par (K r))) k a = C (\ env -> r (dn a <<^ getNot, K (\ b -> k • env ∘ runNotUntrue b)))
