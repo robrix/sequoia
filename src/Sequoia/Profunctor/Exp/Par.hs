aa92ec253bb78a9ca5e7d7c4dfddc89c752a3cfc
Rob Rix <rob.rix@me.com>
2021-08-07 13:26:04 -0400

    Define a Profunctor instance for Exp.

--- src/Sequoia/Profunctor/Exp/Par.hs
+++ src/Sequoia/Profunctor/Exp/Par.hs
@@ -2,9 +2,14 @@ module Sequoia.Profunctor.Exp.Par
 ( Exp(..)
 ) where
 
+import Data.Bifunctor (bimap)
+import Data.Profunctor
 import Sequoia.Calculus.Not
 import Sequoia.Calculus.NotUntrue
 import Sequoia.Calculus.Par
 
 newtype Exp e r a b = Exp { getExp :: a ¬ r ⅋ e ≁ b }
   deriving (Functor)
+
+instance Profunctor (Exp e r) where
+  dimap f g = Exp . bimap (lmap f) (rmap g) . getExp
