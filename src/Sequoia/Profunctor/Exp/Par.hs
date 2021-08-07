f8ab026c285574184491d08f991df9c667b95d2b
Rob Rix <rob.rix@me.com>
2021-08-07 16:32:14 -0400

    Simplify down to just functions.

--- src/Sequoia/Profunctor/Exp/Par.hs
+++ src/Sequoia/Profunctor/Exp/Par.hs
@@ -9,15 +9,12 @@ module Sequoia.Profunctor.Exp.Par
 ) where
 
 import Prelude hiding (exp)
-import Sequoia.Calculus.NotUntrue
 import Sequoia.Profunctor
 import Sequoia.Profunctor.Context
-import Sequoia.Profunctor.Continuation
-import Sequoia.Profunctor.Value
 
 -- Exponentials
 
-newtype Exp env res a b = Exp { getExp :: a -> (env ≁ b) • res -> res }
+newtype Exp env res a b = Exp { getExp :: a -> ((env -> b) -> res) -> res }
 
 instance Functor (Exp env res a) where
   fmap = rmap
@@ -28,14 +25,14 @@ instance Profunctor (Exp e r) where
 
 -- Construction
 
-exp :: (a -> (env ≁ b) • res -> res) -> Exp env res a b
+exp :: (a -> ((env -> b) -> res) -> res) -> Exp env res a b
 exp = Exp
 
 exp' :: (a -> b) -> Exp env res a b
-exp' f = Exp (\ a kb -> kb • NotUntrue (pure (f a)))
+exp' f = Exp (\ a kb -> kb (pure (f a)))
 
 
 -- Elimination
 
-runExp :: Exp env res a b -> b • res -> a -> env ==> res
-runExp (Exp r) k a = C (\ env -> r a (K (\ b -> k • env ∘ runNotUntrue b)))
+runExp :: Exp env res a b -> (b -> res) -> a -> env ==> res
+runExp (Exp r) k a = C (\ env -> r a (\ b -> k (b env)))
