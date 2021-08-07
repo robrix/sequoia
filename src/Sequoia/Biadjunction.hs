2c5412689d8737261ca093d7f3f09e3dde29cb46
Rob Rix <rob.rix@me.com>
2021-08-06 21:28:45 -0400

    Define a Biadjunction instance for Either/(,).

--- src/Sequoia/Biadjunction.hs
+++ src/Sequoia/Biadjunction.hs
@@ -6,9 +6,15 @@ module Sequoia.Biadjunction
 
 import Data.Bifunctor
 import Sequoia.Birepresentable
+import Sequoia.Conjunction
+import Sequoia.Disjunction
 
 -- Biadjunctions
 
 class (Bifunctor f, Birepresentable u) => Biadjunction f u | f -> u, u -> f where
   bileftAdjunct :: (f a a -> b) -> (a -> u b b)
   birightAdjunct :: (a -> u b b) -> (f a a -> b)
+
+instance Biadjunction Either (,) where
+  bileftAdjunct  f = f . inl >---< f . inr
+  birightAdjunct f = exl . f <--> exr . f
