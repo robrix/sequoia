39cba35a9cb46d3a9c6a4be447851fd0f1edd6fb
Rob Rix <rob.rix@me.com>
2021-08-07 16:22:14 -0400

    Inline Par.

--- src/Sequoia/Profunctor/Exp/Par.hs
+++ src/Sequoia/Profunctor/Exp/Par.hs
@@ -12,7 +12,6 @@ import Data.Bifunctor
 import Prelude hiding (exp)
 import Sequoia.Calculus.Not
 import Sequoia.Calculus.NotUntrue
-import Sequoia.Connective.Par.Parameterized
 import Sequoia.Profunctor
 import Sequoia.Profunctor.Context
 import Sequoia.Profunctor.Continuation
@@ -20,23 +19,25 @@ import Sequoia.Profunctor.Value
 
 -- Exponentials
 
-newtype Exp env res a b = Exp { getExp :: Par res (a ¬ res) (env ≁ b) }
-  deriving (Functor)
+newtype Exp env res a b = Exp { getExp :: ((a ¬ res) • res, (env ≁ b) • res) • res }
+
+instance Functor (Exp env res a) where
+  fmap = rmap
 
 instance Profunctor (Exp e r) where
-  dimap f g = Exp . bimap (lmap f) (rmap g) . getExp
+  dimap f g = Exp . lmap (bimap (lmap (lmap f)) (lmap (rmap g))) . getExp
 
 
 -- Construction
 
 exp :: ((a ¬ res) • res -> (env ≁ b) • res -> res) -> Exp env res a b
-exp = Exp . Par . K . uncurry
+exp = Exp . K . uncurry
 
 exp' :: (a -> b) -> Exp env res a b
-exp' f = Exp (Par (K (\ (ka, kb) -> ka • Not (kb <<^ pure . f))))
+exp' f = Exp (K (\ (ka, kb) -> ka • Not (kb <<^ pure . f)))
 
 
 -- Elimination
 
 runExp :: Exp env res a b -> b • res -> a -> env ==> res
-runExp (Exp (Par (K r))) k a = C (\ env -> r (dn a <<^ getNot, K (\ b -> k • env ∘ runNotUntrue b)))
+runExp (Exp (K r)) k a = C (\ env -> r (dn a <<^ getNot, K (\ b -> k • env ∘ runNotUntrue b)))
