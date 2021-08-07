c3bc58ddfefaa767460d1ea4a0814c92b6bbed33
Rob Rix <rob.rix@me.com>
2021-08-07 18:47:12 -0400

    Apply triple negation elimination.

--- src/Sequoia/Profunctor/Exp/Par.hs
+++ src/Sequoia/Profunctor/Exp/Par.hs
@@ -19,19 +19,19 @@ import Sequoia.Profunctor.Value
 
 -- Exponentials
 
-newtype Exp env res a b = Exp { getExp :: forall res . (env ≁ b) • res -> (a ¬ res) • res -> res  }
+newtype Exp env res a b = Exp { getExp :: forall res . (env ≁ b) • res -> (a ¬ res)  }
 
 instance Functor (Exp env res a) where
   fmap = rmap
 
 instance Profunctor (Exp e r) where
-  dimap f g (Exp r) = Exp (dimap (lmap (rmap g)) (lmap (lmap (lmap f))) r)
+  dimap f g (Exp r) = Exp (dimap (lmap (rmap g)) (lmap f) r)
 
 
 -- Construction
 
 exp :: (forall res . (env ≁ b) • res -> a -> res) -> Exp env res a b
-exp f = Exp (\ b a -> a • inK (f b))
+exp f = Exp (inK . f)
 
 exp' :: (a -> b) -> Exp env res a b
 exp' f = exp (\ b a -> b • pure (f a))
@@ -40,4 +40,4 @@ exp' f = exp (\ b a -> b • pure (f a))
 -- Elimination
 
 runExp :: Exp env res a b -> b • res -> a -> env ==> res
-runExp (Exp r) k a = C (\ env -> r (k <<^ (env ∘)) (dn a))
+runExp (Exp r) k a = C (\ env -> r (k <<^ (env ∘)) • a)
