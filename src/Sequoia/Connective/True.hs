d1365acaa79d3ead0aa48f4dd26a662e95b5d9df
Rob Rix <rob.rix@me.com>
2021-08-07 21:44:41 -0400

    Move true down into a section for constructors.

--- src/Sequoia/Connective/True.hs
+++ src/Sequoia/Connective/True.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE UndecidableInstances #-}
 module Sequoia.Connective.True
 ( -- * True
-  true
-, True(..)
+  True(..)
 , type (✓)
+  -- * Construction
+, true
 ) where
 
 import Prelude hiding (True)
@@ -11,9 +12,6 @@ import Sequoia.Polarity
 import Sequoia.Profunctor.Continuation
 
 -- True
-
-true :: a -> True r a
-true = (`True` idK)
 
 data True r a = True { trueA :: a, trueK :: r • r }
   deriving (Functor)
@@ -23,3 +21,9 @@ instance Pos a => Polarized P (True e a)
 type (✓) = True
 
 infixr 9 ✓
+
+
+-- Construction
+
+true :: a -> True r a
+true = (`True` idK)
