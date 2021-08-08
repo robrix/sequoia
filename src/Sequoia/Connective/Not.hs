7eabffbae9eae37d41ecf6a8903ced7678e853b6
Rob Rix <rob.rix@me.com>
2021-08-08 09:49:02 -0400

    More instances for Not.

--- src/Sequoia/Connective/Not.hs
+++ src/Sequoia/Connective/Not.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE UndecidableInstances #-}
 module Sequoia.Connective.Not
 ( -- * Not
   Not(..)
 , type (¬)
 ) where
 
+import Data.Functor.Identity
 import Data.Profunctor
+import Data.Profunctor.Rep
+import Data.Profunctor.Sieve
 import Sequoia.Connective.Bottom
 import Sequoia.Polarity
 import Sequoia.Profunctor.Continuation
@@ -13,7 +17,13 @@ import Sequoia.Profunctor.Continuation
 
 newtype Not a r = Not { getNot :: a • Bottom r }
   deriving (Functor)
-  deriving (Continuation, ContinuationE, ContinuationI, Profunctor) via (•)
+  deriving (Continuation, ContinuationE, ContinuationI, Corepresentable, Costrong, Profunctor) via (•)
+
+instance Sieve Not Identity where
+  sieve = rmap Identity . (•)
+
+instance Cosieve Not Identity where
+  cosieve = lmap runIdentity . (•)
 
 instance Pos a => Polarized N (Not a r) where
 
