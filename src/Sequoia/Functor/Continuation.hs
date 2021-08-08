9c344aff5222daa709401feb81550ddc1910b27f
Rob Rix <rob.rix@me.com>
2021-08-08 18:10:12 -0400

    Define a Continuation class.

--- src/Sequoia/Functor/Continuation.hs
+++ src/Sequoia/Functor/Continuation.hs
@@ -1,9 +1,19 @@
+{-# LANGUAGE FunctionalDependencies #-}
 -- | Continuations encoded as (contravariant) functors.
 module Sequoia.Functor.Continuation
 ( -- * Continuation functor
   type (•)(..)
+  -- * Continuation abstraction
+, Continuation(..)
 ) where
 
 -- Continuation functor
 
 newtype r • a = K { runK :: a -> r }
+
+
+-- Continuation abstraction
+
+class Continuation r k | k -> r where
+  inK :: (a -> r) -> k a
+  (•) :: k a -> (a -> r)
