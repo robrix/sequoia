e865ed4dd3e54615eb0641c5e532e6275c62a827
Rob Rix <rob.rix@me.com>
2021-08-09 12:57:28 -0400

    Define the syntax for a sequent calculus.

--- src/Sequoia/Interpreter.hs
+++ src/Sequoia/Interpreter.hs
@@ -1,3 +1,31 @@
 -- | Direct-style and CPS interpreters for a small polarized calculus.
 module Sequoia.Interpreter
-() where
+( -- * Expressions
+  Expr(..)
+, Scope(..)
+) where
+
+import Sequoia.DeBruijn
+
+-- Expressions
+
+data Expr
+  = Var Index
+  | RTop
+  -- No rule for RZero
+  | RBottom
+  | ROne
+  | RWith Expr Expr
+  | RSum1 Expr
+  | RSum2 Expr
+  | RNot Scope
+  -- No rule for LTop
+  | LZero
+  | LBottom
+  | LOne
+  | LWith1 Expr Scope
+  | LWith2 Expr Scope
+  | LSum Expr Scope Scope
+  | LNot Expr Expr
+
+newtype Scope = Scope { getScope :: Expr }
