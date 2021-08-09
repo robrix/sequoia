d58a7e4b42c6286de52fcc9e5f1c4128bf1eb0fb
Rob Rix <rob.rix@me.com>
2021-08-09 08:36:11 -0400

    Define De Bruijn indices and levels.

--- src/Sequoia/DeBruijn.hs
+++ src/Sequoia/DeBruijn.hs
@@ -1,2 +1,36 @@
 module Sequoia.DeBruijn
-() where
+( -- De Bruijn indices
+  Index(..)
+, indexToLevel
+  -- De Bruijn levels
+, Level(..)
+, levelToIndex
+) where
+
+import Data.Functor.Classes
+
+-- De Bruijn indices
+
+-- | De Bruijn indices, counting up from the binding site to the reference site (“inside out”).
+newtype Index = Index { getIndex :: Int }
+  deriving (Enum, Eq, Num, Ord)
+
+instance Show Index where
+  showsPrec p = showsUnaryWith showsPrec "Index" p . getIndex
+
+
+indexToLevel :: Level -> Index -> Level
+indexToLevel (Level d) (Index index) = Level $ d - index - 1
+
+
+-- De Bruijn levels
+
+-- | De Bruijn indices, counting up from the root to the binding site (“outside in”).
+newtype Level = Level { getLevel :: Int }
+  deriving (Enum, Eq, Num, Ord)
+
+instance Show Level where
+  showsPrec p = showsUnaryWith showsPrec "Level" p . getLevel
+
+levelToIndex :: Level -> Level -> Index
+levelToIndex (Level d) (Level level) = Index $ d - level - 1
