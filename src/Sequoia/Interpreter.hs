4bc6f8bd89b9d737a402e44eea87e6c1f6ad41e2
Rob Rix <rob.rix@me.com>
2021-08-09 13:04:04 -0400

    Define values and eliminations.

--- src/Sequoia/Interpreter.hs
+++ src/Sequoia/Interpreter.hs
@@ -3,9 +3,13 @@ module Sequoia.Interpreter
 ( -- * Expressions
   Expr(..)
 , Scope(..)
+  -- * Values
+, Val(..)
+, Elim(..)
 ) where
 
 import Sequoia.DeBruijn
+import Sequoia.Snoc
 
 -- Expressions
 
@@ -33,3 +37,27 @@ data Expr
 
 newtype Scope = Scope { getScope :: Expr }
   deriving (Show)
+
+
+-- Values
+
+data Val
+  = VNe Level (Snoc Elim)
+  | VTop
+  | VBottom
+  | VOne
+  | VWith Val Val
+  | VSum1 Val
+  | VSum2 Val
+  | VNot (Val -> Val)
+  | VNeg (Val -> Val)
+
+data Elim
+  = EZero
+  | EBottom
+  | EOne
+  | EWIth1 (Val -> Val)
+  | EWIth2 (Val -> Val)
+  | ESum (Val -> Val) (Val -> Val)
+  | ENot Val
+  | ENeg Val
