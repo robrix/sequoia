345dee08bf8b475473e5528ba889294d32f862c1
Rob Rix <rob.rix@me.com>
2021-08-09 08:18:14 -0400

    Define snoc lists.

--- src/Sequoia/Snoc.hs
+++ src/Sequoia/Snoc.hs
@@ -1,2 +1,18 @@
 module Sequoia.Snoc
-() where
+( -- Snoc lists
+  Snoc(..)
+) where
+
+import Data.Foldable (toList)
+
+-- Snoc lists
+
+data Snoc a
+  = Nil
+  | Snoc a :> a
+  deriving (Eq, Foldable, Functor, Ord, Traversable)
+
+infixl 5 :>
+
+instance Show a => Show (Snoc a) where
+  showsPrec p s = showParen (p > 10) $ showString "fromList" . showChar ' ' . showList (toList s)
