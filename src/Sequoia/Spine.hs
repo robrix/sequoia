0b7e6c01b0611ae714d9816d66b793b1eba8db5c
Rob Rix <rob.rix@me.com>
2021-08-10 03:59:24 -0400

    Define a Category instance for Spine.

--- src/Sequoia/Spine.hs
+++ src/Sequoia/Spine.hs
@@ -4,8 +4,18 @@ module Sequoia.Spine
   Spine(..)
 ) where
 
+import qualified Control.Category as Cat
+
+-- Spines
+
 data Spine arr a b where
   Nil :: Spine arr a a
   (:>) :: Spine arr a b -> arr b c -> Spine arr a c
 
 infixl 5 :>
+
+instance Cat.Category (Spine arr) where
+  id = Nil
+  fs . gs = case fs of
+    Nil   -> gs
+    fs:>f -> (gs Cat.>>> fs) :> f
