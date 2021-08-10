35bc787c7ddaffaeb4f73d0aa8c9044f732ffca4
Rob Rix <rob.rix@me.com>
2021-08-10 03:01:54 -0400

    Define a GADT for spines represented as type-aligned snoc lists.

--- src/Sequoia/Spine.hs
+++ src/Sequoia/Spine.hs
@@ -1,2 +1,11 @@
+{-# LANGUAGE GADTs #-}
 module Sequoia.Spine
-() where
+( -- * Spines
+  Spine(..)
+) where
+
+data Spine arr a b where
+  Nil :: Spine arr a a
+  (:>) :: Spine arr a b -> arr b c -> Spine arr a c
+
+infixl 5 :>
