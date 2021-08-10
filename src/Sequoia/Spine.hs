81720645478d43475412f3fd2b5c48b9bbaa0ac1
Rob Rix <rob.rix@me.com>
2021-08-10 08:02:00 -0400

    Rename the Spine constructors to resemble composition.

--- src/Sequoia/Spine.hs
+++ src/Sequoia/Spine.hs
@@ -9,13 +9,13 @@ import qualified Control.Category as Cat
 -- Spines
 
 data Spine arr a b where
-  Nil :: Spine arr a a
-  (:>) :: Spine arr a b -> arr b c -> Spine arr a c
+  Id :: Spine arr a a
+  (:.) :: Spine arr a b -> arr b c -> Spine arr a c
 
-infixl 5 :>
+infixl 9 :.
 
 instance Cat.Category (Spine arr) where
-  id = Nil
+  id = Id
   fs . gs = case fs of
-    Nil   -> gs
-    fs:>f -> (gs Cat.>>> fs) :> f
+    Id    -> gs
+    fs:.f -> (gs Cat.>>> fs) :. f
