34a4f31b8c61b0f23e2e09462ddcab4973f7a14e
Rob Rix <rob.rix@me.com>
2021-08-12 09:12:50 -0400

    Give (co)vals both parameters.

--- src/Sequoia/Interpreter/Typed.hs
+++ src/Sequoia/Interpreter/Typed.hs
@@ -72,37 +72,37 @@ deriving instance Show (Coexpr ctx a b)
 
 -- Values
 
-data Val ctx a where
-  VNe :: IxL a ctx -> Val ctx a
-  VTop :: Val ctx Top
-  VWith :: Val ctx a -> Val ctx b -> Val ctx (a & b)
-  VSum1 :: Val ctx a -> Val ctx (a ⊕ b)
-  VSum2 :: Val ctx b -> Val ctx (a ⊕ b)
-  VOne :: Val ctx (One (E ctx))
-  VPar :: Val ctx (Either a b) -> Val ctx (a ⅋ b)
-  VTensor :: Val ctx a -> Val ctx b -> Val ctx (a ⊗ b)
-  VFun :: (Val (a < ctx) a -> Val (a < ctx) b) -> Val ctx (Fun (R ctx) a b)
-  VSub :: Val ctx a -> Coval ctx b -> Val ctx (Sub (R ctx) b a)
+data Val ctx a b where
+  VNe :: IxL a ctx -> Val ctx (E ctx) a
+  VTop :: Val ctx (E ctx) Top
+  VWith :: Val ctx (E ctx) a -> Val ctx (E ctx) b -> Val ctx (E ctx) (a & b)
+  VSum1 :: Val ctx (E ctx) a -> Val ctx (E ctx) (a ⊕ b)
+  VSum2 :: Val ctx (E ctx) b -> Val ctx (E ctx) (a ⊕ b)
+  VOne :: Val ctx (E ctx) (One (E ctx))
+  VPar :: Val ctx (E ctx) (Either a b) -> Val ctx (E ctx) (a ⅋ b)
+  VTensor :: Val ctx (E ctx) a -> Val ctx (E ctx) b -> Val ctx (E ctx) (a ⊗ b)
+  VFun :: (Val (a < ctx) (E ctx) a -> Val (a < ctx) (E ctx) b) -> Val ctx (E ctx) (Fun (R ctx) a b)
+  VSub :: Val ctx (E ctx) a -> Coval ctx b (R ctx) -> Val ctx (E ctx) (Sub (R ctx) b a)
 
-data Coval ctx a where
-  EZero :: Coval ctx Zero
-  EWith1 :: Coval ctx a -> Coval ctx (a & b)
-  EWith2 :: Coval ctx b -> Coval ctx (a & b)
-  ESum :: Coval ctx a -> Coval ctx b -> Coval ctx (a ⊕ b)
-  EBottom :: Coval ctx (Bottom (R ctx))
-  EOne :: Coval ctx a -> Coval ctx (One (E ctx), a)
-  EPar :: Coval ctx a -> Coval ctx b -> Coval ctx (a ⅋ b)
-  ETensor :: Coval ctx (a, b) -> Coval ctx (a ⊗ b)
-  EFun :: Val ctx a -> Coval ctx b -> Coval ctx (Fun (R ctx) a b)
-  ESub :: (Val (a < ctx) a -> Val (a < ctx) b) -> Coval ctx (Sub (R ctx) b a)
+data Coval ctx a b where
+  EZero :: Coval ctx Zero (R ctx)
+  EWith1 :: Coval ctx a (R ctx) -> Coval ctx (a & b) (R ctx)
+  EWith2 :: Coval ctx b (R ctx) -> Coval ctx (a & b) (R ctx)
+  ESum :: Coval ctx a (R ctx) -> Coval ctx b (R ctx) -> Coval ctx (a ⊕ b) (R ctx)
+  EBottom :: Coval ctx (Bottom (R ctx)) (R ctx)
+  EOne :: Coval ctx a (R ctx) -> Coval ctx (One (E ctx), a) (R ctx)
+  EPar :: Coval ctx a (R ctx) -> Coval ctx b (R ctx) -> Coval ctx (a ⅋ b) (R ctx)
+  ETensor :: Coval ctx (a, b) (R ctx) -> Coval ctx (a ⊗ b) (R ctx)
+  EFun :: Val ctx (E ctx) a -> Coval ctx b (R ctx) -> Coval ctx (Fun (R ctx) a b) (R ctx)
+  ESub :: (Val (a < ctx) (E ctx) a -> Val (a < ctx) (E ctx) b) -> Coval ctx (Sub (R ctx) b a) (R ctx)
 
-bindVal :: (a -> b) -> (Val (x < ctx) x -> a) -> b
+bindVal :: (a -> b) -> (Val (x < ctx) (E ctx) x -> a) -> b
 bindVal with b = with (b (VNe IxLZ))
 
 
 -- Quotation
 
-quoteVal :: Val ctx b -> Expr ctx (E ctx) b
+quoteVal :: Val ctx (E ctx) b -> Expr ctx (E ctx) b
 quoteVal = \case
   VNe l       -> Var l
   VTop        -> RTop
@@ -115,7 +115,7 @@ quoteVal = \case
   VFun f      -> RFun (quoteBinder f)
   VSub a b    -> RSub (quoteVal a) (quoteCoval b)
 
-quoteCoval :: Coval ctx a -> Coexpr ctx a (R ctx)
+quoteCoval :: Coval ctx a (R ctx) -> Coexpr ctx a (R ctx)
 quoteCoval = \case
   EZero     -> LZero
   EWith1 f  -> LWith1 (quoteCoval f)
@@ -128,7 +128,7 @@ quoteCoval = \case
   EFun a b  -> LFun (quoteVal a) (quoteCoval b)
   ESub f    -> LSub (quoteBinder f)
 
-quoteBinder :: (Val (t < ctx) t -> Val (t < ctx) u) -> Expr (t < ctx) (E ctx) u
+quoteBinder :: (Val (t < ctx) (E ctx) t -> Val (t < ctx) (E ctx) u) -> Expr (t < ctx) (E ctx) u
 quoteBinder = bindVal quoteVal
 
 
@@ -165,7 +165,7 @@ coevalDef ctx = \case
 
 -- Execution
 
-execVal :: Ctx ctx => ctx -> Val ctx a -> DN (R ctx) a
+execVal :: Ctx ctx => ctx -> Val ctx (E ctx) a -> DN (R ctx) a
 execVal ctx = \case
   VNe i       -> pure (i <! ctx)
   VTop        -> pure Top
@@ -178,7 +178,7 @@ execVal ctx = \case
   VFun f      -> pure (fun (\ b a -> runDN (bindVal (execVal (a :< ctx)) f) • b))
   VSub a b    -> execVal ctx a <&> (execCoval ctx b :>-)
 
-execCoval :: Ctx ctx => ctx -> Coval ctx a -> (a • R ctx)
+execCoval :: Ctx ctx => ctx -> Coval ctx a (R ctx) -> (a • R ctx)
 execCoval ctx = \case
   EZero     -> K absurdP
   EWith1 a  -> exlL (execCoval ctx a)
