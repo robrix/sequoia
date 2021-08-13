be03aeba6899eb24bab6c841751f1caadc6b9ffc
Rob Rix <rob.rix@me.com>
2021-08-12 22:46:42 -0400

    Add par.

--- src/Sequoia/Interpreter/Typed.hs
+++ src/Sequoia/Interpreter/Typed.hs
@@ -39,6 +39,7 @@ import Sequoia.Conjunction
 import Sequoia.Connective.Function
 import Sequoia.Connective.Negate
 import Sequoia.Connective.Not
+import Sequoia.Connective.Par
 import Sequoia.Connective.Subtraction
 import Sequoia.Connective.Sum
 import Sequoia.Connective.Tensor
@@ -62,6 +63,7 @@ data Expr b where
   XWithR :: Expr a -> Expr b -> Expr (a & b)
   XSumR1 :: Expr a -> Expr (a ⊕ b)
   XSumR2 :: Expr b -> Expr (a ⊕ b)
+  XParR :: Expr (Either a b) -> Expr (a ⅋ b)
   XTensorR :: Expr a -> Expr b -> Expr (a ⊗ b)
   XNotR :: Coexpr a -> Expr (Not a R)
   XNegR :: Coexpr a -> Expr (Negate E a R)
@@ -74,6 +76,7 @@ data Coexpr a where
   XWithL1 :: Coexpr a -> Coexpr (a & b)
   XWithL2 :: Coexpr b -> Coexpr (a & b)
   XSumL :: Coexpr a -> Coexpr b -> Coexpr (a ⊕ b)
+  XParL :: Coexpr a -> Coexpr b -> Coexpr (a ⅋ b)
   XTensorL :: Coexpr (a, b) -> Coexpr (a ⊗ b)
   XNotL :: Expr a -> Coexpr (Not a R)
   XNegL :: Expr a -> Coexpr (Negate E a R)
@@ -90,6 +93,7 @@ data Val b where
   VWithR :: Val a -> Val b -> Val (a & b)
   VSumR1 :: Val a -> Val (a ⊕ b)
   VSumR2 :: Val b -> Val (a ⊕ b)
+  VParR :: Val (Either a b) -> Val (a ⅋ b)
   VTensorR :: Val a -> Val b -> Val (a ⊗ b)
   VNotR :: Coval a -> Val (Not a R)
   VNegR :: Coval a -> Val (Negate E a R)
@@ -100,6 +104,7 @@ data Coval a where
   VWithL1 :: Coval a -> Coval (a & b)
   VWithL2 :: Coval b -> Coval (a & b)
   VSumL :: Coval a -> Coval b -> Coval (a ⊕ b)
+  VParL :: Coval a -> Coval b -> Coval (a ⅋ b)
   VTensorL :: Coval (a, b) -> Coval (a ⊗ b)
   VNotL :: Val a -> Coval (Not a R)
   VNegL :: Val a -> Coval (Negate E a R)
@@ -115,6 +120,7 @@ quoteVal c = \case
   VWithR a b   -> XWithR (quoteVal c a) (quoteVal c b)
   VSumR1 a     -> XSumR1 (quoteVal c a)
   VSumR2 b     -> XSumR2 (quoteVal c b)
+  VParR a      -> XParR (quoteVal c a)
   VTensorR a b -> XTensorR (quoteVal c a) (quoteVal c b)
   VNotR a      -> XNotR (quoteCoval c a)
   VNegR a      -> XNegR (quoteCoval c a)
@@ -126,6 +132,7 @@ quoteCoval c = \case
   VWithL1 f  -> XWithL1 (quoteCoval c f)
   VWithL2 g  -> XWithL2 (quoteCoval c g)
   VSumL f g  -> XSumL (quoteCoval c f) (quoteCoval c g)
+  VParL f g  -> XParL (quoteCoval c f) (quoteCoval c g)
   VTensorL f -> XTensorL (quoteCoval c f)
   VNotL a    -> XNotL (quoteVal c a)
   VNegL a    -> XNegL (quoteVal c a)
@@ -156,6 +163,7 @@ evalDef ctx = \case
   XWithR a b   -> VWithR (evalDef ctx a) (evalDef ctx b)
   XSumR1 a     -> VSumR1 (evalDef ctx a)
   XSumR2 b     -> VSumR2 (evalDef ctx b)
+  XParR a      -> VParR (evalDef ctx a)
   XTensorR a b -> VTensorR (evalDef ctx a) (evalDef ctx b)
   XNotR a      -> VNotR (coevalDef ctx a)
   XNegR a      -> VNegR (coevalDef ctx a)
@@ -167,6 +175,7 @@ coevalDef ctx = \case
   XWithL1 f  -> VWithL1 (coevalDef ctx f)
   XWithL2 g  -> VWithL2 (coevalDef ctx g)
   XSumL f g  -> VSumL (coevalDef ctx f) (coevalDef ctx g)
+  XParL f g  -> VParL (coevalDef ctx f) (coevalDef ctx g)
   XTensorL f -> VTensorL (coevalDef ctx f)
   XNotL a    -> VNotL (evalDef ctx a)
   XNegL a    -> VNegL (evalDef ctx a)
@@ -185,6 +194,7 @@ execVal ctx = \case
   VWithR a b   -> liftA2 inlr (execVal ctx a) (execVal ctx b)
   VSumR1 a     -> inlF (execVal ctx a)
   VSumR2 b     -> inrF (execVal ctx b)
+  VParR a      -> coerceDisj <$> execVal ctx a
   VTensorR a b -> liftA2 inlr (execVal ctx a) (execVal ctx b)
   VNotR a      -> Eval (inK (• coerceK (coeval (execCoval ctx a))))
   VNegR a      -> Eval (inK (• Negate E (coeval (execCoval ctx a))))
@@ -196,6 +206,7 @@ execCoval ctx = \case
   VWithL1 f  -> exlL (execCoval ctx f)
   VWithL2 g  -> exrL (execCoval ctx g)
   VSumL f g  -> execCoval ctx f <••> execCoval ctx g
+  VParL f g  -> execCoval ctx f <••> execCoval ctx g
   VTensorL f -> execCoval ctx f <<^ coerceConj
   VNotL a    -> inK (\ x -> eval (execVal ctx a) • coerceK x)
   VNegL a    -> inK (\ x -> eval (execVal ctx a) • coerceK x)
