86a047c4bdd7a7b154aeef574338226c50994daa
Rob Rix <rob.rix@me.com>
2021-08-12 21:54:32 -0400

    Add Negate.

--- src/Sequoia/Interpreter/Typed.hs
+++ src/Sequoia/Interpreter/Typed.hs
@@ -37,6 +37,7 @@ import Control.Applicative (liftA2)
 import Prelude hiding (exp)
 import Sequoia.Conjunction
 import Sequoia.Connective.Function
+import Sequoia.Connective.Negate
 import Sequoia.Connective.Not
 import Sequoia.Connective.Subtraction
 import Sequoia.Connective.Sum
@@ -61,6 +62,7 @@ data Expr b where
   XSumR1 :: Expr a -> Expr (a ⊕ b)
   XSumR2 :: Expr b -> Expr (a ⊕ b)
   XNotR :: Coexpr a -> Expr (Not a R)
+  XNegR :: Coexpr a -> Expr (Negate E a R)
   XFunR :: Expr b -> Expr (Fun R a b)
   XSubR :: Expr a -> Coexpr b -> Expr (Sub R b a)
 
@@ -71,6 +73,7 @@ data Coexpr a where
   XWithL2 :: Coexpr b -> Coexpr (a & b)
   XSumL :: Coexpr a -> Coexpr b -> Coexpr (a ⊕ b)
   XNotL :: Expr a -> Coexpr (Not a R)
+  XNegL :: Expr a -> Coexpr (Negate E a R)
   XFunL :: Expr a -> Coexpr b -> Coexpr (Fun R a b)
   XSubL :: Expr b -> Coexpr (Sub R b a)
 
@@ -85,6 +88,7 @@ data Val b where
   VSumR1 :: Val a -> Val (a ⊕ b)
   VSumR2 :: Val b -> Val (a ⊕ b)
   VNotR :: Coval a -> Val (Not a R)
+  VNegR :: Coval a -> Val (Negate E a R)
   VFunR :: (Val a -> Val b) -> Val (Fun R a b)
   VSubR :: Val a -> Coval b -> Val (Sub R b a)
 
@@ -93,6 +97,7 @@ data Coval a where
   VWithL2 :: Coval b -> Coval (a & b)
   VSumL :: Coval a -> Coval b -> Coval (a ⊕ b)
   VNotL :: Val a -> Coval (Not a R)
+  VNegL :: Val a -> Coval (Negate E a R)
   VFunL :: Val a -> Coval b -> Coval (Fun R a b)
   VSubL :: (Val a -> Val b) -> Coval (Sub R b a)
 
@@ -106,6 +111,7 @@ quoteVal c = \case
   VSumR1 a   -> XSumR1 (quoteVal c a)
   VSumR2 b   -> XSumR2 (quoteVal c b)
   VNotR a    -> XNotR (quoteCoval c a)
+  VNegR a    -> XNegR (quoteCoval c a)
   VFunR f    -> XFunR (quoteBinder c f)
   VSubR a b  -> XSubR (quoteVal c a) (quoteCoval c b)
 
@@ -115,6 +121,7 @@ quoteCoval c = \case
   VWithL2 g -> XWithL2 (quoteCoval c g)
   VSumL f g -> XSumL (quoteCoval c f) (quoteCoval c g)
   VNotL a   -> XNotL (quoteVal c a)
+  VNegL a   -> XNegL (quoteVal c a)
   VFunL a b -> XFunL (quoteVal c a) (quoteCoval c b)
   VSubL f   -> XSubL (quoteBinder c f)
 
@@ -143,6 +150,7 @@ evalDef ctx = \case
   XSumR1 a   -> VSumR1 (evalDef ctx a)
   XSumR2 b   -> VSumR2 (evalDef ctx b)
   XNotR a    -> VNotR (coevalDef ctx a)
+  XNegR a    -> VNegR (coevalDef ctx a)
   XFunR f    -> VFunR (evalBinder ctx f)
   XSubR a b  -> VSubR (evalDef ctx a) (coevalDef ctx b)
 
@@ -152,6 +160,7 @@ coevalDef ctx = \case
   XWithL2 g -> VWithL2 (coevalDef ctx g)
   XSumL f g -> VSumL (coevalDef ctx f) (coevalDef ctx g)
   XNotL a   -> VNotL (evalDef ctx a)
+  XNegL a   -> VNegL (evalDef ctx a)
   XFunL a b -> VFunL (evalDef ctx a) (coevalDef ctx b)
   XSubL f   -> VSubL (evalBinder ctx f)
 
@@ -168,6 +177,7 @@ execVal ctx = \case
   VSumR1 a   -> inlF (execVal ctx a)
   VSumR2 b   -> inrF (execVal ctx b)
   VNotR a    -> Eval (inK (• coerceK (coeval (execCoval ctx a))))
+  VNegR a    -> Eval (inK (• Negate E (coeval (execCoval ctx a))))
   VFunR f    -> pure (fun (\ b a -> let ctx' = I a :< ctx in eval (execVal ctx' (f (VNe (lv ctx')))) • Coeval b))
   VSubR a b  -> do { a' <- execVal ctx a ; pure (coeval (execCoval ctx b) :>- a') }
 
@@ -177,6 +187,7 @@ execCoval ctx = \case
   VWithL2 g -> exrL (execCoval ctx g)
   VSumL f g -> execCoval ctx f <••> execCoval ctx g
   VNotL a   -> inK (\ x -> eval (execVal ctx a) • coerceK x)
+  VNegL a   -> inK (\ x -> eval (execVal ctx a) • coerceK x)
   VFunL a b -> inK (\ f -> eval (execVal ctx a >>= evalFun f) • execCoval ctx b)
   VSubL f   -> inK (\ (b :>- a) -> let ctx' = I a :< ctx in eval (execVal ctx' (f (VNe (lv ctx')))) • Coeval b)
 
